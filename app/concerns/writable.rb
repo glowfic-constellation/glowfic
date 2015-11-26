1b9ac122f2cab394c3d04d59bc6d310136a49286
Marri Nikari <xxstealthestarsxx@gmail.com>
2015-11-26 16:34:20 -0600

    Don't allow people to post as others' characters or icons

--- app/concerns/writable.rb
+++ app/concerns/writable.rb
@@ -9,5 +9,20 @@ module Writable
     attr_accessible :character, :character_id, :user, :user_id, :icon, :icon_id, :content
 
     validates_presence_of :user, :content
+    validate :character_ownership, :icon_ownership
+
+    private
+
+    def character_ownership
+      return true unless character
+      return true if character.user_id == user_id
+      errors.add(:character, "must be yours")
+    end
+
+    def icon_ownership
+      return true unless icon
+      return true if icon.user_id == user_id
+      errors.add(:icon, "must be yours")
+    end
   end
 end
