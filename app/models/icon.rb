1de6926992992a8f7780e573774b77641008d3c4
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-01-18 21:22:55 -0500

    Basic URL validation. Really, really, really basic.

--- app/models/icon.rb
+++ app/models/icon.rb
@@ -4,9 +4,17 @@ class Icon < ActiveRecord::Base
   has_and_belongs_to_many :galleries
 
   validates_presence_of :url, :user, :keyword
+  validate :url_is_url
   nilify_blanks
 
   def to_json
     { id: id, url: url, keyword: keyword }
+  end
+
+  private
+
+  def url_is_url
+    return true if url.starts_with?('http')
+    errors.add(:url, "must be an actual fully qualified url (http://www.example.com)")
   end
 end
