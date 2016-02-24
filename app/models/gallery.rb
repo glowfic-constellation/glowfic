50483048701b2e6710c7bd87937a19119cd2876d
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-02-24 00:49:05 -0500

    Move callbacks

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,12 +1,23 @@
 class Gallery < ActiveRecord::Base
   belongs_to :user
   belongs_to :cover_icon, :class_name => Icon
-  has_and_belongs_to_many :icons
+  has_and_belongs_to_many :icons, after_add: :set_has_gallery, after_remove: :unset_has_gallery
   has_and_belongs_to_many :characters
 
   validates_presence_of :user, :name
 
   def default_icon
     cover_icon || icons.first
+  end
+
+  private
+
+  def set_has_gallery(icon)
+    icon.update_attributes(has_gallery: true)
+  end
+
+  def unset_has_gallery(icon)
+    return if icon.galleries.present?
+    icon.update_attributes(has_gallery: false)
   end
 end
