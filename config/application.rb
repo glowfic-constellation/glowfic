428cc1933b6f898e4d47d94dc124e12c32d23d4f
Edward Jones <throne3d@gmail.com>
2016-07-15 00:14:46 +0100

    Add `<s>` (newer strikethrough tag) to the `ALLOWED_TAGS` constant

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Glowfic
-  ALLOWED_TAGS = ["b", "i", "u", "sub", "sup", "del", "hr", "p", "br", "div", "span", "pre", "code", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "ol", "li", "dl", "dt", "dd", "a", "img", "blockquote", "table", "td", "th", "tr", "strike", "strong", "em", "big", "small", "font"]
+  ALLOWED_TAGS = ["b", "i", "u", "sub", "sup", "del", "hr", "p", "br", "div", "span", "pre", "code", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "ol", "li", "dl", "dt", "dd", "a", "img", "blockquote", "table", "td", "th", "tr", "strike", "s", "strong", "em", "big", "small", "font"]
 
   class Application < Rails::Application
     config.assets.enabled = true
