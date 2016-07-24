b5f41c9a5482a6c9911334acd0bff3a04e0ab07d
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-07-24 02:06:06 -0400

    Fix next and prev links

--- app/controllers/writable_controller.rb
+++ app/controllers/writable_controller.rb
@@ -71,8 +71,8 @@ class WritableController < ApplicationController
     redirect_to post_path(@post, page: @replies.total_pages, per_page: per) and return if cur_page > @replies.total_pages
     use_javascript('paginator')
 
-    @next_post = Post.where(board_id: @post.board_id).where("id > #{@post.id}").order('id asc').limit(1).first
-    @prev_post = Post.where(board_id: @post.board_id).where("id < #{@post.id}").order('id desc').limit(1).first
+    @next_post = Post.where(board_id: @post.board_id).where(section_id: @post.section_id).where(section_order: @post.section_order + 1).first
+    @prev_post = Post.where(board_id: @post.board_id).where(section_id: @post.section_id).where(section_order: @post.section_order - 1).first
 
     if logged_in?
       use_javascript('posts')
