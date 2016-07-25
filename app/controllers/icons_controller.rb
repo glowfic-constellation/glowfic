6b68464938b1b68a0e13c81cb0fe4332e8cd7384
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-07-25 18:20:07 -0400

    Don't update things when replacing icons

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -81,10 +81,12 @@ class IconsController < ApplicationController
     Post.transaction do
       Reply.where(icon_id: @icon.id).each do |reply|
         reply.icon_id = new_icon.try(:id)
+        reply.skip_post_update = true
         reply.save
       end
       Post.where(icon_id: @icon.id).each do |post|
         post.icon_id = new_icon.try(:id)
+        post.skip_edited = true
         post.save
       end
     end
