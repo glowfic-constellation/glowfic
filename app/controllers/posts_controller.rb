a002df9b468c7f7f767ac89d2fb3a889ef4e92d2
Edward Jones <throne3d@gmail.com>
2016-08-15 14:31:24 +0100

    Remove `reorder_sections` from `PostsController`
    Can't see any routes through to it; can probably be removed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -82,8 +82,6 @@ class PostsController < WritableController
   end
 
   def create
-    reorder_sections and return if params[:commit] == "reorder"
-
     gon.original_content = params[:post][:content]
     preview(:post, posts_path) and return if params[:button_preview].present?
 
@@ -248,18 +246,6 @@ class PostsController < WritableController
       flash[:error] = "You do not have permission to modify this post."
       redirect_to post_path(@post)
     end
-  end
-
-  def reorder_sections
-    Post.transaction do
-      params[:changes].each do |post_id, section_order|
-        post = Post.where(id: post_id).first
-        next unless post
-        post.section_order = section_order
-        post.save
-      end
-    end
-    render json: {}
   end
 
   def build_tags
