5b13007c456891706b05f5878f02b6115e8d1982
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-08-19 23:55:42 -0800

    Use a do/while like loop

--- app/models/password_reset.rb
+++ app/models/password_reset.rb
@@ -18,10 +18,9 @@ class PasswordReset < ActiveRecord::Base
   def generate_unique_auth_token
     return if auth_token.present?
     return unless user
-    # TODO do while loop
-    self.auth_token = generate_auth_token
-    while self.class.where(auth_token: self.auth_token).exists?
+    loop do 
       self.auth_token = generate_auth_token
+      break unless self.class.where(auth_token: self.auth_token).exists?
     end
   end
 
