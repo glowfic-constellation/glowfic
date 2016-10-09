cd1064313068965a1a9c78fc8d59027566a3a3ab
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-10-09 15:23:22 -0700

    Set super long cache control on static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,6 @@ Glowfic::Application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -59,6 +55,8 @@ Glowfic::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'https://dhtmoj33sf3e0.cloudfront.net'
+  config.static_cache_control = 'public, max-age=31536000'
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
