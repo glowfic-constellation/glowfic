211512533d22d4fe74fa044908d62dc7fadccc8a
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-10-09 22:24:54 -0700

    Move production settings in case that helps the cache headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Glowfic::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.static_cache_control = 'public, max-age=31536000'
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -55,8 +58,6 @@ Glowfic::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'https://dhtmoj33sf3e0.cloudfront.net'
-  config.static_cache_control = 'public, max-age=31536000'
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
