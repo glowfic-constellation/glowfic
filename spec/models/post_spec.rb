15e5a3683e55b464db3f5a43ae062c415302d15d
Edward Jones <throne3d@gmail.com>
2016-10-25 19:53:25 +0100

    Use be_the_same_time_as in tests because time precision differs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,34 +4,34 @@ RSpec.describe Post do
   it "should have the right timestamps" do
     # creation
     post = create(:post)
-    expect(post.edited_at).to eq(post.created_at)
-    expect(post.tagged_at).to eq(post.created_at)
+    expect(post.edited_at).to be_the_same_time_as(post.created_at)
+    expect(post.tagged_at).to be_the_same_time_as(post.created_at)
 
     # edited with no replies
     post.content = 'new content'
     post.save
-    expect(post.tagged_at).to eq(post.edited_at)
+    expect(post.tagged_at).to be_the_same_time_as(post.edited_at)
     expect(post.tagged_at).to be > post.created_at
     old_edited_at = post.edited_at
 
     # reply created
     reply = create(:reply, post: post)
     post.reload
-    expect(post.tagged_at).to eq(reply.created_at)
-    expect(post.edited_at).to eq(old_edited_at)
+    expect(post.tagged_at).to be_the_same_time_as(reply.created_at)
+    expect(post.edited_at).to be_the_same_time_as(old_edited_at)
     expect(post.tagged_at).to be > post.edited_at
     old_tagged_at = post.tagged_at
 
     # edited with replies
     post.content = 'newer content'
     post.save
-    expect(post.tagged_at).to eq(old_tagged_at)
+    expect(post.tagged_at).to be_the_same_time_as(old_tagged_at)
     expect(post.edited_at).to be > old_edited_at
 
     # second reply created
     reply2 = create(:reply, post: post)
     post.reload
-    expect(post.tagged_at).to eq(reply2.created_at)
+    expect(post.tagged_at).to be_the_same_time_as(reply2.created_at)
     expect(post.updated_at).to be >= reply2.created_at
     expect(post.tagged_at).to be > post.edited_at
     old_tagged_at = post.tagged_at
@@ -42,16 +42,16 @@ RSpec.describe Post do
     reply.skip_post_update = true unless reply.post.last_reply_id == reply.id
     reply.save
     post.reload
-    expect(post.tagged_at).to eq(old_tagged_at) # BAD
-    expect(post.edited_at).to eq(old_edited_at)
+    expect(post.tagged_at).to be_the_same_time_as(old_tagged_at) # BAD
+    expect(post.edited_at).to be_the_same_time_as(old_edited_at)
 
     # second reply updated
     reply2.content = 'new content'
     reply2.skip_post_update = true unless reply2.post.last_reply_id == reply2.id
     reply2.save
     post.reload
-    expect(post.tagged_at).to eq(reply2.updated_at)
-    expect(post.edited_at).to eq(old_edited_at)
+    expect(post.tagged_at).to be_the_same_time_as(reply2.updated_at)
+    expect(post.edited_at).to be_the_same_time_as(old_edited_at)
   end
 
   it "should allow blank content" do
