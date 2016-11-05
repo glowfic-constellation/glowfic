ef3ca0569dcbf11a38af809d0877e7151f6c1985
Marri Nikari <xxstealthestarsxx@gmail.com>
2016-11-05 12:41:01 -0400

    Improvements to the Daily Report so we can stop generating it manually.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,4 @@
 class ReportsController < ApplicationController
-  around_filter :set_fixed_timezone
-
   def index
   end
 
@@ -9,13 +7,22 @@ class ReportsController < ApplicationController
       flash[:error] = "Could not identify the type of report."
       redirect_to reports_path
     end
+
+    if logged_in?
+      @opened_posts = PostView.where(user_id: current_user.id).select([:post_id, :read_at])
+      @opened_ids = @opened_posts.map(&:post_id)
+    end
   end
 
   private
 
-  def set_fixed_timezone(&block)
-    Time.use_zone("Alaska", &block)
+  def has_unread?(post)
+    return false unless @opened_posts
+    view = @opened_posts.detect { |v| v.post_id == post.id }
+    return false unless view
+    view.read_at < post.tagged_at
   end
+  helper_method :has_unread?
 
   def posts_for(day)
     posts = Post.where(tagged_at: day.beginning_of_day .. day.end_of_day).includes(:board, :user, :last_user)
@@ -32,8 +39,9 @@ class ReportsController < ApplicationController
   helper_method :posts_for
 
   def linked_for(day, post, replies=nil)
+    return post if post.created_at.to_date == day.to_date
     replies ||= post.replies.where(created_at: day.beginning_of_day .. day.end_of_day).order('created_at asc')
-    return post if replies.empty? || post.created_at.to_date == day.to_date
+    return post if replies.empty?
     replies.first
   end
   helper_method :linked_for
