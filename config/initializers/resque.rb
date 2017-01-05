10ce27777ef216396c3006c45a6a477e9631886c
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-01-04 22:45:22 -0500

    Add SIGTERM handling for ResqueMailer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,3 +5,12 @@ end
 Resque.after_fork do
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
+
+Resque::Mailer.error_handler = lambda { |mailer, message, error, action, args|
+  # Necessary to re-enqueue jobs that receieve the SIGTERM signal
+  if error.is_a?(Resque::TermException)
+    Resque.enqueue(mailer, action, *args)
+  else
+    raise error
+  end
+}
