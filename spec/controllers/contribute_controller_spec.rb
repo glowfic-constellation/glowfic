ef294a61987abad927d7922e0dd1f5ce8994c8f9
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-01-08 22:22:21 -0500

    Contribute tests

---
+++ spec/controllers/contribute_controller_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+RSpec.describe ContributeController do
+  describe "GET index" do
+    it "succeeds when logged out" do
+      get :index
+      expect(response).to have_http_status(200)
+      expect(assigns(:page_title)).to eq("Contribute")
+    end
+
+    it "succeeds when logged in" do
+      login
+      get :index
+      expect(response).to have_http_status(200)
+      expect(assigns(:page_title)).to eq("Contribute")
+    end
+  end
+end
