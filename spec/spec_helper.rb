4549c99e0dd5252a049c59e4dcfb2dc1a942d466
Edward Jones <throne3d@gmail.com>
2017-01-14 17:54:07 +0000

    Fix time matching (convert to UTC before checking)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -110,15 +110,19 @@ end
 
 RSpec::Matchers.define :be_the_same_time_as do |expected|
   match do |actual|
-    expected.to_s(:db) == actual.to_s(:db)
+    expected.utc.to_s(:iso8601) == actual.utc.to_s(:iso8601)
   end
 
   failure_message do |actual|
-    "expected #{actual} to be the same time as #{expected}"
+    "expected #{actual} to be the same time as #{expected}\n" +
+    "actual.utc.to_s(:iso8601): #{actual.utc.to_s(:iso8601)}\n" +
+    "expected.utc.to_s(:iso8601): #{expected.utc.to_s(:iso8601)}"
   end
 
   failure_message_when_negated do |actual|
-    "expected #{actual} not to be the same time as #{expected}"
+    "expected #{actual} not to be the same time as #{expected}\n" +
+    "actual.utc.to_s(:iso8601): #{actual.utc.to_s(:iso8601)}\n" +
+    "expected.utc.to_s(:iso8601): #{expected.utc.to_s(:iso8601)}"
   end
 end
 
