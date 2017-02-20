2c1b1ae4af4b6214500a5244a8a32cc977385048
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-02-20 18:16:17 -0500

    template.characters will default to using name sort

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,15 +1,14 @@
 class Template < ActiveRecord::Base
   belongs_to :user, inverse_of: :templates
-  has_many :characters
-  has_many :ordered_characters, order: 'LOWER(name)', class_name: Character
+  has_many :characters, order: 'LOWER(name) ASC'
 
   validates_presence_of :name
 
-  before_destroy :clear_character_templates
+  after_destroy :clear_character_templates
 
   private
 
   def clear_character_templates
-    characters.update_all(template_id: nil)
+    Character.where(template_id: id).update_all(template_id: nil)
   end
 end
