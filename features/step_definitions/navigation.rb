abe04ba2e1b0240aaff8cf1a79709197cb251b82
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-02-26 19:04:46 -0800

    Some message view tests

--- features/step_definitions/navigation.rb
+++ features/step_definitions/navigation.rb
@@ -6,8 +6,28 @@ Given(/^I am logged in$/) do
   click_button "Log in"
 end
 
+Given(/^I have a message thread$/) do
+  first = create(:message, sender: User.last)
+  second = create(:message, thread_id: first.id, sender: first.recipient, recipient: first.sender, parent: first)
+  last = create(:message, thread_id: first.id, sender: first.sender, recipient: first.recipient, parent: second)
+end
+
+Given(/^I have a long message$/) do
+  create(:message, sender: User.last, message: "abcde" * 22)
+end
+
 When(/^I start a new post$/) do
   visit new_post_path
+end
+
+When(/^I go to my (in|out)box$/) do |box|
+  visit messages_path(view: box + "box")
+end
+
+When(/^I open the message$/) do
+  within("#content") do
+    click_link Message.last.unempty_subject
+  end
 end
 
 Then(/^I should see "(.*)"$/) do |content|
@@ -16,4 +36,15 @@ end
 
 Then(/^I should not see "(.*)"$/) do |content|
   expect(page).not_to have_content(content)
+end
+
+Then(/^I should see (\d) messages$/) do |num|
+  expect(page).to have_selector('.message-collapse', count: num)
+end
+
+Then(/^I should see the shortened message$/) do
+  within(".message-collapse") do
+    expect(page).to have_content("abcde" * 15 + "...")
+    expect(page).not_to have_content("abcde" * 20)
+  end
 end
