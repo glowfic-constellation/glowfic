bfffef7053e19446d7dacab16fb04d82cb68ea3f
Edward Jones <throne3d@gmail.com>
2017-05-05 18:44:27 +0100

    Set character default icons to default by gallery if nil
    - Migrate to set characters' default icons to gallery default
      if nil (removes extra logic, reduces database queries)
    - Remove the backup for default icons so people can have
      characters with galleries that do not have a default
      icon

---
+++ db/migrate/20170505173455_set_character_default_icons.rb
@@ -0,0 +1,13 @@
+class SetCharacterDefaultIcons < ActiveRecord::Migration
+  def up
+    Character.where(default_icon_id: nil).includes(:galleries).find_each do |char|
+      next if char.galleries.blank?
+      char.default_icon = char.galleries.detect(&:default_icon).try(:default_icon)
+      char.save!
+    end
+  end
+
+  def down
+    raise(ActiveRecord::IrreversibleMigration, "Cannot undo setting character default icons")
+  end
+end
