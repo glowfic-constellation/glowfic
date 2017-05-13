8df788089b86afdaf3554be9ecc97e1e4d4398b2
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-05-13 00:36:05 -0400

    Make add existing icons dynamically load icons

--- app/assets/javascripts/galleries/add_existing.js
+++ app/assets/javascripts/galleries/add_existing.js
@@ -1,14 +1,7 @@
 image_ids = []
 skip_warning = false;
 $(document).ready( function() {
-  $(".add-gallery-icon").click(function() {
-    $(this).toggleClass('selected-icon');
-    if ($(this).hasClass('selected-icon')) {
-      image_ids.push(this.dataset['id']);
-    } else {
-      image_ids.pop(this.dataset['id']);
-    }
-  });
+  bindGalleryIcons(".add-gallery-icon");
 
   $("#add-gallery-icons").submit(function() {
     if (image_ids.length < 1) { return false; }
@@ -16,9 +9,47 @@ $(document).ready( function() {
     skip_warning = true;
     return true;
   });
+
+  $(".gallery-minmax").click(function() {
+    var gallery_id = $(this).data('id');
+
+    // Hide icons if they're already visible
+    if ($("#icons-" + gallery_id).is(':visible')) {
+      $("#icons-" + gallery_id).hide();
+      $("#minmax-" + gallery_id).text("+");
+      return true;
+    }
+
+    // Show icons if they're hidden
+    $("#icons-" + gallery_id).show();
+    $("#minmax-" + gallery_id).text("-");
+    if($("#icons-" + gallery_id).html().length > 0) { return true; }
+
+    // Load and bind icons if they have not already been loaded
+    $.get("/api/v1/galleries/" + gallery_id, {}, function(resp) {
+      $.each(resp.icons, function(index, icon) {
+        var icon_div = $("<div>").attr({class: 'gallery-icon'});
+        var icon_img = $("<img>").attr({src: icon.url, id: icon.id, alt: icon.keyword, title: icon.keyword, 'class': 'icon add-gallery-icon'});
+        icon_div.append(icon_img).append("<br>").append($("<span>").attr({class: 'icon-keyword'}).append(icon.keyword));
+        $("#icons-" + gallery_id).append(icon_div);
+      });
+      bindGalleryIcons("#icons-" + gallery_id + " .add-gallery-icon");
+    });
+  });
 });
 
 $(window).on('beforeunload', function(){
   if (skip_warning || image_ids.length === 0) return;
   return "Are you sure you wish to navigate away? You have " + image_ids.length + " image(s) selected.";
 });
+
+bindGalleryIcons = function(css_selector) {
+  $(css_selector).click(function() {
+    $(this).toggleClass('selected-icon');
+    if ($(this).hasClass('selected-icon')) {
+      image_ids.push(this.dataset['id']);
+    } else {
+      image_ids.pop(this.dataset['id']);
+    }
+  });
+}
