18134a5c0268520a74c6b6d6027d837e4cd692c4
Edward Jones <throne3d@gmail.com>
2017-05-24 18:37:31 +0100

    Use has_many :through relation for galleries and icons
    (Fix recognizing galleries_icons relation with protected attributes)

--- app/models/galleries_icon.rb
+++ app/models/galleries_icon.rb
@@ -3,7 +3,12 @@ class GalleriesIcon < ActiveRecord::Base
   belongs_to :gallery
   accepts_nested_attributes_for :icon, allow_destroy: true
 
+  after_create :set_has_gallery
   after_destroy :unset_has_gallery
+
+  def set_has_gallery
+    icon.update_attributes(has_gallery: true)
+  end
 
   def unset_has_gallery
     return if icon.galleries.present?
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,8 +1,9 @@
 class Gallery < ActiveRecord::Base
   belongs_to :user
   belongs_to :cover_icon, class_name: Icon
+
   has_many :galleries_icons
-  has_and_belongs_to_many :icons, -> { order('LOWER(keyword)') }, after_add: :set_has_gallery, after_remove: :unset_has_gallery
+  has_many :icons, -> { order('LOWER(keyword)') }, through: :galleries_icons
 
   has_many :characters_galleries
   has_many :characters, through: :characters_galleries
@@ -19,17 +20,5 @@ class Gallery < ActiveRecord::Base
 
   def character_gallery_for(character)
     characters_galleries.where(character_id: character).first
-  end
-
-  private
-
-  def set_has_gallery(icon)
-    return unless valid? # don't change icon status unless the gallery being saved is valid
-    icon.update_attributes(has_gallery: true)
-  end
-
-  def unset_has_gallery(icon)
-    return if icon.galleries.present?
-    icon.update_attributes(has_gallery: false)
   end
 end
