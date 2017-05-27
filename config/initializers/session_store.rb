710f19c0877f604f54d29ba5aef63a577a0e9e13
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-05-26 22:09:06 -0400

    Revert "Don't pass options to the cookie, see if this helps"
    This reverts commit c691a2ea1af0ddf07c9667b6032ab6cd4d096a1e.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-options = {}#Rails.env.production? ? {domain: 'glowfic.com', tld_length: 2} : {}
+options = Rails.env.production? ? {domain: 'glowfic.com', tld_length: 2} : {}
 Rails.application.config.session_store :cookie_store, key: '_glowfic_session', **options
