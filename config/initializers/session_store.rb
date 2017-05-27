8bcc89c30b804c745ee9fc632258aa0499fc5ac6
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-05-26 21:36:40 -0400

    Revert "Revert "Cookies across domains""
    This reverts commit 0086010963c11ea889dc0738533014ed6bfc6225.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_glowfic_session'
+options = Rails.env.production? ? {domain: 'glowfic.com', tld_length: 2} : {}
+Rails.application.config.session_store :cookie_store, key: '_glowfic_session', **options
