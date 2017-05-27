88cb280e55fb3351d4a62454b9aa7809e4f96c19
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-05-26 23:20:41 -0400

    Try renaming cookies...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-options = Rails.env.production? ? {domain: 'glowfic.com', tld_length: 2} : {}
-Rails.application.config.session_store :cookie_store, key: '_glowfic_session'
+Rails.application.config.session_store :cookie_store, key: '_glowfic_session_' + Rails.env
