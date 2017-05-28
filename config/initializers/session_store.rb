7d6281c16d7767ba4a2ffc4e9038ae2d1112bc18
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-05-28 01:34:03 -0400

    Multi domain again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_glowfic_session_' + Rails.env
+options = if Rails.env.production?
+  {domain: 'glowfic.com', tld_length: 2}
+elsif Rails.env.development?
+  {domain: 'localhost', tld_length: 2}
+else
+  {}
+end
+
+Rails.application.config.session_store :cookie_store, key: '_glowfic_constellation_' + Rails.env, **options
