6da211e1c07a10b176ec371cc7e12642e45f7d63
Edward Jones <throne3d@gmail.com>
2017-08-26 15:01:27 +0100

    Reorder rows according to server response, add code comments

--- app/assets/javascripts/board_sections.js
+++ app/assets/javascripts/board_sections.js
@@ -32,7 +32,7 @@ function unbindArrows(orderBox) {
 
 function reorderRows(orderBox) {
   var arrowBox = $('tbody', orderBox);
-  var rows = $('.section-ordered', orderBox);
+  var rows = $('.section-ordered', arrowBox);
   var ordered = rows.sort(function(a, b) { return $(a).data('order') > $(b).data('order') ? 1 : -1; }).appendTo(arrowBox);
   $("tr:even:not(.section-warning) td", orderBox).removeClass('even').addClass('odd');
   $("tr:odd:not(.section-warning) td", orderBox).removeClass('odd').addClass('even');
@@ -40,41 +40,56 @@ function reorderRows(orderBox) {
 }
 
 function moveRow(sourceRow, targetRow, orderBox, path, param) {
-  unbindArrows(orderBox);
   // Reduce race conditions by only allowing one update at a time
+  unbindArrows(orderBox);
   $("#loading", orderBox).show();
   $("#saveconf", orderBox).stop(true, true).hide();
 
+  // Switch the row order pre-emptively
   var sourceOrder = sourceRow.data('order');
   var targetOrder = targetRow.data('order');
   sourceRow.data('order', targetOrder);
   targetRow.data('order', sourceOrder);
-
   var orderedRows = reorderRows(orderBox);
 
+  // Figure out the full desired order and send it to the server
   var orderedIds = [];
   orderedRows.each(function() {
     orderedIds.push(parseInt($(this).data('id')));
   });
   var json = {};
   json['ordered_' + param] = orderedIds;
+  // and restrict to relevant section_id if given
   if (window.gon && window.gon.section_id) json.section_id = window.gon.section_id;
 
   $.post(path, json, function(resp) {
-    var rows = $('.section-ordered', orderBox);
-    if (rows.length !== resp[param].length && $('.section-warning', orderBox).length === 0) {
+    // Check the list doesn't have new elements
+    if (orderedRows.length !== resp[param].length && $('.section-warning', orderBox).length === 0) {
       var warning = $("<tr class='section-warning'>").append($("<td>").html('There are items missing from this list! Please reload.'));
       orderBox.prepend(warning);
+      console.log(resp.responseText);
     }
+
+    // Set the full ordering according to the server response
+    var returnedIds = resp[param];
+    orderedRows.each(function() {
+      var row = $(this);
+      row.data('order', returnedIds.indexOf(row.data('id')));
+    });
+    reorderRows(orderBox);
+
+    // Re-enable the buttons
     $("#loading", orderBox).hide();
     $("#saveconf", orderBox).show().delay(2000).fadeOut();
     bindArrows(orderBox, path, param);
-  }).fail(function(data) {
+  }).fail(function(resp) {
+    // Display an error and debug to console
     $("#loading", orderBox).hide();
     $("#saveerror", orderBox).show();
     if ($('.section-warning', orderBox).length === 0) {
       var warning = $("<tr class='section-warning'>").append($("<td>").html('There was an error saving your changes! Please reload.'));
       orderBox.prepend(warning);
+      console.log(resp.responseText);
     }
   });
 }
