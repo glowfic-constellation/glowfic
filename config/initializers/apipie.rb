ebb76b2e67020ad5ba5b0c6c1385d54e0aa33a0e
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-09-01 19:11:21 +0200

    Apipie upgrade broke our docs page, because our files were generated with no locale and the latest version has the fix https://github.com/Apipie/apipie-rails/pull/543 merged in, which now correctly defaults locale from the previous 'no idea use nil' to 'use the default_locale of the app', so it would try to find .en files and they did not exist. This will set our language to nil, so it looks for files without the .en, which do exist. Long term fix is to properly generate localized files, but it's not immediately clear to me how to do that.

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,5 +3,6 @@ Apipie.configure do |config|
   config.api_base_url            = "/api/v1"
   config.doc_base_url            = "/docs"
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
-  config.show_all_examples = true
+  config.show_all_examples       = true
+  config.default_locale          = nil
 end
