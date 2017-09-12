94470683a5943d095a6ef81a1a2bb4f4f91e8cbf
Edward Jones <throne3d@gmail.com>
2017-09-12 02:03:38 +0200

    Set view uniqueness at a database level

---
+++ db/migrate/20170911235423_make_views_unique.rb
@@ -0,0 +1,9 @@
+class MakeViewsUnique < ActiveRecord::Migration
+  def change
+    remove_index :board_views, column: [:user_id, :board_id]
+    add_index :board_views, [:user_id, :board_id], unique: true
+
+    remove_index :post_views, column: [:user_id, :post_id]
+    add_index :post_views, [:user_id, :post_id], unique: true
+  end
+end
--- spec/models/board_view_spec.rb
+++ spec/models/board_view_spec.rb
@@ -30,6 +30,9 @@ RSpec.describe BoardView do
       new_view = build(:board_view, user: view.user, board: view.board)
       expect(new_view).not_to be_valid
       expect(new_view.save).to eq(false)
+      expect {
+        new_view.save!(validate: false)
+      }.to raise_error(ActiveRecord::RecordNotUnique)
     end
 
     it "allows one user to have multiple board views" do
