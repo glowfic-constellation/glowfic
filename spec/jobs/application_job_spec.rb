f47d375d30531262abfb76dc422eee7dae5b8a46
Edward Jones <throne3d@gmail.com>
2017-09-22 19:34:11 +0100

    Test that it retries jobs on error

--- spec/jobs/application_job_spec.rb
+++ spec/jobs/application_job_spec.rb
@@ -17,6 +17,12 @@ RSpec.describe ApplicationJob do
     expect(StubJob).to have_been_enqueued.with(1).on_queue('high')
   end
 
+  it "retries on error" do
+    expect_any_instance_of(StubJob).to receive(:perform).and_raise(StandardError)
+    StubJob.perform_now(1)
+    expect(StubJob).to have_been_enqueued.with(1).on_queue('high')
+  end
+
   it "sends email when retry gives up" do
     exc = Exception.new
     expect(ExceptionNotifier).to receive(:notify_exception).with(exc, data: {job: StubJob.name, args: [2, :test]})
