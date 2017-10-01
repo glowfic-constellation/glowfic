78e43607598e8a6911e9ce7572621f09edc7d59b
Edward Jones <throne3d@gmail.com>
2017-10-01 01:11:16 +0100

    Hide paginator links as screen narrows
    - Use requestAnimationFrame instead of timeout for responsiveness
    
    - Calculate visible links using will_paginate logic
    
    - Do not duplicate ellipses

--- app/assets/javascripts/posts/show.js
+++ app/assets/javascripts/posts/show.js
@@ -54,10 +54,10 @@ $(document).ready(function() {
   paginators.each(function() { reflowPaginator(this); });
   var resizeDebounce = null;
   $(window).resize(function() {
-    clearTimeout(resizeDebounce);
-    resizeDebounce = setTimeout(function() {
+    window.cancelAnimationFrame(resizeDebounce);
+    resizeDebounce = window.requestAnimationFrame(function() {
       paginators.each(function() { reflowPaginator(this); });
-    }, 100);
+    });
   });
 
   // Now that we've finished the scripts that change page locations, scroll to #unread
@@ -71,7 +71,93 @@ function reflowPaginator(paginator) {
   var narrowClear = paginator.find('.narrow-clear');
 
   narrowClear.css('clear', 'none');
+
+  var innerWindow = 4;
+  var outerBoundary = 2;
+  hidePaginatorLinks(paginator, innerWindow, outerBoundary);
   if (paginator.height() < 60) return;
 
   narrowClear.css('clear', 'both');
+
+  if (paginator.height() < 100) return;
+  while (paginator.height() >= 100 && innerWindow > 0) {
+    hidePaginatorLinks(paginator, innerWindow, outerBoundary);
+    innerWindow -= 1;
+  }
+}
+
+function pageAsNumber(page) {
+  return parseInt(page.html().trim(), 10);
+}
+
+function newInsertedEllipsis() {
+  return $('<span class="gap inserted-ellipsis">').append('…');
+}
+
+function range(lower, upper) {
+  // [lower .. upper] (inclusive)
+  var list = [];
+  for (var i=lower; i<=upper; i++) {
+    list.push(i);
+  }
+  return list;
+}
+
+function calculateVisiblePages(innerWindow, outerBoundary, totalPages, currentNum) {
+  var windowFrom = currentNum - innerWindow;
+  var windowTo = currentNum + innerWindow;
+  if (windowTo > totalPages) {
+    windowFrom -= windowTo - totalPages;
+    windowTo = totalPages;
+  }
+  if (windowFrom < 1) {
+    windowTo += 1 - windowFrom;
+    windowFrom = 1;
+    if (windowTo > totalPages) windowTo = totalPages;
+  }
+
+  var middle = range(windowFrom, windowTo);
+
+  var left;
+  if (outerBoundary + 3 < middle[0]) {
+    left = range(1, outerBoundary+1);
+    left.push('gap');
+  } else {
+    left = range(1, middle[0]);
+  }
+
+  var right;
+  if (totalPages - outerBoundary - 2 > middle[middle.length-1]) {
+    right = range(totalPages - outerBoundary, totalPages);
+  } else {
+    right = range(middle[middle.length-1] + 1, totalPages);
+    right.unshift('gap');
+  }
+
+  return left.concat(middle).concat(right);
+}
+
+function hidePaginatorLinks(paginator, innerWindow, outerBoundary) {
+  paginator.find('.inserted-ellipsis').remove();
+  var links = paginator.find('a:not(.previous_page):not(.next_page)');
+  links.show();
+
+  // logic for page numbers from will_paginate
+  var lastLink = paginator.find('.next_page').prev('a,.current');
+  var current = paginator.find('.current');
+  var currentNum = pageAsNumber(current);
+  var totalPages = pageAsNumber(lastLink);
+  var pages = calculateVisiblePages(innerWindow, outerBoundary, totalPages, currentNum);
+
+  var linksToHide = links;
+  links.each(function() {
+    var link = $(this);
+    var num = pageAsNumber(link);
+    var index = pages.indexOf(num);
+    if (index < 0) return;
+    linksToHide = linksToHide.not(link);
+    if (pages[index + 1] === 'gap' && !link.next('.gap')) link.after(newInsertedEllipsis());
+  });
+
+  linksToHide.hide();
 }
