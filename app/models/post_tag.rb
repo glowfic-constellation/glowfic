b20e8e3963038d7a7f478162c79f786184e672cd
Edward Jones <throne3d@gmail.com>
2017-11-18 20:25:59 +0000

    Make PostTag not have a user

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,6 +1,5 @@
 class PostTag < ApplicationRecord
   belongs_to :post, inverse_of: :post_tags, optional: false
-  belongs_to :user, optional: false
   belongs_to :tag, optional: false
   belongs_to :setting, foreign_key: :tag_id, optional: true
   belongs_to :content_warning, foreign_key: :tag_id, optional: true
