cbef3396b1986f7cb50cd39f457d45886eb970ce
Marri Nikari <xxstealthestarsxx@gmail.com>
2017-11-24 15:44:12 -0500

    Use AWS SES for email sending instead of SMTP which is glitchy

--- config/initializers/gmail_smtp.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['GMAIL_USERNAME'],
-  :password             => ENV['GMAIL_PASSWORD'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
