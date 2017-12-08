0a6db7235a89bf18a30fdcb695e3d93fd4972eb3
Edward Jones <throne3d@gmail.com>
2017-12-08 19:17:55 -0200

    Fix references to changed attributes in after_save callbacks
    Per deprecation warnings:
    
    DEPRECATION WARNING: The behavior of `attribute_changed?` inside of
    after callbacks will be changing in the next version of Rails. The new
    return value will reflect the behavior of calling the method after
    `save` returned (e.g. the opposite of what it returns now). To maintain
    the current behavior, use `saved_change_to_attribute?` instead. (called
    from block in order_change? at
    /media/throne3d/Storage/Documents/glowfic/app/concerns/orderable.rb:37)
    
    DEPRECATION WARNING: The behavior of `attribute_was` inside of after
    callbacks will be changing in the next version of Rails. The new return
    value will reflect the behavior of calling the method after `save`
    returned (e.g. the opposite of what it returns now). To maintain the
    current behavior, use `attribute_before_last_save` instead. (called from
    reorder_others at
    /media/throne3d/Storage/Documents/glowfic/app/concerns/orderable.rb:15)
    
    Unfortunately, some place(s?) we use a single method in both after and
    before callbacks – Orderable's order_change?, which is called from
    `autofill_order` (`before_save`) and `reorder_others` (`after_save`,
    `after_destroy`).
    
    In addition, `after_destroy` seems to use the before syntax, since it
    hasn't saved a modification to the value.
    
    The Icon deprecation warning hasn't been fixed because it seems to be
    outputting during a *before* callback (when I broke up the after_destroy
    and before_update routes to it) – this seems to happen when a
    GalleriesIcon is created, and in its after_create callback it updates an
    icon's has_gallery status. Since this is *actually* in the Icon's before
    callback, and it still outputs the warning, I think the deprecation
    notice is broken.
    
    This probably needs more looking at.

--- app/concerns/orderable.rb
+++ app/concerns/orderable.rb
@@ -3,20 +3,23 @@ module Orderable
 
   included do
     before_save :autofill_order
-    after_save :reorder_others
-    after_destroy :reorder_others
+    after_save :reorder_others_after
+    after_destroy :reorder_others_before
 
     private
 
-    def reorder_others
-      return unless destroyed? || order_change?
+    def reorder_others(is_after)
+      return unless destroyed? || order_change?(is_after)
 
       if has_attribute?(:board_id) # all indexes are ordered
-        board_checking = Board.find_by_id(board_id_was) || board
+        board_checking_id = is_after ? board_id_before_last_save : board_id_was
+        board_checking = Board.find_by_id(board_checking_id) || board
         return unless board_checking.ordered?
       end
 
-      other_where = Hash[ordered_attributes.map { |atr| [atr, send("#{atr}_was")] }]
+      other_where = Hash[ordered_attributes.map do |atr|
+        [atr, send(is_after ? "#{atr}_before_last_save" : "#{atr}_was")]
+      end]
       others = self.class.where(other_where).order('section_order asc')
       return unless others.present?
 
@@ -27,14 +30,25 @@ module Orderable
       end
     end
 
+    def reorder_others_after
+      reorder_others(true)
+    end
+
+    def reorder_others_before
+      reorder_others(false)
+    end
+
     def autofill_order
-      return unless new_record? || order_change?
+      return unless new_record? || order_change?(false)
       self.section_order = ordered_items.count
     end
 
-    def order_change?
+    def order_change?(is_after)
       return if new_record? # otherwise we will reorder on create
-      ordered_attributes.any? { |atr| send("#{atr}_changed?") }
+      ordered_attributes.any? do |atr|
+        method = is_after ? "saved_change_to_#{atr}?" : "#{atr}_changed?"
+        send(method)
+      end
     end
 
     def ordered_items
