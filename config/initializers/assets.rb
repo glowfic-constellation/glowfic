130a4d2933b1e768092d8e11d110e05a94d46148
Edward Jones <throne3d@gmail.com>
2017-12-08 17:29:55 -0200

    Run rails app:update
    Mostly minor documentation changes.
    
    - Creates a `bin/yarn` file because Rails now includes yarn
      support
    - Loads defaults for Rails 5.1
    - Adds a channel_prefix to ActionCable (doesn't affect us
      since we don't use that yet)
    - (Manually) add a comment about what `include_root_in_json`
      does
    - Modifies Cache-Control parameters to interpolate meaningful
      values
    - Enables async file watcher in development environment
    - Enables reading new encrypted secrets
    - Includes `node_modules` in asset paths for if we use Yarn
    - Uses the `json` cookie serializer instead of `hybrid`
      (`hybrid` was for the old cookie update from back around
      Rails 4)
    - Enables old defaults for new Rails 5.1 changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,13 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
+# Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
 Rails.application.config.assets.precompile += %w( posts/*.js users/*.js boards/*.js characters/*.js global.js paginator.js galleries/*.js messages.js icons.js board_sections.js icons.js reorder.js tags/*.js )
 Rails.application.config.assets.precompile += %w( layouts/*.css tinymce.css )
