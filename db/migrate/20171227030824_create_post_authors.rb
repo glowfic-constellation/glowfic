e9fe4c809919dd931181d59ff609f25bb561f502
Edward Jones <throne3d@gmail.com>
2017-12-28 21:36:50 +0000

    Fix migration after change of author_ids

--- db/migrate/20171227030824_create_post_authors.rb
+++ db/migrate/20171227030824_create_post_authors.rb
@@ -13,11 +13,11 @@ class CreatePostAuthors < ActiveRecord::Migration[5.0]
     add_index :post_authors, :post_id
     add_index :post_authors, :user_id
 
-    Post.select('posts.*').with_author_ids.find_each do |post|
+    Post.select('posts.*').select('ARRAY(SELECT posts.user_id UNION SELECT replies.user_id FROM replies WHERE replies.post_id = posts.id GROUP BY replies.user_id) AS found_author_ids').find_each do |post|
       first_items = Reply.where(id: Reply.select('MIN(id)').where(post_id: post.id).group(:user_id)).index_by(&:user_id)
       first_items[post.user_id] = post
 
-      post.author_ids.each do |author_id|
+      post.found_author_ids.each do |author_id|
         first_item = first_items[author_id]
         PostAuthor.create!(post_id: post.id, user_id: author_id, can_owe: true, joined: true, joined_at: first_item.created_at)
       end
