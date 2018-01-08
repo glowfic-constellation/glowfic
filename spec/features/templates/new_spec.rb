1e2774e7de98a387e2c6ae5777380c6fa48bfdf1
Edward Jones <throne3d@gmail.com>
2018-01-08 20:11:05 +0000

    Test character persistence on template#create failure

---
+++ spec/features/templates/new_spec.rb
@@ -0,0 +1,106 @@
+require "spec_helper"
+
+RSpec.feature "Creating a new template", :type => :feature do
+  scenario "Create an invalid template" do
+    # view new template form without being logged in
+    visit new_template_path
+    expect(page).to have_selector('.flash.error')
+    within('.flash.error') do
+      expect(page).to have_text("You must be logged in")
+    end
+
+    # view new template form with no characters
+    user = login
+    visit new_template_path
+    expect(page).to have_no_selector('.flash.error')
+    expect(page).to have_no_text("You must be logged in")
+    expect(page).to have_selector("th", text: "New Template")
+    expect(page).to have_no_selector("th", text: "Characters")
+
+    # view new template form with no untemplated characters
+    create(:template_character, user: user)
+    visit new_template_path
+    expect(page).to have_no_selector('.flash.error')
+    expect(page).to have_no_selector("th", text: "Characters")
+
+    # view new template form with untemplated characters
+    create(:character, user: user)
+    visit new_template_path
+    expect(page).to have_no_selector('.flash.error')
+    expect(page).to have_selector("th", text: "Characters")
+
+    # create template with no data
+    within('.form-table') do
+      click_button 'Save'
+    end
+    expect(page).to have_selector('.flash.error')
+    within('.flash.error') do
+      expect(page).to have_text('Your template could not be saved.')
+      # TODO: more specific error messages
+    end
+  end
+
+  scenario "Create a simple template" do
+    login
+    visit new_template_path
+    expect(page).to have_no_selector('.flash.error')
+    within('.form-table') do
+      fill_in 'Template Name', with: 'Example template'
+      click_button 'Save'
+    end
+    expect(page).to have_no_selector('.flash.error')
+    expect(page).to have_selector('.flash.success')
+    within('.flash.success') do
+      expect(page).to have_text('Template saved successfully.')
+    end
+  end
+
+  scenario "Create template with description and characters" do
+    user = login
+    other_template = create(:template, user: user)
+    other_template_character = create(:character, user: user, template: other_template)
+    characters = Array.new(5) { |i| create(:character, user: user, name: "Character#{i+1}") }
+
+    # create invalid template with characters and description
+    visit new_template_path
+    within('.form-table') do
+      expect(page).to have_field('Template Name')
+      fill_in 'template_description', with: 'Example template description'
+      characters_row = find('tr') { |x| x.has_selector?('th', text: 'Characters') }
+      within(characters_row) do
+        check('Character1')
+        check('Character5')
+      end
+      click_button 'Save'
+    end
+
+    expect(page).to have_selector('.flash.error')
+    within('.flash.error') do
+      expect(page).to have_text('Your template could not be saved.')
+      # TODO: more specific error messages
+    end
+    within('.form-table') do
+      expect(page).to have_field('Template Name', with: '')
+      expect(page).to have_field('template_description', with: 'Example template description')
+      characters_row = find('tr') { |x| x.has_selector?('th', text: 'Characters') }
+      within(characters_row) do
+        expect(page).to have_checked_field('Character1')
+        expect(page).to have_unchecked_field('Character2')
+        expect(page).to have_unchecked_field('Character3')
+        expect(page).to have_unchecked_field('Character4')
+        expect(page).to have_checked_field('Character5')
+      end
+    end
+
+    # save valid template with description and characters
+    within('.form-table') do
+      fill_in 'Template Name', with: 'Example template'
+      click_button 'Save'
+    end
+    expect(page).to have_no_selector('.flash.error')
+    expect(page).to have_selector('.flash.success')
+    within('.flash.success') do
+      expect(page).to have_text('Template saved successfully.')
+    end
+  end
+end
