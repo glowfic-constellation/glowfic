66b331beadb4eb2f66943845472afd591477d705
Teceler <19716939+teceler@users.noreply.github.com>
2018-01-15 23:20:29 -0600

    Remove whitespace on newlines Fix Layout/TrailingWhitespace

--- spec/features/boards/show_spec.rb
+++ spec/features/boards/show_spec.rb
@@ -4,22 +4,22 @@ RSpec.feature "Show a single continuity", :type => :feature do
   scenario "View a standard continuity" do
     board = create(:board, name: "Test board")
     5.times do create(:post, board: board, user: board.creator) end
-    
+
     visit board_path(board)
-    
+
     expect(page).to have_text("Test board")
     expect(page).to have_selector('.post-subject', count: 5)
   end
-  
+
   scenario "View a continuity with many authors in a post" do
     board = create(:board, name: "Author board")
     post1 = create(:post, board: board, user: board.creator)
     reply = create(:reply, post: post1)
     post2 = create(:post, board: board, user: board.creator)
     4.times do create(:reply, post: post2) end
-    
+
     visit board_path(board)
-    
+
     expect(page).to have_text("Author board")
     expect(page).to have_selector('.post-subject', count: 2)
     within("tbody tr:nth-child(2)") do
