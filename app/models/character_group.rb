f09cf901fad54d2dd264f7ba22c9d45c28aa9b53
Teceler <19716939+teceler@users.noreply.github.com>
2018-02-28 15:52:44 -0600

    Nullify character-group entry for characters and templates on character-group destruction

--- app/models/character_group.rb
+++ app/models/character_group.rb
@@ -1,6 +1,6 @@
 class CharacterGroup < ApplicationRecord
-  has_many :characters
-  has_many :templates
+  has_many :characters, dependent: :nullify
+  has_many :templates, dependent: :nullify
   belongs_to :user, optional: false
   validates :name, presence: true
 end
