231812959d15f34c112ac73342b07436e5a3382d
Teceler <19716939+teceler@users.noreply.github.com>
2018-03-13 13:49:36 -0500

    consdense script

--- id_refactor.rb
+++ id_refactor.rb
@@ -1,3 +1,13 @@
+arrangements = {
+  Template: {
+    Character: :template_id,
+  },
+  Icon: {
+    Character: :default_icon_id,
+    GalleriesIcon: :icon_id
+  },
+}
+
 def update_models(model, key, old_id, new_id)
   model.where(key => old_id).find_each do |object|
     object.update(key => new_id)
@@ -11,62 +21,27 @@ def copy_object(object, exp_id)
   copy.id
 end
 
-Template.transaction do
-  puts "Reassigning templates..."
-  Template.order(:id).all.each_with_index do |template, i|
-    exp_id = i + 1
-    old_id = template.id
-    puts "\tOld id: #{old_id}, Expected new id: #{exp_id}"
-    next if old_id == exp_id
-    new_id = copy_object(template, exp_id)
-    update_models(Character, :template_id, old_id, new_id)
-    template.destroy!
+def iterate_model(model)
+  model.transaction do
+    puts "Reassigning #{model.pluralize}..."
+    model.order(:id).all.each_with_index do |object, i|
+      exp_id = i + 1
+      old_id = object.id
+      puts "\tOld id: #{old_id}, Expected new id: #{exp_id}"
+      next if old_id == exp_id
+      new_id = copy_object(object, exp_id)
+      arrangements[model].for_each do |key, value|
+        update_models(key, value, old_id, new_id)
+      end
+      object.destroy!
+    end
   end
 end
 
-Icon.transaction do
-  puts "Reassigning icons..."
-  Icon.order(:id).all.each_with_index do |icon, i|
-    exp_id = i + 1
-    old_id = icon.id
-    puts "\tOld id: #{old_id}, Expected new id: #{exp_id}"
-    next if old_id == exp_id
-    new_id = copy_object(icon, exp_id)
-    update_models(Character, :default_icon_id, old_id, new_id)
-    update_models(GalleriesIcon, :icon_id, old_id, new_id)
-    icon.destroy!
-  end
-end
-
-Character.transaction do
-  puts "Reassigning characters..."
-  Character.order(:id).all.each_with_index do |char, i|
-    exp_id = i + 1
-    old_id = char.id
-    puts "\tOld id: #{old_id}, Expected new id: #{exp_id}"
-    next if old_id == exp_id
-    new_id = copy_object(char, exp_id)
-    update_models(CharactersGallery, :character_id, old_id, new_id)
-    update_models(CharactersTag, :character_id, old_id, new_id)
-    char.destroy!
-  end
-end
-
-Gallery.transaction do
-  puts "Reassigning galleries..."
-  Gallery.order(:id).all.each_with_index do |gallery, i|
-    exp_id = i + 1
-    old_id = gallery.id
-    puts "\tOld id: #{old_id}, Expected new id: #{exp_id}"
-    next if old_id == exp_id
-    new_id = copy_object(gallery, exp_id)
-    update_models(CharactersGallery, :gallery_id, old_id, new_id)
-    update_models(GalleriesIcon, :gallery_id, old_id, new_id)
-    update_models(GalleryTag, :gallery_id, old_id, new_id)
-    gallery.destroy!
-  end
-end
-
+iterate_model(Template)
+iterate_model(Icon)
+iterate_model(Character)
+iterate_model(Gallery)
 Tag.transaction do
   puts "Reassigning tags..."
   Tag.for_each_with_index do |_n, i|
