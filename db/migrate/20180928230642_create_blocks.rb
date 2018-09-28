0a9b05ed373337e69248f1e0bd5c7a6f7916cae8
teceler <19716939+teceler@users.noreply.github.com>
2018-09-28 18:48:48 -0500

    Further improve migration

--- db/migrate/20180928230642_create_blocks.rb
+++ db/migrate/20180928230642_create_blocks.rb
@@ -1,8 +1,8 @@
 class CreateBlocks < ActiveRecord::Migration[5.1]
   def change
     create_table :blocks do |t|
-      t.integer :blocking_user, null: false
-      t.integer :blocked_user, null: false
+      t.integer :blocking_user_id, null: false
+      t.integer :blocked_user_id, null: false
       t.boolean :no_interact, default: true
       t.boolean :no_posts, default: false
       t.boolean :no_content, default: false
