200edc3aed9c8fe3c6fa340e3b5ad4d5b7d7ab85
teceler <19716939+teceler@users.noreply.github.com>
2018-09-28 19:09:02 -0500

    Add links with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
   has_one :report_view
   belongs_to :avatar, class_name: 'Icon', inverse_of: :user, optional: true
   belongs_to :active_character, class_name: 'Character', inverse_of: :user, optional: true
+  has_many :blocks, inverse_of: :blocking_user
 
   validates :crypted, presence: true
   validates :email,
