2a757628845c892019dc6bf9e10ad8bbb2eb7a76
teceler <19716939+teceler@users.noreply.github.com>
2018-09-29 11:24:28 -0500

    Use can_interact_with on checking provided recipient

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
     recipient = User.find_by_id(params[:recipient_id])
-    @message.recipient = recipient unless recipient && current_user.blocked_interaction.include?(recipient)
+    @message.recipient = recipient if recipient&.can_interact_with?(current_user)
     @page_title = 'Compose Message'
   end
 
