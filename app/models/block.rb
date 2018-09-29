fad5aeaf7939b71c115e5b97bd478f47fbeb5828
teceler <19716939+teceler@users.noreply.github.com>
2018-09-29 12:51:18 -0500

    Enforce uniqueness for a blocker and blockee

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,4 +1,6 @@
 class Block < ApplicationRecord
   belongs_to :blocking_user, class_name: 'User', optional: false, inverse_of: blocks
   belongs_to :blocked_user, class_name: 'User', optional: false
+
+  validates :blocking_user_id, uniqueness: { scope: :blocked_user_id }
 end
