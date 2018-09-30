b6c49bed21795c8e550cf07e81650c6542423583
teceler <19716939+teceler@users.noreply.github.com>
2018-09-29 23:20:08 -0500

    Rename columns (migration)

--- db/migrate/20180928230642_create_blocks.rb
+++ db/migrate/20180928230642_create_blocks.rb
@@ -3,10 +3,10 @@ class CreateBlocks < ActiveRecord::Migration[5.1]
     create_table :blocks do |t|
       t.integer :blocking_user_id, null: false
       t.integer :blocked_user_id, null: false
-      t.boolean :no_interact, default: true
-      t.boolean :no_posts, default: false
-      t.boolean :no_content, default: false
-      t.boolean :invisible, default: false
+      t.boolean :block_interactions, default: true
+      t.boolean :hide_their_posts, default: false
+      t.boolean :hide_their_content, default: false
+      t.boolean :hide_my_content, default: false
 
       t.timestamps
     end
