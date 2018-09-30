5a92b4a72c50344eac914cb6d248d1ea1b791397
teceler <19716939+teceler@users.noreply.github.com>
2018-09-30 00:08:53 -0500

    Prevent messaging blocked users

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,7 +17,8 @@ class MessagesController < ApplicationController
 
   def new
     @message = Message.new
-    @message.recipient = User.find_by_id(params[:recipient_id])
+    recipient = User.find_by_id(params[:recipient_id])
+    @message.recipient = recipient if recipient&.can_interact_with?(current_user)
     @page_title = 'Compose Message'
   end
 
@@ -115,8 +116,9 @@ class MessagesController < ApplicationController
     use_javascript('messages')
     unless @message.try(:parent)
       recent_ids = Message.where(sender_id: current_user.id).order(Arel.sql('MAX(id) desc')).limit(5).group(:recipient_id).pluck(:recipient_id)
-      recents = User.where(id: recent_ids).pluck(:username, :id).sort_by{|x| recent_ids.index(x[1]) }
-      users = User.where.not(id: current_user.id).ordered.pluck(:username, :id)
+      base_users = User.where.not(id: [current_user.id] + current_user.blocked_interaction)
+      recents = base_users.where(id: recent_ids).pluck(:username, :id).sort_by{|x| recent_ids.index(x[1]) }
+      users = base_users.ordered.pluck(:username, :id)
       @select_items = if recents.present?
         {:'Recently messaged' => recents, :'Other users' => users}
       else
