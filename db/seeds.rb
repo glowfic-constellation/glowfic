d5da2eb3e2fdbdb844935409d9cfdd1bc1acd65b
teceler <19716939+teceler@users.noreply.github.com>
2018-11-04 16:03:44 -0600

    Restore board sections

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,12 @@ Board.create!([
 ])
 
 puts "Creating sections..."
-load 'seeds/board_section.rb'
+BoardSection.create!([
+  { board_id: 1, name: "make a wish", status: 1, section_order: 0 },
+  { board_id: 1, name: "hexes", status: 1, section_order: 1 },
+  { board_id: 1, name: "parable of the talents", status: 1, section_order: 2 },
+  { board_id: 1, name: "golden opportunity", status: 0, section_order: 3 },
+])
 
 puts "Creating icons..."
 load 'seeds/icon.rb'
--- db/seeds/board_section.rb
+++
@@ -1,6 +0,0 @@
-BoardSection.create!([
-  { board_id: 1, name: "make a wish", status: 1, section_order: 0 },
-  { board_id: 1, name: "hexes", status: 1, section_order: 1 },
-  { board_id: 1, name: "parable of the talents", status: 1, section_order: 2 },
-  { board_id: 1, name: "golden opportunity", status: 0, section_order: 3 },
-])
