bfa43ebe6a3c11eddf12b8f586a0ed8b57c49fa0
Marri <xxstealthestarsxx@gmail.com>
2018-11-05 21:00:09 +1100

    Refactor @written variable away

--- spec/controllers/posts/create_spec.rb
+++ spec/controllers/posts/create_spec.rb
@@ -117,13 +117,12 @@ RSpec.describe PostsController, 'POST create' do
         },
       }
       expect(response).to render_template(:preview)
-      expect(assigns(:written)).to be_an_instance_of(Reply)
-      expect(assigns(:written)).to be_a_new_record
-      expect(assigns(:written).user).to eq(user)
-      expect(assigns(:written).character).to eq(templateless_character)
-      expect(assigns(:written).icon).to eq(icon)
-      expect(assigns(:written).character_alias).to eq(character_alias)
-      expect(assigns(:post).written).to eq(assigns(:written))
+      expect(assigns(:post).written).to be_an_instance_of(Reply)
+      expect(assigns(:post).written).to be_a_new_record
+      expect(assigns(:post).written.user).to eq(user)
+      expect(assigns(:post).written.character).to eq(templateless_character)
+      expect(assigns(:post).written.icon).to eq(icon)
+      expect(assigns(:post).written.character_alias).to eq(character_alias)
       expect(assigns(:page_title)).to eq('Previewing: test')
       expect(assigns(:author_ids)).to match_array([user.id, coauthor.id])
 
@@ -157,9 +156,9 @@ RSpec.describe PostsController, 'POST create' do
     it "does not crash without arguments" do
       post :create, params: { button_preview: true }
       expect(response).to render_template(:preview)
-      expect(assigns(:written)).to be_an_instance_of(Reply)
-      expect(assigns(:written)).to be_a_new_record
-      expect(assigns(:written).user).to eq(user)
+      expect(assigns(:post).written).to be_an_instance_of(Reply)
+      expect(assigns(:post).written).to be_a_new_record
+      expect(assigns(:post).written.user).to eq(user)
     end
 
     it "does not create authors or viewers" do
@@ -362,7 +361,7 @@ RSpec.describe PostsController, 'POST create' do
       expect(assigns(:post)).not_to be_persisted
       expect(assigns(:post).user).to eq(user)
       expect(assigns(:post).subject).to eq('asubjct')
-      expect(assigns(:written).content).to eq('acontnt')
+      expect(assigns(:post).written.content).to eq('acontnt')
       expect(assigns(:page_title)).to eq('New Post')
       expect(assigns(:author_ids)).to match_array([user.id, coauthor.id])
 
