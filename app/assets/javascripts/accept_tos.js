eb64142e123a3693a6fcdc51a020d2cbd13fe0a3
Marri <xxstealthestarsxx@gmail.com>
2018-11-13 06:58:38 +1000

    Update accept_tos.js
    Require tos div to exist before hiding.

--- app/assets/javascripts/accept_tos.js
+++ app/assets/javascripts/accept_tos.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   // Use hide/show for anonymous users rather than removing content
   // server-side improves SEO as search bots can still scrape content.
-  $("#content").hide();
-  $("#tos").show();
+  if($("#tos").length > 0) {
+    $("#content").hide();
+    $("#tos").show();
+  }
 });
