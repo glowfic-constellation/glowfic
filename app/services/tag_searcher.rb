0a0c02daa70ff1114a90fecb058653947d26c5a2
Marri <xxstealthestarsxx@gmail.com>
2018-11-13 17:05:46 +1000

    Tag search with bonus more-comprehensive JavaScript asset precompilation

---
+++ app/services/tag_searcher.rb
@@ -0,0 +1,22 @@
+class TagSearcher < Object
+  def initialize
+    @qs = Tag.ordered_by_type.select('tags.*')
+  end
+
+  def search(tag_name: nil, tag_type: nil, page: 1)
+    validate_type!(tag_type) if tag_type.present?
+
+    @qs = @qs.where('name LIKE ?', tag_name) if tag_name.present?
+    @qs = @qs.where(type: tag_type) if tag_type.present?
+    @qs = @qs.includes(:user) if tag_type == 'Setting'
+    @qs = @qs.where.not(type: 'GalleryGroup') unless tag_type == 'GalleryGroup'
+    @qs.with_item_counts.paginate(per_page: 25, page: page)
+  end
+
+  def validate_type!(tag_type)
+    return if Tag::TYPES.include?(tag_type)
+    raise InvalidTagType.new("Invalid filter")
+  end
+end
+
+class InvalidTagType < ApiError; end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( posts/*.js users/*.js boards/*.js characters/*.js global.js paginator.js galleries/*.js messages.js icons.js board_sections.js blocks.js icons.js reorder.js tags/*.js accept_tos.js )
+Rails.application.config.assets.precompile += %w(global.js paginator.js messages.js icons.js board_sections.js blocks.js icons.js reorder.js accept_tos.js)
+Rails.application.config.assets.precompile += [/\w+\/*.js/]
 Rails.application.config.assets.precompile += %w( layouts/*.css tinymce.css )
