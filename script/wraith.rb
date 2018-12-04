12fc9a1ce8dca0381601dc97b04965900cf05850
teceler <19716939+teceler@users.noreply.github.com>
2018-12-04 13:18:23 -0600

    Add file for iconless theme, and test-running script

---
+++ script/wraith.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+wraith = ARGV[0] || 'wraith'
+command = ARGV[1] || 'latest'
+
+WRAITH_DIR = 'wraith'
+
+user = User.find_by(username: "Throne3d")
+
+layouts = ['default', 'dark', 'iconless', 'starry', 'starrydark', 'starrylight', 'monochrome', 'river']
+
+layouts.each do |layout|
+  if layout == 'default'
+    user.update!(layout: nil)
+  else
+    user.update!(layout: layout)
+  end
+  `cd #{WRAITH_DIR}`
+  puts `#{wraith} #{command} #{layout}`
+end
