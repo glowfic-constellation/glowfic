51f557820ff1e1877cbc398889e29b934afe482f
teceler <19716939+teceler@users.noreply.github.com>
2018-12-05 20:05:53 -0600

    Remove debug code

--- wraith/javascript/post-menu.js
+++ wraith/javascript/post-menu.js
@@ -1,7 +1,4 @@
 module.exports = function (casper, ready) {
-  casper.options.verbose = true;
-  casper.options.logLevel = "debug"
-
   casper.wait(2000, function() {
     casper.click('#post-menu');
     ready();
