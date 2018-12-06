5e4b133de72b5817dfc8976735cf82409fa2d2cf
teceler <19716939+teceler@users.noreply.github.com>
2018-12-06 12:17:32 -0600

    Regularize author order in posts#search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -211,7 +211,7 @@ class PostsController < WritableController
     # don't start blank if the parameters are set
     @setting = Setting.where(id: params[:setting_id]) if params[:setting_id].present?
     @character = Character.where(id: params[:character_id]) if params[:character_id].present?
-    @user = User.where(id: params[:author_id]) if params[:author_id].present?
+    @user = User.where(id: params[:author_id]).ordered if params[:author_id].present?
     @board = Board.where(id: params[:board_id]) if params[:board_id].present?
 
     return unless params[:commit].present?
