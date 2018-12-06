04a4de8fe42f55b0888dd67a6eda5586df5fed7c
teceler <19716939+teceler@users.noreply.github.com>
2018-12-06 10:47:51 -0600

    Strip out more debugger code

--- wraith/javascript/wait.js
+++ wraith/javascript/wait.js
@@ -1,7 +1,3 @@
 module.exports = function (casper, ready) {
-    casper.options.verbose = true;
-    casper.options.logLevel = "debug"
-
-    // make Wraith wait a bit longer before taking the screenshot
-    casper.wait(10000, ready); // you MUST call the ready() callback for Wraith to continue
+    casper.wait(10000, ready);
 }
