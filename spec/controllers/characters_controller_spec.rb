04bce5c46decd79d851380ec949ffacffac012eb
teceler <19716939+teceler@users.noreply.github.com>
2019-01-09 23:36:16 -0600

    Update tests

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -1432,6 +1432,19 @@ RSpec.describe CharactersController do
         get :search, params: { commit: true, author_id: user.id }
         expect(assigns(:search_results).length).to eq(25)
       end
+
+      it 'handles failure' do
+        create(:character, name: 'a')
+        get :search, params: { commit: true, name: 'a', search_name: true, author_id: -1 }
+        expect(flash[:error]).to eq("User could not be found.")
+      end
+
+      it 'handles multiple failures' do
+        create(:character, name: 'a')
+        get :search, params: { commit: true, name: 'a', search_name: true, author_id: -1, template_id: -1 }
+        expect(flash[:error][:message]).to eq("Search could not be completed.")
+        expect(flash[:error][:array]).to match_array(["User could not be found.", "Template could not be found."])
+      end
     end
   end
 
