5360fda2a1aa09660aeb04800e5513df695df7e3
teceler <19716939+teceler@users.noreply.github.com>
2019-01-12 19:34:55 -0600

    Clean up IconMultiRemover

--- app/services/icon/multi_remover.rb
+++ app/services/icon/multi_remover.rb
@@ -1,12 +1,12 @@
 class Icon::MultiRemover < Generic::Service
   attr_reader :gallery
 
-  def perform(user)
+  def perform(params, user:)
     @gallery = Gallery.find_by_id(params[:gallery_id])
     icon_ids = (params[:marked_ids] || []).map(&:to_i).reject(&:zero?)
     if icon_ids.empty? || (@icons = Icon.where(id: icon_ids)).empty?
-      flash[:error] = "No icons selected."
-      redirect_to user_galleries_path(current_user) and return
+      @errors.add(:base, "No icons selected.")
+      return
     end
 
     if params[:gallery_delete]
@@ -17,30 +17,20 @@ class Icon::MultiRemover < Generic::Service
   end
 
   def remove(user)
-    unless @gallery
-      flash[:error] = "Gallery could not be found."
-      redirect_to user_galleries_path(current_user) and return
-    end
-
-    unless @gallery.user_id == current_user.id
-      flash[:error] = "You do not have permission to modify this gallery."
-      redirect_to user_galleries_path(current_user) and return
-    end
+    @errors.add(:gallery, "could not be found.") unless @gallery
+    @errors.add(:base, "You do not have permission to modify this gallery.") if @gallery && @gallery.user_id != user.id
+    return if @errors.present?
 
     @icons.each do |icon|
-      next unless icon.user_id == current_user.id
+      next unless icon.user_id == user.id
       @gallery.icons.destroy(icon)
     end
-
-    flash[:success] = "Icons removed from gallery."
-    icon_redirect(@gallery) and return
   end
 
   def delete(user)
     @icons.each do |icon|
       next unless icon.user_id == user.id
-      icon.destroy
+      icon.destroy!
     end
-    flash[:success] = "Icons deleted."
   end
 end
