b7a01be37006b3d11ebf58af1e5c59c62798a90a
Marri <xxstealthestarsxx@gmail.com>
2019-01-17 16:53:44 -0500

    Use delete_all for a Post's dependent replies when destroying, since we don't need any of the Reply callbacks to run- yet- and when users try to delete things it errors out (and if devs try to delete things it takes out the database).

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ApplicationRecord
   belongs_to :last_user, class_name: 'User', inverse_of: false, optional: false
   belongs_to :last_reply, class_name: 'Reply', inverse_of: false, optional: true
   has_one :flat_post, dependent: :destroy
-  has_many :replies, inverse_of: :post, dependent: :destroy
+  has_many :replies, inverse_of: :post, dependent: :delete_all
   has_many :reply_drafts, dependent: :destroy
 
   has_many :post_viewers, inverse_of: :post, dependent: :destroy
