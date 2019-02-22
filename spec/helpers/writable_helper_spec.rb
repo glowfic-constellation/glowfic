ac6efabfc8780352ecef0cf49bf5147420176e9d
Marri <xxstealthestarsxx@gmail.com>
2019-02-22 16:18:58 -0300

    Anchors the post list's continuity link in post lists to the post's subcontinuity within the continuity#show page if the post is in a subcontinuity. NB: does not yet cleanly handle sectionless posts in a sectioned continuity.

--- spec/helpers/writable_helper_spec.rb
+++ spec/helpers/writable_helper_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe WritableHelper do
       expect(helper.shortened_desc(text, 1)).to eq('a' * 255 + dots + expand + more)
     end
   end
+
+  describe "#anchored_board_path" do
+    it "anchors for sectioned post" do
+      section = create(:board_section)
+      post = create(:post, board: section.board, section: section)
+      expect(helper.anchored_board_path(post)).to eq(board_path(post.board_id) + "#section-" + section.id.to_s)
+    end
+
+    it "does not anchor for unsectioned post" do
+      post = create(:post)
+      expect(helper.anchored_board_path(post)).to eq(board_path(post.board_id))
+    end
+  end
 end
