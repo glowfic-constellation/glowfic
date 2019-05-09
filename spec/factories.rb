e2f04f88035677a97d0ed446a8f18f17134b63fd
teceler <19716939+teceler@users.noreply.github.com>
2019-05-08 22:13:18 -0500

    Bracket default value for tos_version to prevent factorybot crash

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     sequence :email do |n|
       "fake#{n}@faker.com"
     end
-    tos_version User::CURRENT_TOS_VERSION
+    tos_version { User::CURRENT_TOS_VERSION }
 
     factory :admin_user do
       role_id { 1 }
