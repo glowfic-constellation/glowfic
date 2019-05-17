4801143c4f273a38328b7d454ead330aa7652864
teceler <19716939+teceler@users.noreply.github.com>
2019-05-17 14:55:06 -0500

    Re-order characters and tags

--- db/seeds/tag.rb
+++ db/seeds/tag.rb
@@ -15,34 +15,32 @@ Setting.create!([
 
 puts "Assigning tags to characters..."
 CharacterTag.create!([
-  { character_id: 19, tag_id: 2 },
-  { character_id: 20, tag_id: 3 },
-  { character_id: 22, tag_id: 2 },
-  { character_id: 27, tag_id: 4 },
   { character_id: 26, tag_id: 4 },
-  { character_id: 21, tag_id: 2 },
-  { character_id: 33, tag_id: 2 },
-  { character_id: 26, tag_id: 1 },
-  { character_id: 10, tag_id: 5 },
-  { character_id: 14, tag_id: 7 },
-  { character_id: 15, tag_id: 2 },
-  { character_id: 16, tag_id: 2 },
-  { character_id: 30, tag_id: 5 },
-  { character_id: 31, tag_id: 2 },
-  { character_id: 28, tag_id: 4 },
-  { character_id: 31, tag_id: 10 },
-  { character_id: 29, tag_id: 2 },
-  { character_id: 19, tag_id: 10 },
-  { character_id: 12, tag_id: 8 },
-  { character_id: 17, tag_id: 5 },
-  { character_id: 18, tag_id: 7 },
+  { character_id: 10, tag_id: 8 },
+  { character_id: 12, tag_id: 11 },
+  { character_id: 14, tag_id: 10 },
+  { character_id: 15, tag_id: 5 },
+  { character_id: 16, tag_id: 5 },
+  { character_id: 17, tag_id: 8 },
+  { character_id: 18, tag_id: 10 },
+  { character_id: 19, tag_id: 13 },
+  { character_id: 20, tag_id: 6 },
+  { character_id: 21, tag_id: 5 },
+  { character_id: 22, tag_id: 5 },
+  { character_id: 26, tag_id: 7 },
+  { character_id: 27, tag_id: 7 },
+  { character_id: 28, tag_id: 7 },
+  { character_id: 29, tag_id: 5 },
+  { character_id: 30, tag_id: 8 },
+  { character_id: 31, tag_id: 13 },
+  { character_id: 33, tag_id: 5 },
 ])
 
 puts "Assigning tags to galleries..."
 GalleryTag.create!([
-  { gallery_id: 26, tag_id: 1 },
-  { gallery_id: 28, tag_id: 1 },
-  { gallery_id: 27, tag_id: 1 },
+  { gallery_id: 26, tag_id: 4 },
+  { gallery_id: 28, tag_id: 4 },
+  { gallery_id: 27, tag_id: 4 },
 ])
 
 puts "Attaching settings to each other..."
@@ -54,10 +52,10 @@ TagTag.create!([
 
 puts "Attaching tags to posts..."
 PostTag.create!([
-  { post_id: 1, tag_id: 10 },
-  { post_id: 1, tag_id: 2 },
-  { post_id: 2, tag_id: 10 },
-  { post_id: 2, tag_id: 2 },
-  { post_id: 3, tag_id: 2 },
-  { post_id: 3, tag_id: 10 },
+  { post_id: 1, tag_id: 13 },
+  { post_id: 1, tag_id: 5 },
+  { post_id: 2, tag_id: 13 },
+  { post_id: 2, tag_id: 5 },
+  { post_id: 3, tag_id: 5 },
+  { post_id: 3, tag_id: 13 },
 ])
