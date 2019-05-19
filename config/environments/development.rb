99cc6158e1ec7152a0a3e7aeb93c400f40238835
teceler <19716939+teceler@users.noreply.github.com>
2019-05-19 18:10:14 -0500

    Set up storage.yaml and enviroment files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "glowfic_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
   # Don't care if the mailer can't send.
   # Swap these lines with the commented lines to send mail.
   config.action_mailer.raise_delivery_errors = false
