e20d134e86c6a04dee23071d6ef3c28c8006f268
teceler <19716939+teceler@users.noreply.github.com>
2019-05-23 20:08:53 -0500

    Move create/update/destroy to begin/rescue/else syntax

--- app/controllers/generic_controller.rb
+++ app/controllers/generic_controller.rb
@@ -23,18 +23,21 @@ class GenericController < ApplicationController
     model = model_class.new(model_params)
     set_params(model)
 
-    unless model.save
-      flash.now[:error] = {}
-      flash.now[:error][:message] = "#{model_name} could not be created."
-      flash.now[:error][:array] = model.errors.full_messages
+    begin
+      model.save!
+    rescue ActiveRecord::RecordInvalid
+      flash.now[:error] = {
+        message: "#{model_name} could not be created.",
+        array: model.errors.full_messages
+      }
       @page_title = "New #{model_name}"
       set_model(model)
       setup_editor
-      render :new and return
+      render :new
+    else
+      flash[:success] = "#{model_name} created successfully."
+      redirect_to model_path(model)
     end
-
-    flash[:success] = "#{model_name} created successfully."
-    redirect_to model_path(model)
   end
 
   def show
@@ -46,27 +49,34 @@ class GenericController < ApplicationController
   end
 
   def update
-    unless @model.update_attributes(model_params)
-      flash.now[:error] = {}
-      flash.now[:error][:message] = "#{model_name} could not be saved because of the following problems:"
-      flash.now[:error][:array] = @model.errors.full_messages
+    begin
+      @model.update!(model_params)
+    rescue ActiveRecord::RecordInvalid
+      flash.now[:error] = {
+        message: "#{model_name} could not be saved because of the following problems:",
+        array: @model.errors.full_messages
+      }
       @page_title = "Edit #{model_name}: #{@model.name}"
-      render :edit and return
+      render :edit
+    else
+      flash[:success] = "#{model_name} saved!"
+      redirect_to model_path(@model)
     end
-
-    flash[:success] = "#{model_name} saved!"
-    redirect_to model_path(@model)
   end
 
   def destroy
-    @model.destroy!
-    flash[:success] = "#{model_name} deleted."
-    redirect_to models_path
-  rescue ActiveRecord::RecordNotDestroyed
-    flash[:error] = {}
-    flash[:error][:message] = "#{model_name} could not be deleted."
-    flash[:error][:array] = @model.errors.full_messages
-    redirect_to model_path(@model)
+    begin
+      @model.destroy!
+    rescue ActiveRecord::RecordNotDestroyed
+      flash[:error] = {
+        message: "#{model_name} could not be deleted.",
+        array: @model.errors.full_messages
+      }
+      redirect_to model_path(@model)
+    else
+      flash[:success] = "#{model_name} deleted."
+      redirect_to models_path
+    end
   end
 
   protected
