dab148367cc9cd9ead06c5b30bd149de4da8cc1e
teceler <19716939+teceler@users.noreply.github.com>
2019-05-23 20:01:45 -0500

    Revert Controllers to master

--- app/controllers/board_sections_controller.rb
+++ app/controllers/board_sections_controller.rb
@@ -1,32 +1,45 @@
 # frozen_string_literal: true
-class BoardSectionsController < GenericController
+class BoardSectionsController < ApplicationController
+  before_action :login_required, except: :show
+  before_action :find_section, except: [:new, :create]
+  before_action :require_permission, except: [:show, :update]
+
+  def new
+    @board_section = BoardSection.new(board_id: params[:board_id])
+    @page_title = 'New Section'
+  end
 
   def create
     @board_section = BoardSection.new(section_params)
     unless @board_section.board.nil? || @board_section.board.editable_by?(current_user)
-      flash[:error] = "You do not have permission to edit this continuity."
+      flash[:error] = "You do not have permission to modify this continuity."
       redirect_to boards_path and return
     end
 
     begin
       @board_section.save!
-    rescue ActiveRecord::RecordInvalid
-      flash.now[:error] = {
-        message: "Section could not be created.",
-        array: @board_section.errors.full_messages
-      }
+    rescue ActiveRecord::RecordInvalid => e
+      render_errors(@board_section, action: 'created', now: true, class_name: 'Section')
+      log_error(e) unless @board_section.errors.present?
+
       @page_title = 'New Section'
       render :new
     else
-      flash[:success] = "New section, #{@board_section.name}, has successfully been created for #{@board_section.board.name}."
+      flash[:success] = "New section, #{@board_section.name}, created for #{@board_section.board.name}."
       redirect_to edit_board_path(@board_section.board)
     end
   end
 
   def show
-    super
+    @page_title = @board_section.name
     @posts = posts_from_relation(@board_section.posts.ordered_in_section)
     @meta_og = og_data
+  end
+
+  def edit
+    @page_title = 'Edit ' + @board_section.name
+    use_javascript('board_sections')
+    gon.section_id = @board_section.id
   end
 
   def update
@@ -36,22 +49,50 @@ class BoardSectionsController < GenericController
 
     begin
       @board_section.save!
-    rescue ActiveRecord::RecordInvalid
-      flash.now[:error] = {
-        message: "Section could not be updated.",
-        array: @board_section.errors.full_messages
-      }
+    rescue ActiveRecord::RecordInvalid => e
+      render_errors(@board_section, action: 'updated', now: true, class_name: 'Section')
+      log_error(e) unless @board_section.errors.present?
+
       @page_title = 'Edit ' + @board_section.name_was
-      setup_editor
+      use_javascript('board_sections')
+      gon.section_id = @board_section.id
       render :edit
     else
-      flash[:success] = "#{@board_section.name} has been successfully updated."
+      flash[:success] = "Section updated."
       redirect_to board_section_path(@board_section)
     end
   end
 
+  def destroy
+    begin
+      @board_section.destroy!
+    rescue ActiveRecord::RecordNotDestroyed => e
+      render_errors(@board_section, action: 'deleted', class_name: 'Section')
+      log_error(e) unless @board_section.errors.present?
+      redirect_to board_section_path(@board_section)
+    else
+      flash[:success] = "Section deleted."
+      redirect_to edit_board_path(@board_section.board)
+    end
+  end
 
   private
+
+  def find_section
+    @board_section = BoardSection.find_by_id(params[:id])
+    unless @board_section
+      flash[:error] = "Section not found."
+      redirect_to boards_path and return
+    end
+  end
+
+  def require_permission
+    board = @board_section.try(:board) || Board.find_by_id(params[:board_id])
+    if board && !board.editable_by?(current_user)
+      flash[:error] = "You do not have permission to modify this continuity."
+      redirect_to boards_path and return
+    end
+  end
 
   def og_data
     stats = []
@@ -68,16 +109,11 @@ class BoardSectionsController < GenericController
     }
   end
 
-  def model_params
+  def section_params
     params.fetch(:board_section, {}).permit(
       :board_id,
       :name,
       :description,
     )
-  end
-
-  def setup_editor
-    use_javascript('board_sections')
-    gon.section_id = @board_section&.id
   end
 end
--- app/controllers/indexes_controller.rb
+++ app/controllers/indexes_controller.rb
@@ -1,26 +1,75 @@
 # frozen_string_literal: true
-class IndexesController < GenericController
+class IndexesController < ApplicationController
+  before_action :login_required, except: [:index, :show]
+  before_action :find_index, except: [:index, :new, :create]
+  before_action :permission_required, except: [:index, :new, :create, :show]
+  before_action :prepare_editor, only: :edit
+
   def index
-    super
+    @page_title = "Indexes"
     @indexes = Index.order('id asc').paginate(per_page: 25, page: page)
   end
 
+  def new
+    @page_title = "New Index"
+    @index = Index.new(user: current_user)
+  end
+
+  def create
+    @index = Index.new(index_params)
+    @index.user = current_user
+
+    begin
+      @index.save!
+    rescue ActiveRecord::RecordInvalid => e
+      render_errors(@index, action: 'created', now: true)
+      log_error(e) unless @index.errors.present?
+
+      @page_title = 'New Index'
+      render :new
+    else
+      flash[:success] = "Index created."
+      redirect_to index_path(@index) and return
+    end
+  end
+
   def show
-    super
+    unless @index.visible_to?(current_user)
+      flash[:error] = "You do not have permission to view this index."
+      redirect_to indexes_path and return
+    end
+
+    @page_title = @index.name.to_s
     @sectionless = @index.posts.where(index_posts: {index_section_id: nil})
     @sectionless = @sectionless.ordered_by_index
-    @sectionless = posts_from_relation(@sectionless, with_pagination: false, select: ', index_posts.description as index_description')
-    @sectionless = @sectionless.select { |p| p.visible_to?(current_user) }
+    dbselect = ', index_posts.description as index_description, index_posts.id as index_post_id'
+    @sectionless = posts_from_relation(@sectionless, with_pagination: false, select: dbselect)
+  end
+
+  def edit
+  end
+
+  def update
+    begin
+      @index.update!(index_params)
+    rescue ActiveRecord::RecordInvalid => e
+      render_errors(@index, action: 'updated', now: true)
+      log_error(e) unless @index.errors.present?
+
+      prepare_editor
+      render :edit
+    else
+      flash[:success] = "Index updated."
+      redirect_to index_path(@index)
+    end
   end
 
   def destroy
     begin
       @index.destroy!
-    rescue ActiveRecord::RecordNotDestroyed
-      flash[:error] = {
-        message: "Index could not be deleted.",
-        array: @index.errors.full_messages
-      }
+    rescue ActiveRecord::RecordNotDestroyed => e
+      render_errors(@index, action: 'deleted')
+      log_error(e) unless @index.errors.present?
       redirect_to index_path(@index)
     else
       redirect_to indexes_path
@@ -30,11 +79,30 @@ class IndexesController < GenericController
 
   private
 
-  def model_params
-    params.fetch(:index, {}).permit(:name, :description, :privacy, :open_to_anyone)
+  def find_index
+    unless (@index = Index.find_by_id(params[:id]))
+      flash[:error] = "Index could not be found."
+      redirect_to indexes_path
+    end
   end
 
-  def set_params(new_index)
-    new_index.user = current_user
+  def permission_required
+    unless @index.editable_by?(current_user)
+      flash[:error] = "You do not have permission to modify this index."
+      redirect_to index_path(@index)
+    end
+  end
+
+  def index_params
+    params.fetch(:index, {}).permit(:name, :description, :privacy, :authors_locked)
+  end
+
+  def prepare_editor
+    @page_title = "Edit Index: #{@index.name}"
+    use_javascript('posts/index_edit')
+    @index_sections = @index.index_sections.ordered
+    @unsectioned_posts = @index.posts.where(index_posts: {index_section_id: nil})
+    @unsectioned_posts = @unsectioned_posts.select("posts.*, index_posts.id as index_post_id, index_posts.section_order as section_order")
+    @unsectioned_posts = @unsectioned_posts.order('index_posts.section_order ASC')
   end
 end
