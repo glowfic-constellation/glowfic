b3f8d2f6e1945760a7e193add01c6e7d9a44b200
teceler <19716939+teceler@users.noreply.github.com>
2019-07-24 18:39:13 -0400

    Scope to owned icons and simplify loops

--- app/services/icon/multi_remover.rb
+++ app/services/icon/multi_remover.rb
@@ -4,7 +4,7 @@ class Icon::MultiRemover < Generic::Service
   def perform(params, user:)
     @gallery = Gallery.find_by_id(params[:gallery_id])
     icon_ids = (params[:marked_ids] || []).map(&:to_i).reject(&:zero?)
-    if icon_ids.empty? || (@icons = Icon.where(id: icon_ids)).empty?
+    if icon_ids.empty? || (@icons = Icon.where(id: icon_ids, user_id: user.id)).empty?
       @errors.add(:base, "No icons selected.")
       return
     end
@@ -12,7 +12,7 @@ class Icon::MultiRemover < Generic::Service
     if params[:gallery_delete]
       remove(user)
     else
-      delete(user)
+      @icons.destroy_all
     end
   end
 
@@ -20,17 +20,6 @@ class Icon::MultiRemover < Generic::Service
     @errors.add(:gallery, "could not be found.") unless @gallery
     @errors.add(:base, "You do not have permission to modify this gallery.") if @gallery && @gallery.user_id != user.id
     return if @errors.present?
-
-    @icons.each do |icon|
-      next unless icon.user_id == user.id
-      @gallery.icons.destroy(icon)
-    end
-  end
-
-  def delete(user)
-    @icons.each do |icon|
-      next unless icon.user_id == user.id
-      icon.destroy!
-    end
+    @icons.each { |icon| @gallery.icons.destroy(icon) }
   end
 end
