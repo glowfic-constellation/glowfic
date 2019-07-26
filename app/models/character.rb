0c42af5f8a6ae8992dc2e4790db2ff3bed94d250
teceler <19716939+teceler@users.noreply.github.com>
2019-07-26 17:02:12 -0400

    Make ungrouped_galleries an association

--- app/models/character.rb
+++ app/models/character.rb
@@ -11,6 +11,8 @@ class Character < ApplicationRecord
 
   has_many :characters_galleries, inverse_of: :character, dependent: :destroy
   has_many :galleries, through: :characters_galleries, dependent: :destroy
+  has_many :ungrouped_characters_galleries, -> { where(added_by_group: false) }, class_name: 'CharactersGallery', inverse_of: :character
+  has_many :ungrouped_galleries, through: :ungrouped_characters_galleries, source: :gallery
   has_many :icons, -> { group('icons.id').ordered }, through: :galleries
 
   has_many :character_tags, inverse_of: :character, dependent: :destroy
@@ -69,10 +71,6 @@ class Character < ApplicationRecord
       other.section_order = index
       other.save
     end
-  end
-
-  def ungrouped_gallery_ids
-    characters_galleries.reject(&:added_by_group?).map(&:gallery_id)
   end
 
   def character_gallery_for(gallery)
