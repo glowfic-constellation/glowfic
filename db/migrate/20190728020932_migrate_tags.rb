db5e7c0f4d25d15e754a020221c3cf98bfec1da3
teceler <19716939+teceler@users.noreply.github.com>
2019-07-28 16:09:15 -0400

    Fixes

--- db/migrate/20190728020932_migrate_tags.rb
+++ db/migrate/20190728020932_migrate_tags.rb
@@ -20,23 +20,23 @@ class MigrateTags < ActiveRecord::Migration[5.2]
 
   def down
     Tag.transaction do
-      warnings = ActsAsTaggableOn::Tag.for_context(:content_warning)
+      warnings = ActsAsTaggableOn::Tag.for_context(:content_warnings)
       warnings.each do |warning|
         ContentWarning.create!(name: warning.name, created_at: warning.created_at, updated_at: warning.updated_at)
       end
 
-      labels = ActsAsTaggableOn::Tag.for_context(:label)
+      labels = ActsAsTaggableOn::Tag.for_context(:labels)
       labels.each do |label|
         Label.create!(name: label.name, created_at: label.created_at, updated_at: label.updated_at)
       end
 
       Post.tagged_with((warnings + labels), any: true).each do |post|
         post.content_warning_list.each do |warning|
-          PostTag.create!(post: post, warning: ContentWarning.find_by(name: warning))
+          PostTag.create!(post: post, tag: ContentWarning.find_by(name: warning))
         end
 
         post.label_list.each do |label|
-          PostTag.create!(post: post, warning: ContentWarning.find_by(name: warning))
+          PostTag.create!(post: post, tag: Label.find_by(name: label))
         end
       end
     end
