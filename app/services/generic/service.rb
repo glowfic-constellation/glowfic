178164bb5e87e77109b311394d3d3bd12702ba3a
teceler <19716939+teceler@users.noreply.github.com>
2019-08-02 17:22:02 -0400

    Create Generic::Service

---
+++ app/services/generic/service.rb
@@ -0,0 +1,21 @@
+class Generic::Service < Object
+  extend ActiveModel::Translation
+  extend ActiveModel::Validations
+
+  attr_reader :errors, :model
+
+  def initialize
+    @errors = ActiveModel::Errors.new(self)
+  end
+
+  def has_error?(error, attribute=:base)
+    return false unless @errors.key?(attribute)
+    @errors.added?(attribute, error)
+  end
+
+  def has_any_error?(errors, attribute: :base)
+    return false unless @errors.key?(attribute)
+    errors.select! { |error| @errors.added?(attribute, error) }
+    errors.present?
+  end
+end
