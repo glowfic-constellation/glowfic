bb70903a959d2bf10c52d2af1bed5b9704395e29
teceler <19716939+teceler@users.noreply.github.com>
2019-09-04 21:15:29 -0500

    Make s3_delete tests use old_uploaded_icon

--- spec/controllers/api/v1/icons_controller_spec.rb
+++ spec/controllers/api/v1/icons_controller_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Api::V1::IconsController do
 
     it "should not allow deleting a URL in use" do
       handle_s3_bucket
-      icon = create(:uploaded_icon)
+      icon = create(:old_uploaded_icon)
       login_as(icon.user)
       expect(S3_BUCKET).not_to receive(:delete_objects)
       post :s3_delete, params: { s3_key: icon.s3_key }
@@ -45,7 +45,7 @@ RSpec.describe Api::V1::IconsController do
     it "should delete the URL" do
       handle_s3_bucket
       user = create(:user)
-      icon = build(:uploaded_icon, user: user)
+      icon = build(:old_uploaded_icon, user: user)
       login_as(user)
       delete_key = {delete: {objects: [{key: icon.s3_key}], quiet: true}}
       expect(S3_BUCKET).to receive(:delete_objects).with(delete_key)
