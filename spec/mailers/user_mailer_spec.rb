d7d39c689d727c76ed39af8ded9b324cc6720a6f
teceler <19716939+teceler@users.noreply.github.com>
2019-10-05 22:49:41 -0500

    Consistently use before(:each)

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  before { ResqueSpec.reset! }
+  before(:each) { ResqueSpec.reset! }
 
   describe "#post_has_new_reply" do
     it "sends email" do
