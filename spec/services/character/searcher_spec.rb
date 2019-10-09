c1a84f26d188748734be34a2299608a52449d7e9
teceler <19716939+teceler@users.noreply.github.com>
2019-10-09 16:21:30 -0500

    Allow templates to default to []

--- spec/services/character/searcher_spec.rb
+++ spec/services/character/searcher_spec.rb
@@ -11,37 +11,37 @@ RSpec.describe Character::Searcher do
   end
 
   it "searches names correctly" do
-    results = Character::Searcher.new(templates: []).search({ name: 'a', search_name: true })
+    results = Character::Searcher.new.search({ name: 'a', search_name: true })
     expect(results).to match_array([name])
   end
 
   it "searches screenname correctly" do
-    results = Character::Searcher.new(templates: []).search({ name: 'a', search_screenname: true })
+    results = Character::Searcher.new.search({ name: 'a', search_screenname: true })
     expect(results).to match_array([screenname])
   end
 
   it "searches nickname correctly" do
-    results = Character::Searcher.new(templates: []).search({ name: 'a', search_nickname: true })
+    results = Character::Searcher.new.search({ name: 'a', search_nickname: true })
     expect(results).to match_array([nickname])
   end
 
   it "searches name + screenname correctly" do
-    results = Character::Searcher.new(templates: []).search({ name: 'a', search_name: true, search_screenname: true })
+    results = Character::Searcher.new.search({ name: 'a', search_name: true, search_screenname: true })
     expect(results).to match_array([name, screenname])
   end
 
   it "searches name + nickname correctly" do
-    results = Character::Searcher.new(templates: []).search({ name: 'a', search_name: true, search_nickname: true })
+    results = Character::Searcher.new.search({ name: 'a', search_name: true, search_nickname: true })
     expect(results).to match_array([name, nickname])
   end
 
   it "searches nickname + screenname correctly" do
-    results = Character::Searcher.new(templates: []).search({ name: 'a', search_nickname: true, search_screenname: true })
+    results = Character::Searcher.new.search({ name: 'a', search_nickname: true, search_screenname: true })
     expect(results).to match_array([nickname, screenname])
   end
 
   it "searches all correctly" do
-    results = Character::Searcher.new(templates: []).search({ name: 'a', search_name: true, search_screenname: true, search_nickname: true })
+    results = Character::Searcher.new.search({ name: 'a', search_name: true, search_screenname: true, search_nickname: true })
     expect(results).to match_array([name, screenname, nickname])
   end
 
@@ -53,7 +53,7 @@ RSpec.describe Character::Searcher do
     char1 = create(:character, user: user, template: template, name: 'a')
     char5 = create(:character, user: user, name: 'e')
     char3 = create(:character, user: user, name: 'c')
-    results = Character::Searcher.new(templates: []).search({ author_id: user.id })
+    results = Character::Searcher.new.search({ author_id: user.id })
     expect(results).to eq([char1, char2, char3, char4, char5])
   end
 
@@ -62,7 +62,7 @@ RSpec.describe Character::Searcher do
     26.times do |i|
       create(:character, user: user, name: "character#{i}")
     end
-    results = Character::Searcher.new(templates: []).search({ author_id: user.id })
+    results = Character::Searcher.new.search({ author_id: user.id })
     expect(results.length).to eq(25)
   end
 end
