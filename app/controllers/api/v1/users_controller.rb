7fc8c9b7c2d271fcafc7978d62cde95d07c05e27
Marri <xxstealthestarsxx@gmail.com>
2019-10-31 10:44:56 -0400

    Beginning a user/posts API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -22,4 +22,24 @@ class Api::V1::UsersController < Api::ApiController
     users = paginate queryset.active, per_page: 25
     render json: {results: users}
   end
+
+  api :GET, '/users/:id/posts', 'Load all posts where the specified user is an author'
+  param :authorship, :number, required: false, desc: 'Level of involvement in a post. TODO define'
+  error 404, "User not found"
+  def posts
+    return unless (user = find_object(User))
+    
+    posts = Post.where(privacy: Concealable::PUBLIC) # TODO expand when OAuth exists
+    case params[:authorship] # TODO does not include first-poster-or-cameo-not-different-primary-author, seems too edge case
+    when 1 # only posts you were the first post
+      posts = posts.where(user: user)
+    when 2 # only posts you are any of the primary authors # TODO wait for creators to have PostAuthors
+      posts = posts.where(id: PostAuthor.where(user: user).select(:post_id))
+    when 3 # only posts where you are a cameo author
+    when 4 # only posts where you are a primary author, not first poster or cameo
+    when 5 # only posts where you are not the starter
+      posts = posts.where(id: PostAuthor.where(user: user).select(:post_id)).where.not(user: user)
+    else # default to all posts they have joined as any of first poster, primary author or cameo author
+    end
+  end
 end
