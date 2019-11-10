398f3d46c1bea70dcfe1681068796da3ae837682
Marri Nikari <xxstealthestarsxx@gmail.com>
2019-11-09 16:40:47 -0800

    Attempt to clean up the migration without breaking everything

--- db/migrate/20191109005526_change_audited_changes_to_json.rb
+++ db/migrate/20191109005526_change_audited_changes_to_json.rb
@@ -1,18 +1,21 @@
 class ChangeAuditedChangesToJson < ActiveRecord::Migration[5.2]
   def up
-    rename_column :audits, :audited_changes, :old_changes
-    add_column :audits, :audited_changes, :jsonb
-    Audited::Audit.all.each do |audit|
-      audit.update!(audited_changes: YAML.load(audit.old_changes))
+    change_column_type(:jsonb) do |audit|
+      YAML.load(audit.old_changes)
     end
-    remove_column :audits, :old_changes
   end
 
   def down
+    change_column_type(:text) do |audit|
+      audit.old_changes
+    end
+  end
+
+  def change_column_type(column_type)
     rename_column :audits, :audited_changes, :old_changes
-    add_column :audits, :audited_changes, :text
-    Audited::Audit.all.each do |audit|
-      audit.update!(audited_changes: JSON.load(audit.old_changes))
+    add_column :audits, :audited_changes, column_type
+    Audited::Audit.find_each do |audit|
+      audit.update!(audited_changes: yield(audit))
     end
     remove_column :audits, :old_changes
   end
