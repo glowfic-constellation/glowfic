78384fec5e6d5ba6c858e39c714da9a268a18e1e
teceler <19716939+teceler@users.noreply.github.com>
2019-11-16 21:24:46 -0600

    Get sprockets precompiling working

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,17 @@
+//= link_tree ../images
+//= link favicons/browserconfig.xml
+//= link favicons/manifest.json
+
+//= require application.js
+//= link_tree ../javascripts .js
+
+//= link_directory ../stylesheets css
+//= stub _palette.scss scss
+//= stub _variables.scss scss
+//= link layouts/dark.css
+//= link layouts/iconless.css
+//= link layouts/monochrome.css
+//= link layouts/river.css
+//= link layouts/starry.css
+//= link layouts/starrydark.css
+//= link layouts/starrylight.css
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,20 +8,4 @@ Rails.application.configure do
   # Rails.application.config.assets.paths << Emoji.images_path
   # Add Yarn node_modules folder to the asset load path.
   config.assets.paths << Rails.root.join('node_modules')
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in the app/assets
-  # folder are already added.
-  config.assets.precompile += [/\w+\/*.js/]
-
-  config.assets.precompile << "accept_tos.js"
-  config.assets.precompile << "blocks.js"
-  config.assets.precompile << "board_sections.js"
-  config.assets.precompile << "global.js"
-  config.assets.precompile << "icons.js"
-  config.assets.precompile << "messages.js"
-  config.assets.precompile << "paginator.js"
-  config.assets.precompile << "reorder.js"
-
-  config.assets.precompile += %w( layouts/*.css tinymce.css )
 end
