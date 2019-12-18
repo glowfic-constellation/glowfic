8701ee0c99cc56c51aa247e0d823d26e2a06380c
Marri Nikari <xxstealthestarsxx@gmail.com>
2019-12-18 11:25:21 -0500

    Tweaks

--- db/migrate/20191218011608_change_audited_changes_to_json.rb
+++ db/migrate/20191218011608_change_audited_changes_to_json.rb
@@ -1,6 +1,6 @@
 class ChangeAuditedChangesToJson < ActiveRecord::Migration[5.2]
   def change
     rename_column :audits, :audited_changes, :old_changes
-    add_column :audits, :audited_changes, column_type
+    add_column :audits, :audited_changes, :jsonb
   end
 end
