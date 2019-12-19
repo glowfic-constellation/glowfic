4b899634158a8ff7ebc6a53fd6f32d2691014e15
Marri Nikari <xxstealthestarsxx@gmail.com>
2019-12-19 09:46:54 -0500

    Fix YAML load which doesn't seem to be working

--- db/migrate/20191218011609_update_audits_slowly.rb
+++ db/migrate/20191218011609_update_audits_slowly.rb
@@ -1,16 +1,8 @@
 class UpdateAuditsSlowly < ActiveRecord::Migration[5.2]
-  def up
-    change_column_type(:jsonb) { |audit| YAML.load(audit.old_changes) }
-  end
-
-  def down
-    change_column_type(:text) { |audit| audit.old_changes }
-  end
-
-  def change_column_type(column_type)
+  def change
     Audited::Audit.find_each do |audit|
       puts "At #{audit.id}" if audit.id % 1000 == 0
-      audit.update!(audited_changes: yield(audit))
+      audit.update!(audited_changes: audit.old_changes)
     end
     remove_column :audits, :old_changes
   end
