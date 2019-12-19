8057eec472c82513dfb9f155dfaa0cd99d4a3947
teceler <19716939+teceler@users.noreply.github.com>
2019-12-18 18:30:34 -0600

    Delete memorization test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -593,13 +593,6 @@ RSpec.describe Post do
   end
 
   describe "#first_unread_for" do
-    it "uses instance variable if set" do
-      post = create(:post)
-      post.instance_variable_set('@first_unread', 3)
-      expect(post).not_to receive(:last_read)
-      expect(post.first_unread_for(nil)).to eq(3)
-    end
-
     it "uses itself if not yet viewed" do
       post = create(:post)
       create(:reply, post: post)
