432ff3348db7365526787e1ad8f075d2e378c9e4
teceler <19716939+teceler@users.noreply.github.com>
2019-12-19 10:15:34 -0600

    Fix migration

--- db/migrate/20191218011609_update_audits_slowly.rb
+++ db/migrate/20191218011609_update_audits_slowly.rb
@@ -2,7 +2,7 @@ class UpdateAuditsSlowly < ActiveRecord::Migration[5.2]
   def change
     Audited::Audit.find_each do |audit|
       puts "At #{audit.id}" if audit.id % 1000 == 0
-      audit.update!(audited_changes: audit.old_changes)
+      audit.update!(audited_changes: YAML.load(audit.old_changes))
     end
     remove_column :audits, :old_changes
   end
