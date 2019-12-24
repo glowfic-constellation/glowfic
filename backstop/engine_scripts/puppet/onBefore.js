ced830d363bfdbd33218563cadc391fbe9fe1ae2
Edward Jones <throne3d@gmail.com>
2019-12-24 00:33:40 +0000

    Skip BackstopTools messages in console output

--- backstop/engine_scripts/puppet/onBefore.js
+++ backstop/engine_scripts/puppet/onBefore.js
@@ -1,3 +1,10 @@
 module.exports = async (page, scenario, vp) => {
   await require('./loadCookies')(page, scenario);
+
+  // skip spam messages in console
+  const oldLog = console.log;
+  console.log = (...args) => {
+    if (args.length === 1 && args[0].includes("BackstopTools have been installed")) return;
+    oldLog(...args);
+  }
 };
