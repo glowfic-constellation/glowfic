4887a7916edf31c9558c68ebc5cce4407cc298f7
teceler <19716939+teceler@users.noreply.github.com>
2019-12-26 21:29:49 -0600

    Fix down migration

--- db/migrate/20190728020932_migrate_tags.rb
+++ db/migrate/20190728020932_migrate_tags.rb
@@ -23,26 +23,30 @@ class MigrateTags < ActiveRecord::Migration[5.2]
   end
 
   def down
-    Tag.transaction do
-      warnings = ActsAsTaggableOn::Tag.for_context(:content_warnings)
-      warnings.each do |warning|
-        ContentWarning.create!(name: warning.name, created_at: warning.created_at, updated_at: warning.updated_at)
-      end
-
-      labels = ActsAsTaggableOn::Tag.for_context(:labels)
-      labels.each do |label|
-        Label.create!(name: label.name, created_at: label.created_at, updated_at: label.updated_at)
-      end
-
-      Post.tagged_with((warnings + labels), any: true).each do |post|
-        post.content_warning_list.each do |warning|
-          PostTag.create!(post: post, tag: ContentWarning.find_by(name: warning))
-        end
-
-        post.label_list.each do |label|
-          PostTag.create!(post: post, tag: Label.find_by(name: label))
-        end
-      end
+    warnings = ActsAsTaggableOn::Tag.for_context(:content_warnings)
+    warnings.each do |warning|
+      user = warning.taggings.order(created_at: :asc).first.tagger
+      ContentWarning.create!(name: warning.name, user: user, created_at: warning.created_at, updated_at: warning.updated_at)
     end
+
+    labels = ActsAsTaggableOn::Tag.for_context(:labels)
+    labels.each do |label|
+      user = label.taggings.order(created_at: :asc).first.tagger
+      Label.create!(name: label.name, user: user, created_at: label.created_at, updated_at: label.updated_at)
+    end
+
+    Post.tagged_with((warnings + labels), any: true).each do |post|
+      post.content_warning_list.each do |warning|
+        PostTag.create!(post: post, tag: ContentWarning.find_by(name: warning))
+      end
+      post.content_warning_list = []
+
+      post.label_list.each do |label|
+        PostTag.create!(post: post, tag: Label.find_by(name: label))
+      end
+      post.label_list = []
+    end
+    ActsAsTaggableOn::Tag.destroy_all
+    ActsAsTaggableOn::Tagging.destroy_all
   end
 end
