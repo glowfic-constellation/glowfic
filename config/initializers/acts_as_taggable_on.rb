e5c74906406d43947d9e09cd543a3ed7c0c95b7f
teceler <19716939+teceler@users.noreply.github.com>
2019-12-28 23:54:31 -0600

    Set up and test parent and child relations on Tag

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -4,5 +4,10 @@ ActsAsTaggableOn.taggings_table = :tagging
 module ActsAsTaggableOn
   class Tag < ::ActiveRecord::Base
     acts_as_ordered_taggable_on :settings
+
+    has_many :parents, through: :taggings, source: :tag, dependent: :destroy
+    has_many :child_taggings, -> { where(taggable_type: 'ActsAsTaggableOn::Tag') },
+      class_name: 'ActsAsTaggableOn::Tagging', foreign_key: :tag_id
+    has_many :children, through: :child_taggings, source: :taggable, source_type: "ActsAsTaggableOn::Tag", dependent: :destroy
   end
 end
