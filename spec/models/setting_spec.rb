0496992f15e91cc8d069d56869e848f392c01852
teceler <19716939+teceler@users.noreply.github.com>
2019-12-28 23:54:31 -0600

    Set up and test parent and child relations on Tag

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -45,4 +45,28 @@ RSpec.describe Setting do
       expect(TagTag.count).to eq(1)
     end
   end
+
+  context "associations" do
+    it "tracks parents and children" do
+      parent1 = create(:setting) # has child1, child2, and child3 as children
+      parent2 = create(:setting) # has child2 as child
+      child1 = create(:setting) # has parent1 as parent and child2 as child
+      child2 = create(:setting) # has parent1, parent2, and child1 as parents
+      child3 = create(:setting) # has parent1 as parent
+
+      child1.setting_list = [parent1.name]
+      child1.save!
+
+      child2.update!(setting_list: [parent1, parent2, child1].map(&:name))
+      child3.update!(setting_list: [parent1.name])
+
+      expect(parent1.reload.children.ids).to match_array([child1, child2, child3].map(&:id))
+      expect(parent2.children.ids).to eq([child2.id])
+      expect(child1.children.ids).to eq([child2.id])
+
+      expect(child1.parents.ids).to eq([parent1.id])
+      expect(child2.parents.ids).to match_array([parent1, parent2, child1].map(&:id))
+      expect(child3.parents.ids).to eq([parent1.id])
+    end
+  end
 end
