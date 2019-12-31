2400dca869fba55bf917c8ec5d5d898f0a2bf9a3
Edward Jones <throne3d@gmail.com>
2019-12-31 11:11:23 -0300

    Restrict additional user details to users#index

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -8,7 +8,23 @@ class UserPresenter
   def as_json(options={})
     return {} unless user
     return {id: user.id, username: '(deleted user)'} if user.deleted?
+    return detailed_json(options) if options[:detailed]
+    summary_json(options)
+  end
+
+  private
+
+  def detailed_json(options={})
     attrs = %w(id username moiety moiety_name created_at)
+    attr_json(attrs, options)
+  end
+
+  def summary_json(options={})
+    attrs = %w(id username)
+    attr_json(attrs, options)
+  end
+
+  def attr_json(attrs, options)
     user.as_json_without_presenter({only: attrs}.reverse_merge(options))
   end
 end
