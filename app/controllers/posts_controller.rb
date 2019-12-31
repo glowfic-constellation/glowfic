cbdea426b90b46ac8af82e5c25cedea2a495150e
teceler <19716939+teceler@users.noreply.github.com>
2019-12-31 08:57:26 -0600

    Kill the process_tags the snuck back in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -296,9 +296,6 @@ class PostsController < WritableController
 
     @author_ids = params.fetch(:post, {}).fetch(:unjoined_author_ids, [])
     @viewer_ids = params.fetch(:post, {}).fetch(:viewer_ids, [])
-    @settings = process_tags(Setting, obj_param: :post, id_param: :setting_ids)
-    @content_warnings = process_tags(ContentWarning, obj_param: :post, id_param: :content_warning_ids)
-    @labels = process_tags(Label, obj_param: :post, id_param: :label_ids)
 
     @written = @post
     editor_setup
