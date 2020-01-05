424ebb9728afb97b29898c0aae4086f0a4b8ae45
teceler <19716939+teceler@users.noreply.github.com>
2020-01-05 13:17:27 -0600

    Consolidate code

--- spec/features/characters/new_spec.rb
+++ spec/features/characters/new_spec.rb
@@ -1,6 +1,33 @@
 require "spec_helper"
 
 RSpec.feature "Creating a new character", :type => :feature do
+  def complex_objects(user)
+    create_list(:icon, 2, user: user)
+    create_list(:template, 2, user: user)
+    create_list(:setting, 2)
+    icon = create(:icon, user: user, keyword: 'Example icon')
+    template = create(:template, user: user, name: 'Example template')
+    create(:setting, name: 'Example setting')
+    create(:setting, name: 'Example setting 2')
+    [template, icon]
+  end
+
+  def complex_setup
+    # create character with no name
+    find("img[alt='Example icon']").click
+    expect(page).to have_selector('.selected-icon')
+    expect(find('.selected-icon')[:alt]).to eq('Example icon')
+
+    select2('#character_setting_ids', 'Example setting', 'Example setting 2')
+
+    within('.form-table') do
+      fill_in 'Template Nickname', with: 'Example nickname'
+      fill_in 'Screen Name', with: 'example_screenname'
+      fill_in 'Facecast', with: 'Example facecast'
+      fill_in 'Description', with: 'Example description'
+    end
+  end
+
   scenario "Create an invalid character", js: true do
     # view new character form without being logged in
     visit new_character_path
@@ -17,33 +44,15 @@ RSpec.feature "Creating a new character", :type => :feature do
     expect(page).to have_selector("th", text: "New Character")
     expect(page).to have_no_selector("img.icon")
 
-    create_list(:icon, 2, user: user)
-    create_list(:template, 2, user: user)
-    create_list(:setting, 2)
-    create(:icon, user: user, keyword: 'Example icon')
-    create(:template, user: user, name: 'Example template')
-    create(:setting, name: 'Example setting')
-    create(:setting, name: 'Example setting 2')
+    complex_objects(user)
 
     # view new character form with icons
     visit new_character_path
+
     expect(page).to have_selector("img.icon", count: 3)
-
-    # create character with no name
-    find("img[alt='Example icon']").click
-    expect(page).to have_selector('.selected-icon')
-    expect(find('.selected-icon')[:alt]).to eq('Example icon')
-
-    select2('#character_setting_ids', 'Example setting', 'Example setting 2')
     select2('#character_template_id', 'Example template')
-
-    within('.form-table') do
-      fill_in 'Template Nickname', with: 'Example nickname'
-      fill_in 'Screen Name', with: 'example_screenname'
-      fill_in 'Facecast', with: 'Example facecast'
-      fill_in 'Description', with: 'Example description'
-      click_button 'Save'
-    end
+    complex_setup
+    click_button 'Save'
 
     expect(page).to have_selector('.flash.error')
     within('.flash.error') do
@@ -82,24 +91,18 @@ RSpec.feature "Creating a new character", :type => :feature do
 
   scenario "Creating character with icon, description and extant template", js: true do
     user = login
-    create_list(:icon, 2, user: user)
-    icon = create(:icon, user: user, keyword: 'Example icon')
-    template = create(:template, user: user, name: 'Example template')
-    create(:template, user: user)
+    template, icon = complex_objects(user)
+
     visit new_character_path(template_id: template.id)
+
+    complex_setup
 
     expect(page).to have_no_selector('.flash.error')
     expect(page).to have_selector('#select2-character_template_id-container', text: 'Example template')
 
-    find("img[alt='Example icon']").click
-    within('.form-table') do
-      fill_in 'Character Name', with: 'Example character'
-      fill_in 'Template Nickname', with: 'Example nickname'
-      fill_in 'Screen Name', with: 'example_screenname'
-      fill_in 'Facecast', with: 'Example facecast'
-      fill_in 'Description', with: 'Example description'
-      click_button 'Save'
-    end
+    fill_in 'Character Name', with: 'Example character'
+    click_button 'Save'
+
     expect(page).to have_no_selector('.flash.error')
     expect(page).to have_selector('.flash.success')
     within('.flash.success') do
@@ -121,6 +124,7 @@ RSpec.feature "Creating a new character", :type => :feature do
       expect(page).to have_selector('.character-template', text: 'Example template')
       expect(page).to have_selector('.character-pb', text: 'Example facecast')
       expect(page).to have_selector('.character-description', text: 'Example description')
+      expect(page).to have_selector('.character-setting', text: 'Example setting, Example setting 2')
     end
   end
 
