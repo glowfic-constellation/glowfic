915da5ef27e195a81719ca83830ed2c2cc51be4d
Edward Jones <throne3d@gmail.com>
2020-01-14 01:51:44 -0300

    Add minio to replicate S3 bucket functionality locally

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,38 @@
-Aws.config.update({
+access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID', 'minioadmin')
+secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY', 'minioadmin')
+bucket_name = ENV.fetch('S3_BUCKET_NAME', 'glowfic-dev')
+config = {
   region: 'us-east-1',
-  credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID', nil), ENV.fetch('AWS_SECRET_ACCESS_KEY', nil)),
-})
+  credentials: Aws::Credentials.new(access_key_id, secret_access_key),
+}
 
-bucket = ENV.fetch('S3_BUCKET_NAME', nil)
-S3_BUCKET = bucket ? Aws::S3::Resource.new.bucket(bucket) : nil
+if ENV.key?('MINIO_ENDPOINT')
+  config[:endpoint] = ENV['MINIO_ENDPOINT']
+  config[:force_path_style] = true
+  Aws.config.update(config)
+
+  client = Aws::S3::Client.new
+  begin
+    client.head_bucket(bucket: bucket_name)
+  rescue StandardError => e
+    puts "creating bucket #{bucket_name}..."
+    public_read_policy = {
+      Version: "2012-10-17",
+      Statement: [{
+        Effect: "Allow",
+        Principal: "*",
+        Action: "s3:GetObject",
+        Resource: "arn:aws:s3:::#{bucket_name}/*"
+      }]
+    }.to_json
+
+    client.create_bucket(bucket: bucket_name)
+    client.put_bucket_policy(bucket: bucket_name, policy: public_read_policy)
+  end
+else
+  Aws.config.update(config)
+end
+
+S3_BUCKET = Aws::S3::Resource.new.bucket(bucket_name)
 
 Aws::Rails.add_action_mailer_delivery_method(:aws_ses)
--- spec/jobs/delete_icon_from_s3_job_spec.rb
+++ spec/jobs/delete_icon_from_s3_job_spec.rb
@@ -1,10 +1,7 @@
-require "support/s3_bucket_helper"
-
 RSpec.describe DeleteIconFromS3Job do
   before(:each) { ResqueSpec.reset! }
 
   it "deletes the given key" do
-    handle_s3_bucket
     key = 'arbitrary'
     delete_key = { delete: { objects: [{ key: key }] } }
     expect(S3_BUCKET).to receive(:delete_objects).with(delete_key)
