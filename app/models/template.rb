fefa25c08b92187a92cefe9dd7e9bf1efc92ea35
teceler <19716939+teceler@users.noreply.github.com>
2020-01-19 11:07:02 -0600

    Move helper methods to class methods on Template

--- app/models/template.rb
+++ app/models/template.rb
@@ -11,4 +11,18 @@ class Template < ApplicationRecord
   def plucked_characters
     characters.pluck(Arel.sql("id, concat_ws(' | ', name, template_name, screenname)"))
   end
+
+  def self.settings_info(characters)
+    settings = characters.joins(:settings).group(:id)
+    sql = Arel.sql('ARRAY_AGG(tags.id ORDER BY character_tags.id ASC) AS setting_ids, ARRAY_AGG(tags.name ORDER BY character_tags.id ASC)')
+    settings = settings.pluck(:id, sql)
+    settings.map{ |i| [i[0], i[1].zip(i[2])] }.to_h
+  end
+
+  def self.characters_list(characters, show_template=false)
+    characters = characters.left_outer_joins(:template) if show_template
+    attributes = [:id, :name, :template_name, :screenname, :pb, :user_id, 'users.username', Arel.sql('users.deleted as user_deleted')]
+    attributes += ['templates.id', 'templates.name'] if show_template
+    characters.joins(:user).pluck(*attributes)
+  end
 end
