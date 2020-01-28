2de217beefaf37bce9ca645403be097460beefef
teceler <19716939+teceler@users.noreply.github.com>
2020-01-27 22:39:34 -0600

    Test changed characters_list behaviour, and fix tags#show tests

--- spec/models/template_spec.rb
+++ spec/models/template_spec.rb
@@ -132,103 +132,166 @@ RSpec.describe Template do
   describe "#character_list" do
     let (:user) { create(:user, username: 'John Doe') }
 
-    describe "handles simple characters" do
-      let (:character1) { create(:character, user: user, name: 'Test Character 1') }
-      let (:character2) { create(:character, user: user, name: 'Test Character 2') }
-      let (:assoc) { Character.where(id: [character1.id, character2.id]) }
-
+    RSpec.shared_examples "shared tests" do |view|
       it "when not showing templates" do
-        assoc = Character.where(id: [character1.id, character2.id])
-        expected = [
-          [character1.id, 'Test Character 1', nil, nil, nil, user.id, 'John Doe', false],
-          [character2.id, 'Test Character 2', nil, nil, nil, user.id, 'John Doe', false]
-        ]
-        expect(Template.characters_list(assoc, false)).to match_array(expected)
-      end
-
-      it "when showing templates" do
-        assoc = Character.where(id: [character1.id, character2.id])
-        expected = [
-          [character1.id, 'Test Character 1', nil, nil, nil, user.id, 'John Doe', false, nil, nil],
-          [character2.id, 'Test Character 2', nil, nil, nil, user.id, 'John Doe', false, nil, nil]
-        ]
-        expect(Template.characters_list(assoc, true)).to match_array(expected)
-      end
-
-      it "with deleted users" do
-        user.update!(deleted: true)
-        expected = [
-          [character1.id, 'Test Character 1', nil, nil, nil, user.id, 'John Doe', true],
-          [character2.id, 'Test Character 2', nil, nil, nil, user.id, 'John Doe', true]
-        ]
-        expect(Template.characters_list(assoc, false)).to match_array(expected)
+        expect(Template.characters_list(assoc, page_view: view)).to eq(expected)
       end
 
       it "from multiple users" do
         user2 = create(:user, username: 'Jane Doe')
         character2.update!(user: user2)
-        expected = [
-          [character1.id, 'Test Character 1', nil, nil, nil, user.id, 'John Doe', false],
-          [character2.id, 'Test Character 2', nil, nil, nil, user2.id, 'Jane Doe', false]
-        ]
-        expect(Template.characters_list(assoc, false)).to match_array(expected)
+        expected[0][:user_id] = user2.id
+        expected[0][:username] = 'Jane Doe' if expected[1].key?(:username)
+        expect(Template.characters_list(assoc, page_view: view)).to eq(expected)
+      end
+    end
+
+    describe "handles simple characters" do
+      let! (:character1) { create(:character, user: user, name: 'Test Character b') }
+      let! (:character2) { create(:character, user: user, name: 'Test Character a') }
+      let (:assoc) { Character.where(id: [character1.id, character2.id]).ordered }
+
+      describe "in icon view" do
+        let (:expected) do
+          [
+            {id: character2.id, name: 'Test Character a', screenname: nil, user_id: user.id, url: nil, keyword: nil},
+            {id: character1.id, name: 'Test Character b', screenname: nil, user_id: user.id, url: nil, keyword: nil}
+          ]
+        end
+
+        include_examples "shared tests", 'icon'
+      end
+
+      describe "in list view" do
+        let (:view) { 'list' }
+        let (:expected) do
+          [
+            {id: character2.id, name: 'Test Character a', screenname: nil, nickname: nil, pb: nil,
+             user_id: user.id, username: 'John Doe', user_deleted: false},
+            {id: character1.id, name: 'Test Character b', screenname: nil, nickname: nil, pb: nil,
+             user_id: user.id, username: 'John Doe', user_deleted: false}
+          ]
+        end
+
+        include_examples "shared tests", 'list'
+
+        it "when showing templates" do
+          expected.each do |char|
+            char[:template_id] = nil
+            char[:template_name] = nil
+          end
+          expect(Template.characters_list(assoc, show_template: true, page_view: view)).to eq(expected)
+        end
+
+        it "with deleted users" do
+          user.update!(deleted: true)
+          expected.each { |char| char[:user_deleted] = true }
+          expect(Template.characters_list(assoc, page_view: view)).to eq(expected)
+        end
       end
     end
 
     describe "handles complex characters" do
-      let (:template1) { create(:template, name: 'Test Template 1')}
-      let (:template2) { create(:template, name: 'Test Template 2')}
+      let (:template1) { create(:template, user: user, name: 'Test Template b')}
+      let (:template2) { create(:template, user: user, name: 'Test Template a')}
 
-      let (:character1) do
+      let! (:character1) do
         create(:character, user: user, template: template1,
-               name: 'Test Character 1', screenname: 'screenname_one',
+               name: 'Test Character b', screenname: 'screenname_one',
                template_name: "Nickname 1", pb: "Facecast 1")
       end
 
-      let (:character2) do
+      let! (:character2) do
         create(:character, user: user, template: template2,
-               name: 'Test Character 2', screenname: 'screenname_two',
+               name: 'Test Character a', screenname: 'screenname_two',
                template_name: "Nickname 2", pb: "Facecast 2")
       end
 
-      let (:assoc) { Character.where(id: [character1.id, character2.id]) }
+      let (:assoc) { Character.where(id: [character1.id, character2.id]).ordered }
 
-      it "when showing templates" do
-        expected = [
-          [character1.id, 'Test Character 1', 'Nickname 1', 'screenname_one',
-           'Facecast 1', user.id, 'John Doe', false, template1.id, "Test Template 1"],
-          [character2.id, 'Test Character 2', 'Nickname 2', 'screenname_two',
-           'Facecast 2', user.id, 'John Doe', false, template2.id, "Test Template 2"]
-        ]
-        expect(Template.characters_list(assoc, true)).to match_array(expected)
+      describe "in icon view" do
+        let (:expected) do
+          [
+            {id: character2.id, name: 'Test Character a', screenname: 'screenname_two', user_id: user.id, url: nil, keyword: nil},
+            {id: character1.id, name: 'Test Character b', screenname: 'screenname_one', user_id: user.id, url: nil, keyword: nil}
+          ]
+        end
+
+        include_examples "shared tests", "icon"
+
+        it "with default icons" do
+          icon1 = create(:icon, user: user, keyword: 'icon 1', url: 'https://fakeicon.com/1.png')
+          icon2 = create(:icon, user: user, keyword: 'icon 2', url: 'https://fakeicon.com/2.png')
+
+          character1.update!(default_icon: icon1)
+          character2.update!(default_icon: icon2)
+
+          expected[1][:keyword] = 'icon 1'
+          expected[1][:url] = 'https://fakeicon.com/1.png'
+          expected[0][:keyword] = 'icon 2'
+          expected[0][:url] = 'https://fakeicon.com/2.png'
+
+          expect(Template.characters_list(assoc, page_view: 'icon')).to eq(expected)
+        end
       end
 
-      it "when not showing templates" do
-        expected = [
-          [character1.id, 'Test Character 1', 'Nickname 1', 'screenname_one', 'Facecast 1', user.id, 'John Doe', false],
-          [character2.id, 'Test Character 2', 'Nickname 2', 'screenname_two', 'Facecast 2', user.id, 'John Doe', false]
-        ]
-        expect(Template.characters_list(assoc, false)).to match_array(expected)
+      describe "in list view" do
+        let (:expected) do
+          [{
+            id: character2.id, name: 'Test Character a', screenname: 'screenname_two',
+            nickname: 'Nickname 2', pb: 'Facecast 2',
+            user_id: user.id, username: 'John Doe', user_deleted: false
+          },
+          {
+            id: character1.id, name: 'Test Character b', screenname: 'screenname_one',
+            nickname: 'Nickname 1', pb: 'Facecast 1',
+            user_id: user.id, username: 'John Doe', user_deleted: false
+          }]
+        end
+
+        it "when showing templates" do
+          expect(Template.characters_list(assoc, page_view: 'list')).to eq(expected)
+        end
+
+        include_examples "shared tests", "list"
       end
     end
 
-    it "handles a mixture of characters" do
-      templateless = create_list(:character, 3, user: user)
-      templated = Array.new(3) { create(:character, user: user, template: create(:template)) }
-      template = create(:template)
-      one_template = create_list(:character, 3, user: user, template: template)
-      deleted_user = create_list(:character, 1, user: create(:user, deleted: true))
-      other_user = create_list(:character, 2, user: create(:user))
-      characters = templateless + templated + one_template + deleted_user + other_user
-      assoc = Character.where(id: characters.map(&:id))
-      expected = [
-        templateless.map{ |char| [char.id, char.name, nil, nil, nil, user.id,      user.username,      false, nil,              nil] },
-        templated.map   { |char| [char.id, char.name, nil, nil, nil, user.id,      user.username,      false, char.template.id, char.template.name] },
-        one_template.map{ |char| [char.id, char.name, nil, nil, nil, user.id,      user.username,      false, template.id,      template.name] },
-        deleted_user.map{ |char| [char.id, char.name, nil, nil, nil, char.user.id, char.user.username, true,  nil,              nil] },
-        other_user.map  { |char| [char.id, char.name, nil, nil, nil, char.user.id, char.user.username, false, nil,              nil] },
-      ].flatten(1)
-      expect(Template.characters_list(assoc, true)).to match_array(expected)
+    describe "handles a mixture of characters" do
+      let(:templateless) { create_list(:character, 3, user: user) }
+      let(:templated) { Array.new(3) { create(:character, user: user, template: create(:template)) } }
+      let(:template) { create(:template) }
+      let(:one_template) { create_list(:character, 3, user: user, template: template) }
+      let(:deleted_user) { create_list(:character, 1, user: create(:user, deleted: true)) }
+      let(:other_user) { create_list(:character, 2, user: create(:user)) }
+      let(:characters) { templateless + templated + one_template + deleted_user + other_user }
+      let(:assoc) { Character.where(id: characters.map(&:id)).ordered }
+
+      before(:each) do
+        templateless[2].update!(name: 'b')
+        one_template[1].update!(name: 'a')
+        characters.sort_by!{ |char| [char.template_id.to_i, char.name] }
+      end
+
+      it "in icon view" do
+        expected = characters.map do |char|
+          { id: char.id, name: char.name, screenname: nil, user_id: char.user_id, url: nil, keyword: nil }
+        end
+
+        expect(Template.characters_list(assoc, page_view: 'icon')).to match_array(expected)
+      end
+
+      it "in list view" do
+        expected = characters.map do |char|
+          {
+            id: char.id, name: char.name, screenname: nil, nickname: nil, pb: nil,
+            user_id: char.user_id, username: char.user.username, user_deleted: char.user.deleted?,
+            template_id: char.template_id, template_name: char.template.try(:name)
+          }
+        end
+
+        expect(Template.characters_list(assoc, page_view: 'list', show_template: true)).to match_array(expected)
+      end
     end
   end
 end
