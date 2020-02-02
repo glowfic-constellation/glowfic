95bf7cdb085a45b138f2557cbcc10e25f5ed6084
teceler <19716939+teceler@users.noreply.github.com>
2020-02-01 22:30:50 -0600

    Create written association

--- app/models/post.rb
+++ app/models/post.rb
@@ -38,6 +38,8 @@ class Post < ApplicationRecord
 
   has_many :notifications, inverse_of: :post, dependent: :destroy
 
+  has_one :written, -> { where(reply_order: 0) }, class_name: 'Reply', inverse_of: :post
+
   attr_accessor :is_import
   attr_writer :skip_edited
 
