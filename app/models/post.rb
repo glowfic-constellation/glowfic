19c0efa01dc1a75aa6813dad7e58d40dcc7e6d5b
teceler <19716939+teceler@users.noreply.github.com>
2020-02-02 15:11:10 -0600

    Set user when creating written

--- app/models/post.rb
+++ app/models/post.rb
@@ -358,6 +358,7 @@ class Post < ApplicationRecord
   def create_written
     replies.create!(
       reply_order: 0,
+      user: user,
       content: content,
       icon: icon,
       character: character,
