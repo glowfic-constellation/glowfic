728e997ab05b20c6a7f69f94fd8572b93e8310d0
teceler <19716939+teceler@users.noreply.github.com>
2020-02-02 17:23:47 -0600

    Correct test expectations

--- spec/controllers/replies/restore_spec.rb
+++ spec/controllers/replies/restore_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe RepliesController, 'POST restore' do
     post_attributes.each do |key, val|
       expect(new_attributes[key]).to eq(val)
     end
-    expect(reloaded_post.replies.pluck(:reply_order).sort).to eq(1.upto(5).to_a)
+    expect(reloaded_post.replies.pluck(:reply_order).sort).to eq(0.upto(5).to_a)
   end
 
   it "handles first reply deletion" do
@@ -78,7 +78,7 @@ RSpec.describe RepliesController, 'POST restore' do
     post_attributes.each do |key, val|
       expect(new_attributes[key]).to eq(val)
     end
-    expect(reloaded_post.replies.pluck(:reply_order).sort).to eq(1.upto(3).to_a)
+    expect(reloaded_post.replies.pluck(:reply_order).sort).to eq(0.upto(3).to_a)
   end
 
   it "handles last reply deletion" do
@@ -100,13 +100,13 @@ RSpec.describe RepliesController, 'POST restore' do
     end
     expect(reloaded_post.last_user).to eq(deleted_reply.user)
     expect(reloaded_post.last_reply).to eq(deleted_reply)
-    expect(reloaded_post.replies.pluck(:reply_order).sort).to eq(1.upto(3).to_a)
+    expect(reloaded_post.replies.pluck(:reply_order).sort).to eq(0.upto(3).to_a)
   end
 
   it "handles only reply deletion" do
     rpost = create(:post)
     expect(rpost.last_user).to eq(rpost.user)
-    expect(rpost.last_reply).to be_nil
+    expect(rpost.last_reply).to eq(rpost.written)
 
     deleted_reply = Timecop.freeze(rpost.reload.tagged_at + 1.day) { create(:reply, post: rpost) }
     rpost = Post.find(rpost.id)
@@ -116,11 +116,12 @@ RSpec.describe RepliesController, 'POST restore' do
     deleted_reply.destroy!
     rpost = Post.find(rpost.id)
     expect(rpost.last_user).to eq(rpost.user)
-    expect(rpost.last_reply).to be_nil
+    expect(rpost.last_reply).to eq(rpost.written)
 
     login_as(deleted_reply.user)
     post :restore, params: { id: deleted_reply.id }
     rpost = Post.find(rpost.id)
+    expect(deleted_reply.reload.reply_order).to eq(1)
     expect(rpost.last_user).to eq(deleted_reply.user)
     expect(rpost.last_reply).to eq(deleted_reply)
   end
