c6dc9032ca0d4bbd7eb60798f159a02f02ab60ed
teceler <19716939+teceler@users.noreply.github.com>
2020-02-03 20:20:50 -0600

    Refactor script

--- script/migrate_replies.rb
+++ script/migrate_replies.rb
@@ -1,6 +1,14 @@
-Reply.where(new_order: nil).in_batches do |replies|
+BATCH_SIZE = 1000
+
+replies = Reply.where(new_order: nil)
+
+total = replies.count/BATCH_SIZE
+
+total.times do |i|
   Reply.transaction do
-    puts "Migrating replies #{replies.first.id} through #{replies.last.id} at #{Time.zone.now}"
-    replies.update_all('new_order = reply_order + 1') # rubocop:disable Rails/SkipsModelValidations
+    local_replies = Reply.where(new_order: nil).order(:id).limit(BATCH_SIZE)
+    first_id = local_replies.order(id: :asc).limit(1).pluck(:id).first
+    puts "Migrating replies from #{first_id} (batch #{i+1}/#{total})"
+    local_replies.update_all('new_order = reply_order + 1') # rubocop:disable Rails/SkipsModelValidations
   end
 end
