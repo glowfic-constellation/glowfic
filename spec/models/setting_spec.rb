a2b5d486fd79208631775a7e31189c9281cf0789
teceler <19716939+teceler@users.noreply.github.com>
2020-02-09 12:20:29 -0600

    Remove unused has_items? method and tests

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -1,29 +1,6 @@
 require "spec_helper"
 
 RSpec.describe Setting do
-  describe "#has_items?" do
-    it "has items with a post" do
-      harry_potter = create(:setting, name: 'Harry Potter')
-      post = create(:post, subject: 'Harry Potter and the Goblet of Fire')
-      harry_potter.posts << post
-      expect(harry_potter).to have_items
-    end
-
-    it "has items with a character" do
-      harry_potter = create(:setting, name: 'Harry Potter')
-      hermione = create(:character, name: 'Hermione Granger')
-      harry_potter.characters << hermione
-      expect(harry_potter).to have_items
-    end
-
-    it "has items with a child setting" do
-      harry_potter = create(:setting, name: 'Harry Potter')
-      hazel = create(:setting, name: 'Hazel')
-      harry_potter.child_settings << hazel
-      expect(harry_potter.has_items?).to eq(true)
-    end
-  end
-
   context "tags" do
     it "creates only in-memory tags on invalid create" do
       harry_potter = create(:setting, name: 'Harry Potter')
