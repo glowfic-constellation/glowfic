c26f1921174b88da896c38db78ce560392233099
teceler <19716939+teceler@users.noreply.github.com>
2020-02-27 09:49:37 -0600

    Regenerate migration

--- db/migrate/20200227154404_remove_post_written.rb
+++ db/migrate/20200227154404_remove_post_written.rb
@@ -1,27 +1,11 @@
 class SplitPostTextIntoReplies < ActiveRecord::Migration[5.1]
   def up
-    execute <<~SQL
-      UPDATE replies
-      SET reply_order = reply_order + 1;
-    SQL
-
-    Post.find_each do |post|
-      reply = Reply.create!(
-        post_id: post.id,
-        user_id: post.user_id,
-        character_id: post.character_id,
-        character_alias_id: post.character_alias_id,
-        icon_id: post.icon_id,
-        content: post.content,
-        created_at: post.created_at,
-        updated_at: post.updated_at,
-        skip_post_update: true
-      )
-      reply.update_columns(reply_order: 0, created_at: post.created_at, updated_at: post.edited_at)
-    end
     Post.auditing_enabled = false
     change_table :posts do |t|
-      t.remove :character_id, :character_alias_id, :icon_id, :content
+      t.remove :character_id
+      t.remove :character_alias_id
+      t.remove :icon_id
+      t.remove :content
     end
     Post.auditing_enabled = true
   end
@@ -34,7 +18,7 @@ class SplitPostTextIntoReplies < ActiveRecord::Migration[5.1]
       t.text :content
     end
     Post.find_each do |post|
-      reply = post.replies.find_by(reply_order: 0)
+      reply = post.written
       if post.user_id == reply.user_id
         post.character_id = reply.character_id
         post.character_alias_id = reply.character_alias_id
@@ -44,14 +28,8 @@ class SplitPostTextIntoReplies < ActiveRecord::Migration[5.1]
       else
         raise "Post user does not match initial reply's user"
       end
-      reply.delete
     end
 
-    execute <<~SQL
-      UPDATE replies
-      SET reply_order = reply_order - 1;
-    SQL
-    
     add_index :posts, :character_id
     add_index :posts, :icon_id
     execute "CREATE INDEX idx_fts_post_content ON posts USING gin(to_tsvector('english', coalesce(\"posts\".\"content\"::text, '')))"
