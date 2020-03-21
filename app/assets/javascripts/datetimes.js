a79fc5f8804810304bbcc6e12e2f3b4212227eaa
Edward Jones <throne3d@gmail.com>
2020-03-21 13:07:13 -0300

    Timezoneify timestamps for logged out users
    Also affects flat posts - we cache the timestamps
    there, so I'm improving this to update them with
    JavaScript.

---
+++ app/assets/javascripts/datetimes.js
@@ -0,0 +1,15 @@
+/* global gon */
+var options = {
+  dateStyle: 'medium',
+  timeStyle: 'short',
+};
+var format = Intl.DateTimeFormat('en-US', options);
+
+window.onload = function() {
+  if (gon.logged_in) return;
+  var times = document.getElementsByTagName('time');
+  Array.from(times).forEach(function(time) {
+    var datetime = new Date(time.dateTime);
+    time.innerText = format.format(datetime);
+  });
+};
