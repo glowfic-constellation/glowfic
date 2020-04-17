d92b6d2d3082f99f041d22227ae456cf63c31f80
Marri Nikari <xxstealthestarsxx@gmail.com>
2020-04-17 11:37:10 -0400

    Move back function

--- app/helpers/character_helper.rb
+++ app/helpers/character_helper.rb
@@ -17,14 +17,4 @@ module CharacterHelper
     link_params = params.permit(:character_split, :retired, :view).to_h.merge(link_params)
     url_for(**link_params.symbolize_keys)
   end
-
-  # logic replicated from page_view
-  def character_split
-    return @character_split if @character_split
-    if logged_in?
-      @character_split = params[:character_split] || current_user.default_character_split
-    else
-      @character_split = session[:character_split] = params[:character_split] || session[:character_split] || 'template'
-    end
-  end
 end
