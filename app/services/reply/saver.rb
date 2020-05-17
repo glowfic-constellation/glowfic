6218743d9e405dc98ea9524694b026d40a1b0672
teceler <19716939+teceler@users.noreply.github.com>
2020-05-17 18:12:37 -0500

    Fix

--- app/services/reply/saver.rb
+++ app/services/reply/saver.rb
@@ -1,15 +1,16 @@
 class Reply::Saver < Generic::Saver
   def create
-    if reply.post.present?
-      last_seen_reply_order = reply.post.last_seen_reply_for(@user).try(:reply_order)
-      @unseen_replies = reply.post.replies.ordered.paginate(page: 1, per_page: 10)
+    @reply.assign_attribute(permitted_params)
+    if @reply.post.present?
+      last_seen_reply_order = @reply.post.last_seen_reply_for(@user).try(:reply_order)
+      @unseen_replies = @reply.post.replies.ordered.paginate(page: 1, per_page: 10)
       @unseen_replies = @unseen_replies.where('reply_order > ?', last_seen_reply_order) if last_seen_reply_order.present?
       most_recent_unseen_reply = @unseen_replies.last
 
       if @params[:allow_dupe].blank?
-        last_by_user = reply.post.replies.where(user_id: reply.user_id).ordered.last
+        last_by_user = @reply.post.replies.where(user_id: @reply.user_id).ordered.last
         match_attrs = ['content', 'icon_id', 'character_id', 'character_alias_id']
-        if last_by_user.present? && last_by_user.attributes.slice(*match_attrs) == reply.attributes.slice(*match_attrs)
+        if last_by_user.present? && last_by_user.attributes.slice(*match_attrs) == @reply.attributes.slice(*match_attrs)
           @error_message = "This looks like a duplicate. Did you attempt to post this twice? Please resubmit if this was intentional."
           @allow_dupe = true
           if @unseen_replies.count == 0 || (@unseen_replies.count == 1 && most_recent_unseen_reply.id == last_by_user.id)
@@ -23,7 +24,7 @@ class Reply::Saver < Generic::Saver
       end
 
       if most_recent_unseen_reply.present?
-        reply.post.mark_read(@user, reply.post.read_time_for(@unseen_replies))
+        @reply.post.mark_read(@user, reply.post.read_time_for(@unseen_replies))
         num = @unseen_replies.count
         pluraled = num > 1 ? "have been #{num} new replies" : "has been 1 new reply"
         @error_message = "There #{pluraled} since you last viewed this post."
