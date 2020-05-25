db79e8e74a8d3808b44b84802facd9f7c62d7c4c
Marri Nikari <xxstealthestarsxx@gmail.com>
2020-05-25 17:43:50 -0400

    Use a check_token before_action instead of endpoint-specific login_optional ones to be more comprehensive

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::PostsController < Api::ApiController
   before_action :login_required, except: [:index, :show]
-  before_action :login_optional, only: [:index, :show]
 
   resource_description do
     description 'Viewing and editing posts'
