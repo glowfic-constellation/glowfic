b9941535225675d190e42571fe5926605977fad2
teceler <19716939+teceler@users.noreply.github.com>
2020-06-08 09:07:00 -0500

    Fix merge problem

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -782,7 +782,7 @@ RSpec.describe Post do
 
     it "resets with warning replacement without changing read time" do
       at_time = 3.days.ago
-      post.mark_read(user, at_time, true)
+      post.mark_read(user, at_time: at_time, force: true)
       post.update!(content_warning_list: [create(:content_warning)])
       expect(post.reload).to be_show_warnings_for(user)
       expect(post.last_read(user)).to be_the_same_time_as(at_time)
