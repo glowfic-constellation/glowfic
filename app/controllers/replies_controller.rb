2e0139d5fb78c628401147fceb10c4d0059bedd4
teceler <19716939+teceler@users.noreply.github.com>
2020-06-28 19:10:44 -0500

    Slightly refactor reply#create

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -119,13 +119,13 @@ class RepliesController < WritableController
       preview and return
     end
 
-    reply = Reply.new(permitted_params)
-    reply.user = current_user
+    @reply = Reply.new(permitted_params)
+    @reply.user = current_user
     process_npc(reply, permitted_character_params)
 
-    if reply.post.present?
-      last_seen_reply_order = reply.post.last_seen_reply_for(current_user).try(:reply_order)
-      @unseen_replies = reply.post.replies.ordered.paginate(page: 1, per_page: 10)
+    if @reply.post.present?
+      last_seen_reply_order = @reply.post.last_seen_reply_for(current_user).try(:reply_order)
+      @unseen_replies = @reply.post.replies.ordered.paginate(page: 1, per_page: 10)
       if last_seen_reply_order.present?
         @unseen_replies = @unseen_replies.where('reply_order > ?', last_seen_reply_order)
         @audits = Audited::Audit.where(auditable_id: @unseen_replies.map(&:id)).group(:auditable_id).count
@@ -133,25 +133,22 @@ class RepliesController < WritableController
       most_recent_unseen_reply = @unseen_replies.last
 
       if params[:allow_dupe].blank?
-        last_by_user = reply.post.replies.where(user_id: reply.user_id).ordered.last
+        last_by_user = @reply.post.replies.where(user_id: @reply.user_id).ordered.last
         match_attrs = ['content', 'icon_id', 'character_id', 'character_alias_id']
-        if last_by_user.present? && last_by_user.attributes.slice(*match_attrs) == reply.attributes.slice(*match_attrs)
+        if last_by_user.present? && last_by_user.attributes.slice(*match_attrs) == @reply.attributes.slice(*match_attrs)
           flash.now[:error] = "This looks like a duplicate. Did you attempt to post this twice? Please resubmit if this was intentional."
           @allow_dupe = true
-          if most_recent_unseen_reply.nil? || (most_recent_unseen_reply.id == last_by_user.id && @unseen_replies.count == 1)
-            @reply = reply
-            preview
-          else
+          unless most_recent_unseen_reply.nil? || (most_recent_unseen_reply.id == last_by_user.id && @unseen_replies.count == 1)
             draft = make_draft(false)
             @reply = ReplyDraft.reply_from_draft(draft)
-            preview
           end
+          preview
           return
         end
       end
 
       if most_recent_unseen_reply.present?
-        reply.post.mark_read(current_user, at_time: reply.post.read_time_for(@unseen_replies))
+        @reply.post.mark_read(current_user, at_time: @reply.post.read_time_for(@unseen_replies))
         num = @unseen_replies.count
         pluraled = num > 1 ? "have been #{num} new replies" : "has been 1 new reply"
         flash.now[:error] = "There #{pluraled} since you last viewed this post."
@@ -162,7 +159,7 @@ class RepliesController < WritableController
     end
 
     begin
-      reply.save!
+      @reply.save!
     rescue ActiveRecord::RecordInvalid => e
       render_errors(reply, action: 'created', now: true, err: e)
 
