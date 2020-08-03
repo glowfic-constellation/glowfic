e0dcbf48f5bd203bf688453afed969c2859bab49
teceler <19716939+teceler@users.noreply.github.com>
2020-08-02 23:44:28 -0500

    Fix script

--- script/create_written.rb
+++ script/create_written.rb
@@ -3,15 +3,16 @@ exclude = Reply.where(reply_order: 0).select(:post_id)
 Post.where.not(id: exclude).find_in_batches(batch_size: 500) do |posts|
   Post.transaction do
     puts "Creating writtens for posts #{posts.first.id} through #{posts.last.id}"
-    post.each do |post|
+    posts.each do |post|
       reply = post.replies.create!(
         reply_order: 0,
-        content: content,
-        icon: icon,
-        character: character,
-        character_alias: character_alias,
-        created_at: created_at,
-        updated_at: edited_at,
+        user: post.user,
+        content: post.content,
+        icon: post.icon,
+        character: post.character,
+        character_alias: post.character_alias,
+        created_at: post.created_at,
+        updated_at: post.edited_at,
       )
       audits = post.audits.where("audits.audited_changes ?| array['content', 'icon_id', 'character_id', 'character_alias_id']")
       audits.each do |audit|
