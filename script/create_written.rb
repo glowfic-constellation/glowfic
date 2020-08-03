b5aa6d2d763be32b05ab3e0a43f8a66ff30c6ffd
teceler <19716939+teceler@users.noreply.github.com>
2020-08-02 23:37:48 -0500

    Modify migration script to copy relevant audits

--- script/create_written.rb
+++ script/create_written.rb
@@ -4,7 +4,7 @@ Post.where.not(id: exclude).find_in_batches(batch_size: 500) do |posts|
   Post.transaction do
     puts "Creating writtens for posts #{posts.first.id} through #{posts.last.id}"
     post.each do |post|
-      post.replies.create!(
+      reply = post.replies.create!(
         reply_order: 0,
         content: content,
         icon: icon,
@@ -13,6 +13,19 @@ Post.where.not(id: exclude).find_in_batches(batch_size: 500) do |posts|
         created_at: created_at,
         updated_at: edited_at,
       )
+      audits = post.audits.where("audits.audited_changes ?| array['content', 'icon_id', 'character_id', 'character_alias_id']")
+      audits.each do |audit|
+        reply.audits.create!(
+          user_id: audit.user_id,
+          user_type: audit.user_type,
+          action: audit.action,
+          audited_changes: audit.audited_changes,
+          version: audit.version,
+          remote_address: audit.remote_address,
+          created_at: audit.created_at,
+          request_uuid: audit.request_uuid,
+        )
+      end
     end
   end
 end
