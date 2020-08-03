c4d067a42c35d5f946b5d3fcac172c3bfc7eca50
teceler <19716939+teceler@users.noreply.github.com>
2020-08-03 11:10:27 -0500

    Add a guard clause

--- script/create_written.rb
+++ script/create_written.rb
@@ -1,32 +1,36 @@
-exclude = Reply.where(reply_order: 0).select(:post_id)
+def create_writtens
+  exclude = Reply.where(reply_order: 0).select(:post_id)
 
-Post.where.not(id: exclude).find_in_batches(batch_size: 500) do |posts|
-  Post.transaction do
-    puts "Creating writtens for posts #{posts.first.id} through #{posts.last.id}"
-    posts.each do |post|
-      reply = post.replies.create!(
-        reply_order: 0,
-        user: post.user,
-        content: post.content,
-        icon: post.icon,
-        character: post.character,
-        character_alias: post.character_alias,
-        created_at: post.created_at,
-        updated_at: post.edited_at,
-      )
-      audits = post.audits.where("audits.audited_changes ?| array['content', 'icon_id', 'character_id', 'character_alias_id']")
-      audits.each do |audit|
-        reply.audits.create!(
-          user_id: audit.user_id,
-          user_type: audit.user_type,
-          action: audit.action,
-          audited_changes: audit.audited_changes,
-          version: audit.version,
-          remote_address: audit.remote_address,
-          created_at: audit.created_at,
-          request_uuid: audit.request_uuid,
+  Post.where.not(id: exclude).find_in_batches(batch_size: 500) do |posts|
+    Post.transaction do
+      puts "Creating writtens for posts #{posts.first.id} through #{posts.last.id}"
+      posts.each do |post|
+        reply = post.replies.create!(
+          reply_order: 0,
+          user: post.user,
+          content: post.content,
+          icon: post.icon,
+          character: post.character,
+          character_alias: post.character_alias,
+          created_at: post.created_at,
+          updated_at: post.edited_at,
         )
+        audits = post.audits.where("audits.audited_changes ?| array['content', 'icon_id', 'character_id', 'character_alias_id']")
+        audits.each do |audit|
+          reply.audits.create!(
+            user_id: audit.user_id,
+            user_type: audit.user_type,
+            action: audit.action,
+            audited_changes: audit.audited_changes,
+            version: audit.version,
+            remote_address: audit.remote_address,
+            created_at: audit.created_at,
+            request_uuid: audit.request_uuid,
+          )
+        end
       end
     end
   end
 end
+
+create_writtens if $PROGRAM_NAME == __FILE__
