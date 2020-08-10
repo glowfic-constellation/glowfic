1ac0c3604fb76559259f28e87ada5de4844fda41
teceler <19716939+teceler@users.noreply.github.com>
2020-08-09 21:52:21 -0500

    Use before/after hooks to enable/disable auditing

--- spec/script/create_written_spec.rb
+++ spec/script/create_written_spec.rb
@@ -51,63 +51,69 @@ RSpec.describe "#create_writtens" do # rubocop:disable Rspec/DescribeClass
     expect(written.updated_at).to be_the_same_time_as(post.edited_at)
   end
 
-  it "handles audits correctly" do
-    Post.auditing_enabled = true
-    Reply.auditing_enabled = true
-
-    user = create(:user)
-    icon = create(:icon, user: user)
-    character = create(:template_character, default_icon: icon, user: user)
-    calias = create(:alias, character: character)
-
-    post = create(:post, character: character, icon: icon, user: user, skip_written: true)
-
-    Timecop.freeze(Time.zone.now + 2.hours) do
-      post.update!(subject: 'new subject!')
+  context "with audits" do
+    before(:each) do
+      Post.auditing_enabled = true
+      Reply.auditing_enabled = true
     end
 
-    Timecop.freeze(Time.zone.now + 4.hours) do
-      post.update!(content: 'new content!')
+    after(:each) do
+      Post.auditing_enabled = false
+      Reply.auditing_enabled = false
     end
 
-    Timecop.freeze(Time.zone.now + 6.hours) do
-      post.update!(status: :complete)
+    it "handles audits correctly" do
+      user = create(:user)
+      icon = create(:icon, user: user)
+      character = create(:template_character, default_icon: icon, user: user)
+      calias = create(:alias, character: character)
+
+      post = create(:post, character: character, icon: icon, user: user, skip_written: true)
+
+      Timecop.freeze(Time.zone.now + 2.hours) do
+        post.update!(subject: 'new subject!')
+      end
+
+      Timecop.freeze(Time.zone.now + 4.hours) do
+        post.update!(content: 'new content!')
+      end
+
+      Timecop.freeze(Time.zone.now + 6.hours) do
+        post.update!(status: :complete)
+      end
+
+      Timecop.freeze(Time.zone.now + 8.hours) do
+        post.update!(character_alias: calias)
+      end
+      expect(post.written).to be_nil
+
+      allow(STDOUT).to receive(:puts).with("Creating writtens for posts #{post.id} through #{post.id}")
+      Timecop.freeze(Time.zone.now + 12.hours) do
+        expect { create_writtens }.to change{Reply.count}.by(1)
+      end
+      written = Reply.last
+      expect(written.reply_order).to eq(0)
+      expect(written.user).to eq(post.user)
+      expect(written.content).to eq(post.content)
+      expect(written.icon_id).to eq(icon.id)
+      expect(written.character_id).to eq(character.id)
+      expect(written.character_alias_id).to eq(calias.id)
+      expect(written.created_at).to be_the_same_time_as(post.created_at)
+      expect(written.updated_at).to be_the_same_time_as(post.edited_at)
+      expect(written.audits.count).to eq(3)
+
+      create_audit = written.audits.first
+      expect(create_audit.action).to eq('create')
+      expect(create_audit.audited_changes).to eq(post.audits.first.audited_changes)
+
+      update_audit1 = written.audits.second
+      expect(update_audit1.action).to eq('update')
+      expect(update_audit1.audited_changes).to eq({'content' => ['test content', 'new content!']})
+
+      update_audit2 = written.audits.third
+      expect(update_audit2.action).to eq('update')
+      expect(update_audit2.audited_changes).to eq({'character_alias_id' => [nil, calias.id]})
     end
-
-    Timecop.freeze(Time.zone.now + 8.hours) do
-      post.update!(character_alias: calias)
-    end
-    expect(post.written).to be_nil
-
-    allow(STDOUT).to receive(:puts).with("Creating writtens for posts #{post.id} through #{post.id}")
-    Timecop.freeze(Time.zone.now + 12.hours) do
-      expect { create_writtens }.to change{Reply.count}.by(1)
-    end
-    written = Reply.last
-    expect(written.reply_order).to eq(0)
-    expect(written.user).to eq(post.user)
-    expect(written.content).to eq(post.content)
-    expect(written.icon_id).to eq(icon.id)
-    expect(written.character_id).to eq(character.id)
-    expect(written.character_alias_id).to eq(calias.id)
-    expect(written.created_at).to be_the_same_time_as(post.created_at)
-    expect(written.updated_at).to be_the_same_time_as(post.edited_at)
-    expect(written.audits.count).to eq(3)
-
-    create_audit = written.audits.first
-    expect(create_audit.action).to eq('create')
-    expect(create_audit.audited_changes).to eq(post.audits.first.audited_changes)
-
-    update_audit1 = written.audits.second
-    expect(update_audit1.action).to eq('update')
-    expect(update_audit1.audited_changes).to eq({'content' => ['test content', 'new content!']})
-
-    update_audit2 = written.audits.third
-    expect(update_audit2.action).to eq('update')
-    expect(update_audit2.audited_changes).to eq({'character_alias_id' => [nil, calias.id]})
-
-    Post.auditing_enabled = false
-    Reply.auditing_enabled = false
   end
 
   it "handles multiple posts" do
