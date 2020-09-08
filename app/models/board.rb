5f49336caf9c62c2637ad5dc88a1d4993062a184
teceler <19716939+teceler@users.noreply.github.com>
2020-09-07 20:45:16 -0500

    Disable rubocop on more update_all statements

--- app/models/board.rb
+++ app/models/board.rb
@@ -59,11 +59,11 @@ class Board < ApplicationRecord
     # this should ONLY be called by an admin for emergency fixes
     board_sections.ordered.each_with_index do |section, index|
       next if section.section_order == index
-      section.update_columns(section_order: index)
+      section.update_columns(section_order: index) # rubocop:disable Rails/SkipsModelValidations
     end
     posts.where(section_id: nil).ordered_in_section.each_with_index do |post, index|
       next if post.section_order == index
-      post.update_columns(section_order: index)
+      post.update_columns(section_order: index) # rubocop:disable Rails/SkipsModelValidations
     end
   end
 end
