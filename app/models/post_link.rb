45aabffa63a4d8a586ee3d0706fdb2bf4b9d672d
teceler <19716939+teceler@users.noreply.github.com>
2020-09-11 11:03:38 -0500

    Move options to a const on PostLink model

--- app/models/post_link.rb
+++ app/models/post_link.rb
@@ -3,4 +3,6 @@ class PostLink < ApplicationRecord
   belongs_to :linked_post, class_name: 'Post', inverse_of: :linking_post_joins, optional: false
 
   validates :linking_post, uniqueness: { scope: :linked_post }
+
+  TYPES = ['inspired by', 'an AU of', 'a sequel to', 'cocurrent with']
 end
