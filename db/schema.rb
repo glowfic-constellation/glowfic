22289cddafa875d25c8d4d2fac0a298c4308d2eb
Nova Devereaux <nova@noblejury.com>
2021-03-12 16:26:41 -0800

    Add toggle defaulted off in user settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_173619) do
+ActiveRecord::Schema.define(version: 2021_03_12_162000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -455,6 +455,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_173619) do
     t.integer "role_id"
     t.integer "tos_version"
     t.boolean "deleted", default: false
+    t.boolean "replies_owed_indicator", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
