f28a983abd9cc190efcc5e3d938fe945cb0a51f1
teceler <19716939+teceler@users.noreply.github.com>
2021-05-18 12:17:39 -0500

    Tweak Tag::List initialization

--- app/models/tag/list.rb
+++ app/models/tag/list.rb
@@ -1,13 +1,13 @@
 class Tag::List < Array
   def initialize(list=[])
     super
-    clean_tags unless self == []
-    self
+    return if self == []
+    clean_tags!
   end
 
   private
 
-  def clean_tags
+  def clean_tags!
     reject!(&:blank?)
     map!(&:to_s)
     map! { |name| name.start_with?('_') ? name[1..] : name }
