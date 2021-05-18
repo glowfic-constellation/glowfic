ee449b8441bd0da4ac146c3d284ea6b79bd3d8f3
teceler <19716939+teceler@users.noreply.github.com>
2021-05-18 18:45:23 -0500

    Create SettingsController

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+class SettingsController < ApplicationController
+  include Settinggable
+
+  before_action :login_required, except: [:index, :show]
+  before_action :find_model, except: :index
+  before_action :require_permission, except: [:index, :show, :destroy]
+
+  def index
+    @settings = SettingSearcher.new.search(tag_name: params[:name], tag_type: params[:view], page: page)
+    @post_counts = Post.visible_to(current_user).joins(setting_posts: :setting).where(setting_posts: {setting_id: @settings.map(&:id)})
+    @post_counts = @post_counts.group('setting_posts.setting_id').count
+    @page_title = 'Settings'
+    use_javascript('tags/index')
+  end
+
+  def show
+    @page_title = @setting.name.to_s
+    @view = params[:view]
+    @meta_og = og_data
+
+    if @view == 'posts'
+      @posts = posts_from_relation(@setting.posts.ordered)
+    elsif @view == 'characters'
+      @characters = @setting.characters.includes(:user, :template).ordered.paginate(page: page)
+    elsif @view != 'settings'
+      @view = 'info'
+    end
+  end
+
+  def edit
+    @page_title = "Edit Setting: #{@setting.name}"
+    build_editor
+  end
+
+  def update
+    @setting.assign_attributes(permitted_params)
+
+    begin
+      Setting.transaction do
+        @setting.parent_settings = process_tags(Setting, obj_param: :tag, id_param: :parent_setting_ids) if @setting.is_a?(Setting)
+        @setting.save!
+      end
+    rescue ActiveRecord::RecordInvalid
+      flash.now[:error] = {
+        message: "Setting could not be saved because of the following problems:",
+        array: @setting.errors.full_messages
+      }
+      @page_title = "Edit Setting: #{@setting.name}"
+      build_editor
+      render :edit
+    else
+      flash[:success] = "Setting saved!"
+      redirect_to tag_path(@setting)
+    end
+  end
+
+  def destroy
+    unless @setting.deletable_by?(current_user)
+      flash[:error] = "You do not have permission to edit this tag."
+      redirect_to tag_path(@setting) and return
+    end
+
+    begin
+      @setting.destroy!
+    rescue ActiveRecord::RecordNotDestroyed
+      flash[:error] = {
+        message: "Setting could not be deleted.",
+        array: @setting.errors.full_messages
+      }
+      redirect_to tag_path(@setting)
+    else
+      flash[:success] = "Setting deleted."
+
+      url_params = {}
+      url_params[:page] = page if params[:page].present?
+      url_params[:view] = params[:view] if params[:view].present?
+      redirect_to tags_path(url_params)
+    end
+  end
+
+  private
+
+  def find_model
+    unless (@setting = Setting.find_by_id(params[:id]))
+      flash[:error] = "Setting could not be found."
+      redirect_to tags_path
+    end
+  end
+
+  def require_permission
+    unless @setting.editable_by?(current_user)
+      flash[:error] = "You do not have permission to edit this tag."
+      redirect_to tag_path(@setting)
+    end
+  end
+
+  def build_editor
+    return unless @setting.is_a?(Setting)
+    use_javascript('tags/edit')
+  end
+
+  def og_data
+    desc = []
+    desc << generate_short(@setting.description) if @setting.description.present?
+    stats = []
+    post_count = @setting.posts.privacy_public.count
+    stats << "#{post_count} " + "post".pluralize(post_count) if post_count > 0
+    character_count = @setting.characters.count
+    stats << "#{character_count} " + "character".pluralize(character_count) if character_count > 0
+    desc << stats.join(', ')
+    title = [@setting.name]
+    title << @setting.user.username if @setting.owned? && !@setting.user.deleted?
+    title << @setting.type.titleize
+    {
+      url: tag_url(@setting),
+      title: title.join(' · '),
+      description: desc.join("\n"),
+    }
+  end
+
+  def permitted_params
+    permitted = [:type, :description, :owned]
+    permitted.insert(0, :name, :user_id) if current_user.admin? || @setting.user == current_user
+    params.fetch(:tag, {}).permit(permitted)
+  end
+end
