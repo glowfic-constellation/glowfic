406efa6dfff5e65da529624e3cfa7b05dbbec2ec
teceler <19716939+teceler@users.noreply.github.com>
2021-05-18 16:20:35 -0500

    stash

--- app/models/setting/character.rb
+++ app/models/setting/character.rb
@@ -1,4 +1,6 @@
 class Setting::Character < ApplicationRecord
+  self.table_name = 'setting_characters'
+
   belongs_to :character, inverse_of: :setting_characters, optional: false
   belongs_to :setting, inverse_of: :setting_characters, optional: false
 
--- app/models/setting/post.rb
+++ app/models/setting/post.rb
@@ -1,4 +1,6 @@
 class Setting::Post < ApplicationRecord
+  self.table_name = 'setting_posts'
+
   belongs_to :post, inverse_of: :setting_posts, optional: false
   belongs_to :setting, inverse_of: :setting_posts, optional: false
 
