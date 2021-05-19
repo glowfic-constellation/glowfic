35dd7b62744f90a9648a47f1133e5a098af12060
teceler <19716939+teceler@users.noreply.github.com>
2021-05-19 10:33:27 -0500

    Restore Tag::SettingTag (for migration)

---
+++ app/models/tag/setting_tag.rb
@@ -0,0 +1,8 @@
+class Tag::SettingTag < ApplicationRecord
+  self.table_name = 'tag_tags'
+
+  belongs_to :child_setting, class_name: 'Setting', foreign_key: :tagged_id, inverse_of: :child_setting_tags, optional: true
+  belongs_to :parent_setting, class_name: 'Setting', foreign_key: :tag_id, inverse_of: :parent_setting_tags, optional: true
+
+  validates :child_setting, uniqueness: { scope: :parent_setting }
+end
