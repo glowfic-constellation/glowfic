132a511a2aa0cf172118efb550a15df60332f869
teceler <19716939+teceler@users.noreply.github.com>
2021-05-19 15:17:36 -0500

    Potential speed improvement

--- db/migrate/20210519034032_migrate_tags.rb
+++ db/migrate/20210519034032_migrate_tags.rb
@@ -1,5 +1,7 @@
 class MigrateTags < ActiveRecord::Migration[5.2]
   def up
+    add_column :tags, :new_id, :integer
+
     batch_size = 1000
     times = (Tag.count.to_f / batch_size).ceil
     times.times do |step|
@@ -15,8 +17,8 @@ class MigrateTags < ActiveRecord::Migration[5.2]
       )
 
       settings.rows.each do |row|
-        _id, user_id, name, created_at, updated_at, _type, description, owned = row
-        Setting.create!(
+        id, user_id, name, created_at, updated_at, _type, description, owned = row
+        new = Setting.create!(
           user_id: user_id,
           name: name,
           description: description,
@@ -24,34 +26,38 @@ class MigrateTags < ActiveRecord::Migration[5.2]
           created_at: created_at,
           updated_at: updated_at,
         )
+        connection.exec_update(
+          <<~SQL
+            UPDATE tags
+            SET new_id = #{new.id}
+            WHERE id = #{id}
+          SQL
+        )
       end
     end
 
     CharacterTag.joins(:tag).where("tags.type = 'Setting'").in_batches do |batch|
-      char_tags = batch.pluck('tags.name', :character_id)
-      char_tags.each do |name, character_id|
-        setting = Setting.find_by(name: name)
-        Setting::Character.create!(setting: setting, character_id: character_id)
+      char_tags = batch.pluck('tags.new_id', :character_id)
+      char_tags.each do |setting_id, character_id|
+        Setting::Character.create!(setting_id: setting_id, character_id: character_id)
       end
     end
 
     PostTag.joins(:tag).where("tags.type = 'Setting'").in_batches do |batch|
-      post_tags = batch.pluck('tags.name', :post_id)
-      post_tags.each do |name, post_id|
-        setting = Setting.find_by(name: name)
-        Setting::Post.create!(setting: setting, post_id: post_id)
+      post_tags = batch.pluck('tags.new_id', :post_id)
+      post_tags.each do |setting_id, post_id|
+        Setting::Post.create!(setting_id: setting_id, post_id: post_id)
       end
     end
 
     tag_tags = Tag::SettingTag.joins('INNER JOIN tags t1 ON t1.id = tag_tags.tag_id').joins('INNER JOIN tags t2 ON t2.id = tag_tags.tagged_id')
     tag_tags.in_batches do |batch|
-      batch = batch.pluck('t1.name', 't2.name')
-      batch.each do |tag_name, tagged_name|
-        tag = Setting.find_by(name: tag_name)
-        tagged = Setting.find_by(name: tagged_name)
-        Setting::SettingTag.create!(tag_id: tag.id, tagged_id: tagged.id)
+      batch = batch.pluck('t1.new_id', 't2.new_id')
+      batch.each do |tag_id, tagged_id|
+        Setting::SettingTag.create!(tag_id: tag_id, tagged_id: tagged_id)
       end
     end
+    remove_column :tags, :new_id
   end
 
   def down
