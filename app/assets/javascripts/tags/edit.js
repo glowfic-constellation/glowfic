f950d170dc50b3920939c56db7f152ec51ed00d1
teceler <19716939+teceler@users.noreply.github.com>
2021-05-18 21:58:45 -0500

    Fixes

--- app/assets/javascripts/tags/edit.js
+++ app/assets/javascripts/tags/edit.js
@@ -1,5 +1,5 @@
 /* global createTagSelect */
 $(document).ready(function() {
-  const tagID = $("#tag_parent_setting_ids").data('tag-id');
+  const tagID = $("#setting_parent_setting_ids").data('tag-id');
   createTagSelect("Setting", "parent_setting", "setting", {tag_id: tagID});
 });
--- app/controllers/api/v1/settings_controller.rb
+++ app/controllers/api/v1/settings_controller.rb
@@ -2,26 +2,26 @@ class Api::V1::TagsController < Api::ApiController
   before_action :find_tag, except: :index
 
   resource_description do
-    description 'Viewing tags'
+    description 'Viewing settings'
   end
 
-  api :GET, '/tags', 'Load all the tags of the specified type that match the given query, results ordered by name'
+  api :GET, '/tags', 'Load all the settings that match the given query, results ordered by name'
   param :q, String, required: false, desc: "Query string"
   param :page, :number, required: false, desc: 'Page in results (25 per page)'
-  param :tag_id, :number, required: false, desc: 'Used for settings so we don\'t show the current setting as a possible parent setting of itself'
+  param :setting_id, :number, required: false, desc: 'Used so we don\'t show the current setting as a possible parent setting of itself'
   error 422, "Invalid parameters provided"
   def index
     type = find_type
     queryset = type.where("name LIKE ?", params[:q].to_s + '%').ordered_by_name
 
-    queryset = queryset.where.not(id: params[:tag_id]) if params[:tag_id].present?
+    queryset = queryset.where.not(id: params[:setting_id]) if params[:setting_id].present?
 
     tags = paginate queryset, per_page: 25
     render json: {results: tags}
   end
 
-  api :GET, '/tags/:id', 'Load a single tag as a JSON resource'
-  param :id, :number, required: true, desc: 'Tag ID'
+  api :GET, '/tags/:id', 'Load a single setting as a JSON resource'
+  param :id, :number, required: true, desc: 'Setting ID'
   error 404, "Tag not found"
   def show
     render json: @setting.as_json(user_id: params[:user_id])
@@ -31,7 +31,7 @@ class Api::V1::TagsController < Api::ApiController
 
   def find_tag
     unless (@setting = Setting.find_by(id: params[:id]))
-      error = {message: 'Tag could not be found'}
+      error = {message: 'Setting could not be found'}
       render json: {errors: [error]}, status: :not_found and return
     end
   end
