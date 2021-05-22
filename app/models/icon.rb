5ef04727c8329765684dc2eedb654181429c4304
teceler <19716939+teceler@users.noreply.github.com>
2021-05-21 23:44:35 -0500

    Handle the fact that after_*_commit commands are aliases and rails handles this Weirdly

--- app/models/icon.rb
+++ app/models/icon.rb
@@ -21,8 +21,9 @@ class Icon < ApplicationRecord
 
   before_validation :use_icon_host
   before_save :use_https
-  after_update_commit :delete_from_s3, :update_flat_posts
-  after_destroy_commit :clear_icon_ids, :delete_from_s3
+  after_commit :delete_from_s3, on: [:update, :destroy]
+  after_update_commit :update_flat_posts
+  after_destroy_commit :clear_icon_ids
 
   scope :ordered, -> { order(Arel.sql('lower(keyword) asc'), created_at: :asc, id: :asc) }
 
