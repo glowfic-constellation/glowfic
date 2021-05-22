f5a08afc6b71b6888a6d0962f52f8c381780b8e0
teceler <19716939+teceler@users.noreply.github.com>
2021-05-22 16:37:55 -0500

    Don't use regex syntax for strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -138,7 +138,7 @@ module ApplicationHelper
   # https://apidock.com/rails/v4.2.7/ActionView/Helpers/TextHelper/split_paragraphs
   def split_paragraphs_largebreak(text)
     return [] if text.blank?
-    text.to_str.gsub(/\r\n?/, "\n").split(/\n\n/).map! do |t|
+    text.to_str.gsub(/\r\n?/, "\n").split("\n\n").map! do |t|
       t.gsub!(/(^\n|[^\n]\n)(?=[^\n])/, '\1<br />') || t
     end
   end
