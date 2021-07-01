f4c551013ce4186d93c72b5b8c8fa1c0efda9e43
teceler <19716939+teceler@users.noreply.github.com>
2021-07-01 12:26:56 -0500

    Use const hash for notification message options

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,14 +1,12 @@
 module NotificationHelper
+  NOTIFICATION_MESSAGES = {
+    'import_success'       => 'Post import succeeded',
+    'import_fail'          => 'Post import failed',
+    'new_favorite_post'    => 'An author you favorited has written a new post',
+    'joined_favorite_post' => 'An author you favorited has joined a post',
+  }
+
   def subject_for_type(notification_type)
-    case notification_type
-    when 'import_success'
-      'Post import succeeded'
-    when 'import_fail'
-      'Post import failed'
-    when 'new_favorite_post'
-      'An author you favorited has written a new post'
-    when 'joined_favorite_post'
-      'An author you favorited has joined a post'
-    end
+    NOTIFICATION_MESSAGES[notification_type]
   end
 end
