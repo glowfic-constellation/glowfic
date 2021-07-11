cedf9b37cda7c8472fcacc77467d51de87cbe4c1
teceler <19716939+teceler@users.noreply.github.com>
2021-07-11 13:07:43 -0500

    Test Concealable visible_to?

--- spec/models/index_spec.rb
+++ spec/models/index_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe Index do
+  let(:index) { create(:index, authors_locked: true) }
+  let(:user) { create(:user) }
+
   describe "destroy behavior" do
-    let!(:index) { create(:index) }
     let!(:section) { create(:index_section, index: index) }
     let!(:post) { create(:post) }
     let!(:sectionpost) { create(:index_post, index_section: section, index: index, post: post) }
@@ -44,9 +46,6 @@ RSpec.describe Index do
   end
 
   describe "#editable_by?" do
-    let(:index) { create(:index, authors_locked: true) }
-    let(:user) { create(:user) }
-
     it "requires a user" do
       expect(index.editable_by?(nil)).to eq(false)
     end
@@ -66,6 +65,32 @@ RSpec.describe Index do
 
     it "returns false for others" do
       expect(index.editable_by?(user)).to eq(false)
+    end
+  end
+
+  describe "#visible_to?" do
+    it "returns true if index is public" do
+      expect(index.visible_to?(nil)).to eq(true)
+    end
+
+    it "requires a user" do
+      index.update!(privacy: :registered)
+      expect(index.visible_to?(nil)).to eq(false)
+    end
+
+    it "returns true if index is registered" do
+      index.update!(privacy: :registered)
+      expect(index.visible_to?(user)).to eq(true)
+    end
+
+    it "returns true for admins" do
+      index.update!(privacy: :private)
+      expect(index.visible_to?(create(:admin_user))).to eq(true)
+    end
+
+    it "returns true for creator" do
+      index.update!(privacy: :private)
+      expect(index.visible_to?(index.user)).to eq(true)
     end
   end
 end
