b41169ef1db3a30d9eca20818674fa42326cdd55
teceler <19716939+teceler@users.noreply.github.com>
2021-07-14 11:32:32 -0500

    Pass params

--- app/services/reply/searcher.rb
+++ app/services/reply/searcher.rb
@@ -11,10 +11,10 @@ class Reply::Searcher < Generic::Searcher
     @search_results = @search_results.where(user_id: params[:author_id]) if params[:author_id].present?
     @search_results = @search_results.where(character_id: params[:character_id]) if params[:character_id].present?
     @search_results = @search_results.where(icon_id: params[:icon_id]) if params[:icon_id].present?
-    search_content if params[:subj_content].present?
-    sort
-    filter_posts
-    filter_templates
+    search_content(params[:subj_content]) if params[:subj_content].present?
+    sort(params[:sort], params[:subj_content])
+    filter_posts(params[:board_id])
+    filter_templates(params[:template_id], params[:author_id])
 
     @search_results = @search_results
       .select('replies.*, characters.name, characters.screenname, users.username, users.deleted as user_deleted')
@@ -34,9 +34,9 @@ class Reply::Searcher < Generic::Searcher
 
   private
 
-  def search_content
-    @search_results = @search_results.search(params[:subj_content]).with_pg_search_highlight
-    exact_phrases = params[:subj_content].scan(/"([^"]*)"/)
+  def search_content(content)
+    @search_results = @search_results.search(content).with_pg_search_highlight
+    exact_phrases = content.scan(/"([^"]*)"/)
     if exact_phrases.present?
       exact_phrases.each do |phrase|
         phrase = phrase.first.strip
@@ -46,35 +46,35 @@ class Reply::Searcher < Generic::Searcher
     end
   end
 
-  def sort
-    append_rank = params[:subj_content].present? ? ', rank DESC' : ''
-    if params[:sort] == 'created_new'
+  def sort(sort, content)
+    append_rank = content.present? ? ', rank DESC' : ''
+    if sort == 'created_new'
       @search_results = @search_results.except(:order).order('replies.created_at DESC' + append_rank)
-    elsif params[:sort] == 'created_old'
+    elsif sort == 'created_old'
       @search_results = @search_results.except(:order).order('replies.created_at ASC' + append_rank)
-    elsif params[:subj_content].blank?
+    elsif content.blank?
       @search_results = @search_results.order('replies.created_at DESC')
     end
   end
 
-  def filter_posts
+  def filter_posts(board_id)
     if @post
       @search_results = @search_results.where(post_id: @post.id)
-    elsif params[:board_id].present?
-      post_ids = Post.where(board_id: params[:board_id]).pluck(:id)
+    elsif board_id.present?
+      post_ids = Post.where(board_id: board_id).pluck(:id)
       @search_results = @search_results.where(post_id: post_ids)
     end
   end
 
-  def filter_templates
-    if params[:template_id].present?
-      @templates = Template.where(id: params[:template_id])
+  def filter_templates(template_id, author_id)
+    if template_id.present?
+      @templates = Template.where(id: template_id)
       if @templates.first.present?
         character_ids = Character.where(template_id: @templates.first.id).pluck(:id)
         @search_results = @search_results.where(character_id: character_ids)
       end
-    elsif params[:author_id].present?
-      @templates = @templates.where(user_id: params[:author_id])
+    elsif author_id.present?
+      @templates = @templates.where(user_id: author_id)
     end
   end
 end
