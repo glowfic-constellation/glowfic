b74aca436450559ec9adef6c3dfebb4a211276a9
teceler <19716939+teceler@users.noreply.github.com>
2021-07-13 22:28:54 -0500

    Split GalleriesController#icon code into add_new_icons and add_existing_icons

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -107,61 +107,9 @@ class GalleriesController < UploadingController
 
   def icon
     if params[:image_ids].present?
-      unless @gallery # gallery required for adding icons from other galleries
-        flash[:error] = "Gallery could not be found."
-        redirect_to user_galleries_path(current_user) and return
-      end
-
-      icon_ids = params[:image_ids].split(',').map(&:to_i).reject(&:zero?)
-      icon_ids -= @gallery.icons.pluck(:id)
-      icons = Icon.where(id: icon_ids)
-      icons.each do |icon|
-        next unless icon.user_id == current_user.id
-        @gallery.icons << icon
-      end
-      flash[:success] = "Icons added to gallery."
-      redirect_to @gallery and return
-    end
-
-    icons = (params[:icons] || []).reject { |icon| icon.values.all?(&:blank?) }
-    if icons.empty?
-      flash.now[:error] = "You have to enter something."
-      render :add and return
-    end
-
-    failed = false
-    @icons = icons
-    icons = []
-    @icons.each_with_index do |icon, index|
-      icon = Icon.new(icon_params(icon.except('filename', 'file')))
-      icon.user = current_user
-      unless icon.valid?
-        @icons[index]['url'] = @icons[index]['s3_key'] = '' if icon.errors.messages[:url]&.include?('is invalid')
-        flash.now[:error] ||= {}
-        flash.now[:error][:array] ||= []
-        flash.now[:error][:array] += icon.errors.full_messages.map { |m| "Icon #{index + 1}: #{m.downcase}" }
-        failed = true and next
-      end
-      icons << icon
-    end
-
-    if failed
-      flash.now[:error][:message] = "Your icons could not be saved."
-      render :add and return
-    elsif icons.empty?
-      @icons = []
-      flash.now[:error] = "Your icons could not be saved."
-      render :add
-    elsif icons.all?(&:save)
-      flash[:success] = "Icons saved."
-      if @gallery
-        icons.each { |icon| @gallery.icons << icon }
-        redirect_to @gallery and return
-      end
-      redirect_to user_gallery_path(id: 0, user_id: current_user.id)
+      add_existing_icons
     else
-      flash.now[:error] = "Your icons could not be saved."
-      render :add
+      add_new_icons
     end
   end
 
@@ -213,6 +161,66 @@ class GalleriesController < UploadingController
       @user = current_user
     end
     true
+  end
+
+  def add_existing_icons
+    unless @gallery # gallery required for adding icons from other galleries
+      flash[:error] = "Gallery could not be found."
+      redirect_to user_galleries_path(current_user) and return
+    end
+
+    icon_ids = params[:image_ids].split(',').map(&:to_i).reject(&:zero?)
+    icon_ids -= @gallery.icons.pluck(:id)
+    icons = Icon.where(id: icon_ids)
+    icons.each do |icon|
+      next unless icon.user_id == current_user.id
+      @gallery.icons << icon
+    end
+    flash[:success] = "Icons added to gallery."
+    redirect_to @gallery and return
+  end
+
+  def add_new_icons
+    icons = (params[:icons] || []).reject { |icon| icon.values.all?(&:blank?) }
+    if icons.empty?
+      flash.now[:error] = "You have to enter something."
+      render :add and return
+    end
+
+    failed = false
+    @icons = icons
+    icons = []
+    @icons.each_with_index do |icon, index|
+      icon = Icon.new(icon_params(icon.except('filename', 'file')))
+      icon.user = current_user
+      unless icon.valid?
+        @icons[index]['url'] = @icons[index]['s3_key'] = '' if icon.errors.messages[:url]&.include?('is invalid')
+        flash.now[:error] ||= {}
+        flash.now[:error][:array] ||= []
+        flash.now[:error][:array] += icon.errors.full_messages.map { |m| "Icon #{index + 1}: #{m.downcase}" }
+        failed = true and next
+      end
+      icons << icon
+    end
+
+    if failed
+      flash.now[:error][:message] = "Your icons could not be saved."
+      render :add and return
+    elsif icons.empty?
+      @icons = []
+      flash.now[:error] = "Your icons could not be saved."
+      render :add
+    elsif icons.all?(&:save)
+      flash[:success] = "Icons saved."
+      if @gallery
+        icons.each { |icon| @gallery.icons << icon }
+        redirect_to @gallery and return
+      end
+      redirect_to user_gallery_path(id: 0, user_id: current_user.id)
+    else
+      flash.now[:error] = "Your icons could not be saved."
+      render :add
+    end
   end
 
   def setup_new_icons
