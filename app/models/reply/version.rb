0672e384eec3b35ba405df602895edb4e57c3df1
teceler <19716939+teceler@users.noreply.github.com>
2021-07-15 17:58:43 -0500

    Add post association to Reply::Version

--- app/models/reply/version.rb
+++ app/models/reply/version.rb
@@ -1,3 +1,6 @@
 class Reply::Version < ::Version
   self.table_name = :reply_versions
+
+  belongs_to :reply, foreign_key: :item_id, inverse_of: :versions
+  belongs_to :post, inverse_of: false
 end
