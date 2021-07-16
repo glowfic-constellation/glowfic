53ccf5e096d5ffee19494baeba92ac52402a0c47
teceler <19716939+teceler@users.noreply.github.com>
2021-07-16 14:26:26 -0500

    Test written/_history

--- spec/controllers/posts/history_spec.rb
+++ spec/controllers/posts/history_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe PostsController, 'GET history' do
-  let(:post) { create(:post) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user: user) }
 
   it "requires post" do
     login
@@ -28,16 +29,53 @@ RSpec.describe PostsController, 'GET history' do
   context "with render_view" do
     render_views
 
-    it "works", versioning: true do
-      post.update!(privacy: :access_list)
-      post.update!(board: create(:board))
-      post.update!(content: 'new content')
+    before(:each) { login_as(user) }
 
-      login_as(post.user)
+    after(:each) { Audited.auditing_enabled = false }
+
+    it "works", versioning: true do
+      Version.as_user(user) do
+        post.update!(privacy: :access_list)
+        post.update!(board: create(:board))
+        post.update!(content: 'new content')
+      end
 
       get :history, params: { id: post.id }
 
       expect(response.status).to eq(200)
+    end
+
+    it "works with audits" do
+      Audited.auditing_enabled = true
+      Audited.audit_class.as_user(user) do
+        post.update!(privacy: :access_list)
+        post.update!(board: create(:board))
+        post.update!(content: 'new content')
+      end
+
+      get :history, params: { id: post.id }
+      expect(response.status).to eq(200)
+      expect(assigns(:versions)).to be_kind_of(ActiveRecord::Relation)
+      expect(assigns(:versions).first).to be_kind_of(Audited::Audit)
+    end
+
+    it "works with mixed audits and versions" do
+      Audited.auditing_enabled = true
+      Audited.audit_class.as_user(user) do
+        post.update!(privacy: :access_list)
+        post.update!(board: create(:board))
+      end
+      Audited.auditing_enabled = false
+
+      with_versioning do
+        Version.as_user(user) { post.update!(content: 'new content') }
+      end
+
+      get :history, params: { id: post.id }
+      expect(response.status).to eq(200)
+      expect(assigns(:versions)).to be_kind_of(Array)
+      expect(assigns(:versions).first).to be_kind_of(Audited::Audit)
+      expect(assigns(:versions).last).to be_kind_of(Version)
     end
   end
 end
--- spec/controllers/replies/history_spec.rb
+++ spec/controllers/replies/history_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe RepliesController, 'GET history' do
-  let(:reply) { create(:reply) }
+  let(:user) { create(:user) }
+  let(:reply) { create(:reply, user: user) }
 
   it "requires valid reply" do
     get :history, params: { id: -1 }
@@ -8,14 +9,11 @@ RSpec.describe RepliesController, 'GET history' do
   end
 
   it "requires post access" do
-    expect(reply.user_id).not_to eq(reply.post.user_id)
-    expect(reply.post.visible_to?(reply.user)).to eq(true)
-
     reply.post.update!(privacy: :private)
     reply.reload
-    expect(reply.post.visible_to?(reply.user)).to eq(false)
+    expect(reply.post.visible_to?(user)).to eq(false)
 
-    login_as(reply.user)
+    login_as(user)
     get :history, params: { id: reply.id }
     expect(response).to redirect_to(continuities_url)
     expect(flash[:error]).to eq("You do not have permission to view this post.")
@@ -36,5 +34,55 @@ RSpec.describe RepliesController, 'GET history' do
     login
     get :history, params: { id: reply.id }
     expect(response.status).to eq(200)
+  end
+
+  context "with views" do
+    render_views
+
+    after(:each) { Audited.auditing_enabled = false }
+
+    it "works", versioning: true do
+      Version.as_user(user) do
+        reply
+        reply.update!(content: 'new content', paper_trail_event: 'update')
+        reply.update!(character: create(:character, user: user), paper_trail_event: 'update')
+      end
+      expect(reply.versions.last.event).to eq('update')
+
+      get :history, params: { id: reply.id }
+      expect(response.status).to eq(200)
+      expect(assigns(:versions)).to be_kind_of(ActiveRecord::Relation)
+      expect(assigns(:versions).first).to be_kind_of(Version)
+    end
+
+    it "works with audits" do
+      Audited.auditing_enabled = true
+      Audited.audit_class.as_user(user) do
+        reply.update!(content: 'new content')
+        reply.update!(character: create(:character, user: user))
+      end
+
+      get :history, params: { id: reply.id }
+      expect(response.status).to eq(200)
+      expect(assigns(:versions)).to be_kind_of(ActiveRecord::Relation)
+      expect(assigns(:versions).first).to be_kind_of(Audited::Audit)
+      Audited.auditing_enabled = false
+    end
+
+    it "works with mixed audits and versions" do
+      Audited.auditing_enabled = true
+      Audited.audit_class.as_user(user) { reply.update!(content: 'new content') }
+      Audited.auditing_enabled = false
+      with_versioning do
+        Version.as_user(user) { reply.update!(character: create(:character, user: user), paper_trail_event: 'update') }
+      end
+      expect(reply.versions.last.event).to eq('update')
+
+      get :history, params: { id: reply.id }
+      expect(response.status).to eq(200)
+      expect(assigns(:versions)).to be_kind_of(Array)
+      expect(assigns(:versions).first).to be_kind_of(Audited::Audit)
+      expect(assigns(:versions).last).to be_kind_of(Version)
+    end
   end
 end
