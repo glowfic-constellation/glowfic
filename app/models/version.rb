545386f73a3b3675f3ca3a2039204424c0494fe7
teceler <19716939+teceler@users.noreply.github.com>
2021-07-15 20:50:17 -0500

    Add to version meta-class

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,12 @@
 class Version < PaperTrail::Version
   self.abstract_class = true
 
-  belongs_to :user, optional: true
+  belongs_to :user, foreign_key: :whodunnit, optional: true, inverse_of: false
 
   alias_attribute :user_id, :whodunnit
+
+  def self.as_user(user)
+    user_id = user.is_a?(User) ? user.id : user
+    ::PaperTrail.request(whodunnit: user_id) { yield }
+  end
 end
