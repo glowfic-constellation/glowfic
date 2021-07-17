927730de4e5e0680cabd71ffc04bd8c6a2e920df
teceler <19716939+teceler@users.noreply.github.com>
2021-07-16 22:34:18 -0500

    Set up caching and invalidation

--- app/models/tag/user_tag.rb
+++ app/models/tag/user_tag.rb
@@ -6,4 +6,12 @@ class Tag::UserTag < ApplicationRecord
   belongs_to :access_circle, foreign_key: :tag_id, inverse_of: :user_tags, optional: true
 
   validates :user, uniqueness: { scope: :tag }
+
+  after_commit :invalidate_cache, on: [:create, :destroy]
+
+  private
+
+  def invalidate_cache
+    Rails.cache.delete(PostViewer.cache_string_for(user_id))
+  end
 end
