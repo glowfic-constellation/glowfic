7ff1e0210f1358b6057c64616b1b4d7caa36679a
teceler <19716939+teceler@users.noreply.github.com>
2021-09-19 20:52:10 -0500

    Make migration reversible

--- db/migrate/20210715154620_alter_posts_authors_locked_default.rb
+++ db/migrate/20210715154620_alter_posts_authors_locked_default.rb
@@ -1,5 +1,5 @@
 class AlterPostsAuthorsLockedDefault < ActiveRecord::Migration[5.2]
   def change
-    change_column_default :posts, :authors_locked, true
+    change_column_default :posts, :authors_locked, from: false, to: true
   end
 end
