b8bd22991e405402aaae64e55109de3176658f12
teceler <19716939+teceler@users.noreply.github.com>
2021-12-19 22:02:40 -0600

    Use Rails.env.test? rather than Rails.env == 'test'

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,7 +4,7 @@ class ApplicationRecord < ActiveRecord::Base
   def audited_user_id
     user = ::Audited.store[:audited_user] # from as_user
     user ||= ::Audited.store[:current_user].call if ::Audited.store[:current_user].respond_to?(:call) # from controller method
-    return nil if user.nil? && Rails.env != 'test' # for debugging purposes this should fail loudly in tests
+    return nil if user.nil? && !Rails.env.test? # for debugging purposes this should fail loudly in tests
     user.id
   end
 end
