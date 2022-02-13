26f45abc4a7b978cea243fe626c448c5aeab803c
teceler <19716939+teceler@users.noreply.github.com>
2022-02-13 17:24:52 -0600

    Switch to native rails redis support rather than deprecated parts of redis-rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Show full error reports and use fakeredis for caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :redis_store
+  config.cache_store = :redis_cache_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
