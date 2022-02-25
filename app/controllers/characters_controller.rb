081150ca9c5e3ae3eb58385e749fc462e9cc46f7
Edward Jones <throne3d@gmail.com>
2022-02-25 16:53:34 -0300

    Address Bullet complaints

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -65,7 +65,6 @@ class CharactersController < ApplicationController
 
   def show
     @page_title = @character.name
-    @posts = posts_from_relation(@character.recent_posts)
     @meta_og = og_data
     use_javascript('characters/show') if @character.user_id == current_user.try(:id)
   end
