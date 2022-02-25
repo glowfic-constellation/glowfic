25cabca6816a17264a02cf4fe3f381e30b62206e
Nova <10sdassarma@gmail.com>
2022-02-24 18:39:42 -0800

    Add og:meta tags to stats page (#1467)
    * Add og:meta tags to stats page
    
    * Tests and URL update
    
    * Update spec/controllers/posts_controller_spec.rb
    
    Co-authored-by: Edward Jones <throne3d@gmail.com>
    
    * Update app/controllers/posts_controller.rb
    
    Co-authored-by: Edward Jones <throne3d@gmail.com>
    
    * Satisfy rubocop
    
    Co-authored-by: Nova Devereaux <nova@noblejury.com>
    Co-authored-by: Edward Jones <throne3d@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -176,6 +176,22 @@ class PostsController < WritableController
   end
 
   def stats
+    post_location = @post.board.name
+    post_location += ' » ' + @post.section.name if @post.section.present?
+    post_location += ' » Stats'
+
+    post_description = generate_short(@post.description)
+    post_description += ' ('
+    post_description += helpers.author_links(@post, linked: false)
+    post_description += ')'
+    post_description.strip!
+
+    @meta_og = {
+      title: @post.subject + ' · ' + post_location,
+      description: post_description,
+      url: stats_post_url(@post),
+    }
+
     fresh_when(etag: @post, last_modified: @post.updated_at, public: false)
   end
 
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1442,6 +1442,18 @@ RSpec.describe PostsController do
       expect(flash[:error]).to eq("Post could not be found.")
     end
 
+    it "calculates OpenGraph meta" do
+      user = create(:user, username: 'example user')
+      board = create(:board, name: 'board')
+      post = create(:post, subject: 'title', user: user, board: board)
+      get :stats, params: { id: post.id }
+
+      meta_og = assigns(:meta_og)
+      expect(meta_og[:url]).to eq(stats_post_url(post))
+      expect(meta_og[:title]).to eq('title · board » Stats')
+      expect(meta_og[:description]).to eq('(example user)')
+    end
+
     it "works logged out" do
       get :stats, params: { id: create(:post).id }
       expect(response.status).to eq(200)
