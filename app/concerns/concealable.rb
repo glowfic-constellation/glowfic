5cdc4c76badf494890ef2d00334038ce173359ad
Marri <xxstealthestarsxx@gmail.com>
2022-06-22 18:08:32 -0700

    Add comments with context about new privacy enums

--- app/concerns/concealable.rb
+++ app/concerns/concealable.rb
@@ -2,12 +2,14 @@ module Concealable
   extend ActiveSupport::Concern
 
   included do
+    # 3 used to be used as registered but was updated to full_accounts
+    # with the release of read-only accounts to avoid a db migration.
     enum privacy: {
       public: 0,
       private: 1,
       access_list: 2,
-      full_accounts: 3,
-      registered: 4,
+      full_accounts: 3, # registered accounts that are not read-only
+      registered: 4, # all registered accounts, both full and read-only
     }, _prefix: true
 
     def visible_to?(user)
