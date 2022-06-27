b11ffe4d13001fee212ab69aa8e867614c729ea4
Marri <xxstealthestarsxx@gmail.com>
2022-06-26 21:13:00 -0700

    Fix crash in post#delete_history by defaulting count to 0 harder in has_edit_audits?

--- app/helpers/writable_helper.rb
+++ app/helpers/writable_helper.rb
@@ -24,7 +24,7 @@ module WritableHelper
   def has_edit_audits?(audits, written)
     return false unless written.id.present?
     if written.is_a?(Post)
-      count = audits[:post]
+      count = audits.fetch(:post, 0)
     else
       count = audits.fetch(written.id, 0)
     end
