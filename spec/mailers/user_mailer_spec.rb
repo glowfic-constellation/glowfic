ae660b477d6507fdb1920152e98f384c80ddd2a7
Marri <xxstealthestarsxx@gmail.com>
2022-07-07 18:55:57 -0700

    Include icon helper methods in UserMailer (#1666)
    * Include icon helper methods in UserMailer
    
    * Test rspec for failures
    
    * Use icon since that's what fails
    
    * Consolidate tests
    
    * Test confirmed, add change back

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UserMailer, type: :mailer do
   describe "#post_has_new_reply" do
     it "sends email" do
       ActionMailer::Base.deliveries.clear
-      UserMailer.post_has_new_reply(create(:user).id, create(:reply).id).deliver!
+      UserMailer.post_has_new_reply(create(:user).id, create(:reply, with_icon: true).id).deliver!
       expect(ActionMailer::Base.deliveries.count).to eq(1)
     end
 
