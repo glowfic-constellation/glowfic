97a290f9304c374ca541a892153b83df62e5f50f
teceler <19716939+teceler@users.noreply.github.com>
2022-08-26 17:31:15 -0500

    Consolidate posts#owed

--- spec/controllers/posts/owed_spec.rb
+++ spec/controllers/posts/owed_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe PostsController, 'GET owed' do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:post) { create(:post, user: user) }
+
   it "requires login" do
     get :owed
     expect(response).to redirect_to(root_url)
@@ -22,25 +26,20 @@ RSpec.describe PostsController, 'GET owed' do
   context "with views" do
     render_views
 
-    def create_owed(user)
-      post = create(:post, user: user)
+    before(:each) do
+      login_as(user)
       create(:reply, post: post)
       post.mark_read(user)
     end
 
     it "succeeds" do
-      user = create(:user)
-      login_as(user)
-      create_owed(user)
       get :owed
       expect(response.status).to eq(200)
       expect(response.body).to include('note_go_strong')
     end
 
     it "succeeds with dark" do
-      user = create(:user, layout: 'starrydark')
-      login_as(user)
-      create_owed(user)
+      user.update!(layout: 'starrydark')
       get :owed
       expect(response.status).to eq(200)
       expect(response.body).to include('bullet_go_strong')
@@ -48,21 +47,18 @@ RSpec.describe PostsController, 'GET owed' do
   end
 
   context "with hidden" do
-    let(:user) { create(:user) }
-    let(:unhidden_post) { create(:post, user: user) }
     let(:hidden_post) { create(:post, user: user) }
 
     before(:each) do
       login_as(user)
-      create(:reply, post: unhidden_post)
+      create(:reply, post: post)
       create(:reply, post: hidden_post)
-      author = hidden_post.post_authors.where(user_id: user.id).first
-      author.update!(can_owe: false)
+      hidden_post.opt_out_of_owed(user)
     end
 
     it "does not show hidden without arg" do
       get :owed
-      expect(assigns(:posts)).to eq([unhidden_post])
+      expect(assigns(:posts)).to eq([post])
     end
 
     it "shows only hidden with arg" do
@@ -72,16 +68,12 @@ RSpec.describe PostsController, 'GET owed' do
   end
 
   context "with hiatused" do
-    let(:user) { create(:user) }
-    let(:other_user) { create(:user) }
-
     before(:each) do
       login_as(user)
       create(:post)
     end
 
     it "shows hiatused posts" do
-      post = create(:post, user: user)
       create(:reply, post: post, user: other_user)
       post.update!(status: :hiatus)
 
@@ -91,10 +83,8 @@ RSpec.describe PostsController, 'GET owed' do
     end
 
     it "shows auto-hiatused posts" do
-      post = nil
       Timecop.freeze(1.month.ago) do
-        post = create(:post, user: user)
-        create(:reply, post: post, user: other_user)
+        create(:reply, post: post, user: other_user) # post is initialized in the timecop block as well
       end
       get :owed, params: { view: 'hiatused' }
       expect(response.status).to eq(200)
@@ -103,30 +93,22 @@ RSpec.describe PostsController, 'GET owed' do
   end
 
   context "with posts" do
-    let(:user) { create(:user) }
-    let(:other_user) { create(:user) }
-    let(:post) { create(:post, user: user) }
-
     before(:each) do
       other_user
       login_as(user)
     end
 
-    context "with coauthor reply" do
-      it "shows a post if replied to by someone else" do
-        create(:reply, post_id: post.id, user_id: other_user.id)
+    context "with coauther reply" do
+      before(:each) { create(:reply, post: post, user: other_user) }
 
+      it "shows a post if replied to by someone else" do
         get :owed
         expect(response.status).to eq(200)
         expect(assigns(:posts)).to match_array([post])
       end
 
       it "does not show posts from site_testing" do
-        site_test = create(:board, id: Board::ID_SITETESTING)
-
-        post.board = site_test
-        post.save!
-        create(:reply, post_id: post.id, user_id: other_user.id)
+        post.update!(board: create(:board, id: Board::ID_SITETESTING))
 
         get :owed
         expect(response.status).to eq(200)
@@ -134,7 +116,6 @@ RSpec.describe PostsController, 'GET owed' do
       end
 
       it "hides completed threads" do
-        create(:reply, post: post, user: other_user)
         post.update!(status: :complete)
         get :owed
         expect(response.status).to eq(200)
@@ -142,7 +123,6 @@ RSpec.describe PostsController, 'GET owed' do
       end
 
       it "hides abandoned threads" do
-        create(:reply, post: post, user: other_user)
         post.update!(status: :abandoned)
         get :owed
         expect(response.status).to eq(200)
@@ -150,7 +130,6 @@ RSpec.describe PostsController, 'GET owed' do
       end
 
       it "show hiatused threads by default" do
-        create(:reply, post_id: post.id, user_id: other_user.id)
         post.update!(status: :hiatus)
 
         get :owed
@@ -159,31 +138,22 @@ RSpec.describe PostsController, 'GET owed' do
       end
 
       it "optionally hides hiatused threads" do
-        create(:reply, post_id: post.id, user_id: other_user.id)
         post.update!(status: :hiatus)
+        user.update!(hide_hiatused_tags_owed: true)
 
-        user.hide_hiatused_tags_owed = true
-        user.save!
         get :owed
         expect(response.status).to eq(200)
         expect(assigns(:posts)).to be_empty
       end
 
       it "hides threads the user has manually removed themselves from" do
-        post = create(:post, user: other_user, tagging_authors: [other_user])
-        create(:reply, post: post, user: user)
-        create(:reply, post: post, user: other_user)
         post.opt_out_of_owed(user)
         get :owed
         expect(response.status).to eq(200)
         expect(assigns(:posts)).to be_empty
       end
 
-      it "lists number of posts in the title if present" do
-        user = create(:user)
-        login_as(user)
-        post = create(:post, user: user)
-        create(:reply, post: post)
+      it "lists number of posts in the title" do
         get :owed
         expect(response.status).to eq(200)
         expect(assigns(:page_title)).to eq('[1] Replies Owed')
@@ -191,18 +161,18 @@ RSpec.describe PostsController, 'GET owed' do
     end
 
     context "with own reply" do
-      it "hides a post if you reply to it" do
-        create(:reply, post_id: post.id, user_id: other_user.id)
-        create(:reply, post_id: post.id, user_id: user.id)
+      before(:each) do
+        create(:reply, post: post, user: other_user)
+        create(:reply, post: post, user: user)
+      end
 
+      it "hides a post if you reply to it" do
         get :owed
         expect(response.status).to eq(200)
-        expect(assigns(:posts)).to be_empty
+        expect(assigns(:posts).count).to eq(0)
       end
 
       it "shows threads with existing drafts" do
-        create(:reply, post: post, user: other_user)
-        create(:reply, post: post, user: user)
         create(:reply_draft, post: post, user: user)
         get :owed
         expect(response.status).to eq(200)
@@ -210,8 +180,6 @@ RSpec.describe PostsController, 'GET owed' do
       end
 
       it "does not show threads with drafts by coauthors" do
-        create(:reply, post: post, user: other_user)
-        create(:reply, post: post, user: user)
         create(:reply_draft, post: post, user: other_user)
         get :owed
         expect(response.status).to eq(200)
@@ -227,12 +195,12 @@ RSpec.describe PostsController, 'GET owed' do
     end
 
     it "orders posts by tagged_at" do
-      post2 = create(:post, user_id: user.id)
-      post3 = create(:post, user_id: user.id)
-      post1 = create(:post, user_id: user.id)
-      create(:reply, post_id: post3.id, user_id: other_user.id)
-      create(:reply, post_id: post2.id, user_id: other_user.id)
-      create(:reply, post_id: post1.id, user_id: other_user.id)
+      post2 = create(:post, user: user)
+      post3 = create(:post, user: user)
+      post1 = create(:post, user: user)
+      create(:reply, post: post3, user: other_user)
+      create(:reply, post: post2, user: other_user)
+      create(:reply, post: post1, user: other_user)
       get :owed
       expect(assigns(:posts)).to eq([post1, post2, post3])
     end
