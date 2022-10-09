bb2498b53cfa5158f6782575fc96adf1335df9e9
teceler <19716939+teceler@users.noreply.github.com>
2022-10-09 16:29:32 -0500

    Move old-style character groups to OldCharacterGroup model

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,7 @@ class Character < ApplicationRecord
   belongs_to :user, optional: false
   belongs_to :template, inverse_of: :characters, optional: true
   belongs_to :default_icon, class_name: 'Icon', inverse_of: false, optional: true
-  belongs_to :character_group, optional: true
+  belongs_to :old_character_group, optional: true
   has_many :replies, dependent: false
   has_many :posts, dependent: false # These are handled in callbacks
   has_many :aliases, class_name: 'CharacterAlias', inverse_of: :character, dependent: :destroy
