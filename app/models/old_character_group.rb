75b7ec3bf7469a5656145d4f8881ca3bf28c19ce
teceler <19716939+teceler@users.noreply.github.com>
2022-10-09 16:29:32 -0500

    Move old-style character groups to OldCharacterGroup model

--- app/models/old_character_group.rb
+++ app/models/old_character_group.rb
@@ -1,5 +1,6 @@
-class CharacterGroup < ApplicationRecord
+class OldCharacterGroup < ApplicationRecord
   has_many :characters, dependent: :nullify
   belongs_to :user, optional: false
   validates :name, presence: true
+  self.table_name = 'character_groups'
 end
