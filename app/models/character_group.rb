c5a7e85d355f9848fcb65e9e8988d04304258e30
teceler <19716939+teceler@users.noreply.github.com>
2022-10-10 10:54:09 -0500

    Scope character group name uniqueness to user

--- app/models/character_group.rb
+++ app/models/character_group.rb
@@ -1,8 +1,3 @@
 class CharacterGroup < Tag
-  scope :owned_by, ->(user) {
-    left_outer_joins(character_tags: :character)
-      .left_outer_joins(:template_tags)
-      .joins('LEFT OUTER JOIN characters ON characters.template_id = template_tags.template_id')
-      .where(characters: { user_id: user.id })
-  }
+  validates :name, uniqueness: { scope: [:type, :user] }
 end
