79fb92d018f62c911970e24d4e60ce25c49c54b3
teceler <19716939+teceler@users.noreply.github.com>
2022-10-11 18:31:30 -0500

    Add primary column

--- db/migrate/20221009191904_migrate_character_groups.rb
+++ db/migrate/20221009191904_migrate_character_groups.rb
@@ -14,11 +14,11 @@ class MigrateCharacterGroups < ActiveRecord::Migration[6.0]
       group_id = template.characters.where.not(character_group_id: nil),select(:character_group_id).distinct.pluck(:character_group_id)
       raise StandardError, 'Templates should only have one group' if group_id.length > 1
       group_id = group_id.first
-      TemplateTag.create!(template: template, tag_id: group_cross[group_id])
+      TemplateTag.create!(template: template, tag_id: group_cross[group_id], primary: true)
     end
 
     untemplated_characters.each do |character|
-      CharacterTag.create(character: character, tag_id: group_cross[character.character_group_id])
+      CharacterTag.create(character: character, tag_id: group_cross[character.character_group_id], primary: true)
     end
   end
 
