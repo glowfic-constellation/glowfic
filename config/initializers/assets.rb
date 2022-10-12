688c4f553c6aeda567c3aeeecac3436fc3b7b77e
Edward Jones <throne3d@gmail.com>
2022-10-12 18:39:21 -0300

    Upgrade to Rails 6.1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,27 +1,25 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.configure do
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
 
-  # Add additional assets to the asset load path.
-  # Rails.application.config.assets.paths << Emoji.images_path
-  # Add Yarn node_modules folder to the asset load path.
-  config.assets.paths << Rails.root.join('node_modules')
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in the app/assets
-  # folder are already added.
-  config.assets.precompile += [/\w+\/*.js/]
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+Rails.application.config.assets.precompile += [/\w+\/*.js/]
 
-  config.assets.precompile << "accept_tos.js"
-  config.assets.precompile << "blocks.js"
-  config.assets.precompile << "board_sections.js"
-  config.assets.precompile << "global.js"
-  config.assets.precompile << "icons.js"
-  config.assets.precompile << "messages.js"
-  config.assets.precompile << "paginator.js"
-  config.assets.precompile << "reorder.js"
+Rails.application.config.assets.precompile << "accept_tos.js"
+Rails.application.config.assets.precompile << "blocks.js"
+Rails.application.config.assets.precompile << "board_sections.js"
+Rails.application.config.assets.precompile << "global.js"
+Rails.application.config.assets.precompile << "icons.js"
+Rails.application.config.assets.precompile << "messages.js"
+Rails.application.config.assets.precompile << "paginator.js"
+Rails.application.config.assets.precompile << "reorder.js"
 
-  config.assets.precompile += %w[layouts/*.css tinymce.css]
-end
+Rails.application.config.assets.precompile += %w[layouts/*.css tinymce.css]
