cde0c14065137c94a31c7c2a8146f6463e21bfff
Marri <xxstealthestarsxx@gmail.com>
2023-01-28 22:14:43 -0800

    Enforce maximum length on icon.credit field (#1710)
    * Enforce maximum length on icon.credit field
    
    * build not icon.build
    
    * Test split up, just because I love Tec
    
    * Whoops you saw nothing, definitely did not a word

--- spec/models/icon_spec.rb
+++ spec/models/icon_spec.rb
@@ -26,6 +26,18 @@ RSpec.describe Icon do
       expect(icon).not_to be_valid
     end
 
+    it "requires short credit" do
+      long_credit = 'aa' * 255
+      icon = build(:icon, credit: long_credit)
+      expect(icon).not_to be_valid
+    end
+
+    it "works with short credit" do
+      short_credit = 'b' * 200
+      icon = build(:icon, credit: short_credit)
+      expect(icon).to be_valid
+    end
+
     describe "#uploaded_url_yours" do
       it "should set the url back to its previous url on create" do
         icon = create(:uploaded_icon)
