23f89b288156f7c88887c95cebae2cc2e972db85
Edward Jones <throne3d@gmail.com>
2023-09-22 23:58:21 -0300

    Upgrade Rails config defaults to 6.1

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Glowfic
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
