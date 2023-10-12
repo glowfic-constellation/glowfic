770af1c7a6b740c73df483e33b0e795ed855e21e
Edward Jones <throne3d@gmail.com>
2023-10-12 20:20:30 -0300

    Fix minio icon upload with Docker approach
    The minio:9000 guest isn't usually available from the Docker host under
    that name, but instead binds to localhost with the given port. This
    allows the webserver to connect to minio directly inside the Docker
    network, and also allows the host to connect to the minio instance from
    the UI.

--- app/controllers/uploading_controller.rb
+++ app/controllers/uploading_controller.rb
@@ -5,10 +5,24 @@ class UploadingController < ApplicationController
   def set_s3_url
     return if params[:type] == "existing"
 
-    if !Rails.env.production? && S3_BUCKET.nil?
-      logger.error "S3_BUCKET does not exist; icon upload will FAIL."
-      @s3_direct_post = Struct.new(:url, :fields).new('', nil)
-      return
+    presign_conf = {}
+    unless Rails.env.production?
+      if S3_BUCKET.nil?
+        logger.error "S3_BUCKET does not exist; icon upload will FAIL."
+        @s3_direct_post = Struct.new(:url, :fields).new('', nil)
+        return
+      end
+
+      # for minio and Docker compatibility, replace the guest-compatible "minio" host with the host-compatible "localhost" path
+      if ENV.key?('MINIO_ENDPOINT') && ENV.key?('MINIO_ENDPOINT_EXTERNAL')
+        standard_endpoint = ENV.fetch('MINIO_ENDPOINT', nil)
+        replacement_endpoint = ENV.fetch('MINIO_ENDPOINT_EXTERNAL', nil)
+        bucket_url = S3_BUCKET.url
+        unless bucket_url.include?(standard_endpoint)
+          raise RuntimeError.new("couldn't find minio endpoint in direct post URL: #{standard_endpoint} in #{bucket_url}")
+        end
+        presign_conf[:url] = bucket_url.sub(standard_endpoint, replacement_endpoint)
+      end
     end
 
     @s3_direct_post = S3_BUCKET.presigned_post(
@@ -17,6 +31,7 @@ class UploadingController < ApplicationController
       acl: 'public-read',
       content_type_starts_with: 'image/',
       cache_control: 'public, max-age=31536000',
+      **presign_conf,
     )
   end
 end
