d670388c27beeed9701447436fa0597f964b58d4
Edward Jones <throne3d@gmail.com>
2023-10-12 20:42:25 -0300

    Combine conditions

--- app/controllers/uploading_controller.rb
+++ app/controllers/uploading_controller.rb
@@ -6,17 +6,15 @@ class UploadingController < ApplicationController
     return if params[:type] == "existing"
 
     presign_conf = {}
-    unless Rails.env.production?
+    if !Rails.env.production? && ENV.key?('MINIO_ENDPOINT') && ENV.key?('MINIO_ENDPOINT_EXTERNAL')
       # for minio and Docker compatibility, replace the guest-compatible "minio" host with the host-compatible "localhost" path
-      if ENV.key?('MINIO_ENDPOINT') && ENV.key?('MINIO_ENDPOINT_EXTERNAL')
-        standard_endpoint = ENV.fetch('MINIO_ENDPOINT', nil)
-        replacement_endpoint = ENV.fetch('MINIO_ENDPOINT_EXTERNAL', nil)
-        bucket_url = S3_BUCKET.url
-        unless bucket_url.include?(standard_endpoint)
-          raise RuntimeError.new("couldn't find minio endpoint in direct post URL: #{standard_endpoint} in #{bucket_url}")
-        end
-        presign_conf[:url] = bucket_url.sub(standard_endpoint, replacement_endpoint)
+      standard_endpoint = ENV.fetch('MINIO_ENDPOINT', nil)
+      replacement_endpoint = ENV.fetch('MINIO_ENDPOINT_EXTERNAL', nil)
+      bucket_url = S3_BUCKET.url
+      unless bucket_url.include?(standard_endpoint)
+        raise RuntimeError.new("couldn't find minio endpoint in direct post URL: #{standard_endpoint} in #{bucket_url}")
       end
+      presign_conf[:url] = bucket_url.sub(standard_endpoint, replacement_endpoint)
     end
 
     @s3_direct_post = S3_BUCKET.presigned_post(
