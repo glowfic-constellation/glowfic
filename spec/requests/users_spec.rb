9958a35f77974a31e3c6155ca8a69e5d362d482f
Edward Jones <throne3d@gmail.com>
2023-10-31 00:54:39 -0300

    Add test for user#show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "Users" do
   describe "creation" do
-    it "creates a new reader-mode user and logs in" do
+    it "creates a new reader-mode user, logs in, and has profile page" do
       get "/users/new"
       aggregate_failures do
         expect(response).to have_http_status(200)
@@ -8,16 +8,18 @@ RSpec.describe "Users" do
         expect(response.body).to include("Sign Up")
       end
 
-      post "/users", params: {
-        user: {
-          username: "John Doe",
-          email: "john.doe@example.com",
-          password: "password",
-          password_confirmation: "password",
-        },
-        addition: 14,
-        tos: true,
-      }
+      expect {
+        post "/users", params: {
+          user: {
+            username: "John Doe",
+            email: "john.doe@example.com",
+            password: "password",
+            password_confirmation: "password",
+          },
+          addition: 14,
+          tos: true,
+        }
+      }.to change { User.count }.by(1)
 
       aggregate_failures do
         expect(response).to redirect_to(root_path)
@@ -31,6 +33,15 @@ RSpec.describe "Users" do
         expect(response.body).to include("John Doe")
         expect(response.body).to include("Log out")
         expect(response.body).to include("User created! You have been logged in.")
+      end
+
+      user = User.last
+      get "/users/#{user.id}"
+      aggregate_failures do
+        expect(response).to have_http_status(200)
+        expect(response).to render_template(:show)
+        expect(response.body).to include("John Doe's Recent Posts")
+        expect(response.body).not_to include("Send Message")
       end
     end
   end
