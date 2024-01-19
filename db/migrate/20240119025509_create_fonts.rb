f014ff867f2aaa8fe439896d6d3d7a7146e1ce5b
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-01-19 04:27:58 +0100

    Add fonts table and posts_fonts join table

---
+++ db/migrate/20240119025509_create_fonts.rb
@@ -0,0 +1,15 @@
+class CreateFonts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :fonts do |t|
+      t.string :name, :null => false
+      t.string :css
+      t.timestamps
+    end
+
+    create_table :posts_fonts do |t|
+      t.belongs_to :post
+      t.belongs_to :font
+      t.timestamps
+    end
+  end
+end
