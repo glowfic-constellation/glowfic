ff35dd5ffce34355e6989f9c53c453fdb598c0b6
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-01-19 05:22:37 +0100

    Create picker and inverse association

--- app/models/post_font.rb
+++ app/models/post_font.rb
@@ -1,6 +1,6 @@
 class PostFont < ApplicationRecord
-  belongs_to :post, inverse_of: :posts_fonts, optional: false
-  belongs_to :font, inverse_of: :posts_fonts, optional: false
+  belongs_to :post, inverse_of: :post_fonts, optional: false
+  belongs_to :font, inverse_of: :post_fonts, optional: false
 
   validates :post, uniqueness: { scope: :font }
 end
