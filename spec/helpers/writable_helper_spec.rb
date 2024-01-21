96b9ac1762adbe04d5e2fa8191d1b910a0e2b9c7
teceler <19716939+teceler@users.noreply.github.com>
2024-01-21 15:41:22 -0600

    Test post_or_reply_link and post_or_reply_mem_link

--- spec/helpers/writable_helper_spec.rb
+++ spec/helpers/writable_helper_spec.rb
@@ -212,4 +212,36 @@ RSpec.describe WritableHelper do
       expect(helper.privacy_icon(:private, false)).to eq(expected)
     end
   end
+
+  describe "#post_or_reply_link" do
+    it "requires an id" do
+      reply = build(:reply)
+      expect(helper.post_or_reply_link(reply)).to be_nil
+    end
+
+    it "delegates to post_or_reply_mem_link" do
+      reply = create(:reply)
+      allow(helper).to receive(:post_or_reply_mem_link).and_call_original
+      expect(helper).to receive(:post_or_reply_mem_link).with(id: reply.id, klass: Reply)
+      helper.post_or_reply_link(reply)
+    end
+  end
+
+  describe "#post_or_reply_mem_link" do
+    it "requires an id" do
+      expect(helper.post_or_reply_mem_link(id: nil, klass: '')).to be_nil
+    end
+
+    it "handles a reply" do
+      reply = create(:reply)
+      html = reply_path(reply.id, anchor: "reply-#{reply.id}")
+      expect(helper.post_or_reply_mem_link(id: reply.id, klass: Reply)).to eq(html)
+    end
+
+    it "handles a post" do
+      post = create(:post)
+      html = post_path(post.id)
+      expect(helper.post_or_reply_mem_link(id: post.id, klass: Post)).to eq(html)
+    end
+  end
 end
