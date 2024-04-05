39ef991b80d374c15269a6448b9df610d2a846ab
teceler <19716939+teceler@users.noreply.github.com>
2024-04-04 23:42:26 -0500

    i18n-ify blocks

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -5,12 +5,12 @@ class BlocksController < ApplicationController
   before_action :editor_setup, only: [:new, :edit]
 
   def index
-    @page_title = "Blocked Users"
+    @page_title = t('.title')
     @blocks = Block.where(blocking_user: current_user)
   end
 
   def new
-    @page_title = "Block User"
+    @page_title = t('.title')
     @block = Block.new
     @block.blocking_user = current_user
     @block.blocked_user_id = params.fetch(:block, {})[:blocked_user_id]
@@ -23,30 +23,30 @@ class BlocksController < ApplicationController
     @block.blocked_user_id = params.fetch(:block, {})[:blocked_user_id]
 
     unless @block.save
-      render_errors(@block, action: 'create', now: true, msg: 'User could not be blocked')
+      render_errors(@block, action: 'create', now: true, msg: t('.failure'))
       editor_setup
       @users = [@block.blocked_user].compact
-      @page_title = 'Block User'
+      @page_title = '.new.title'
       render :new and return
     end
 
-    flash[:success] = "User blocked."
+    flash[:success] = t('.success')
     redirect_to blocks_path
   end
 
   def edit
-    @page_title = 'Edit Block: ' + @block.blocked_user.username
+    @page_title = t('.title', name: @block.blocked_user.username)
   end
 
   def update
     unless @block.update(permitted_params)
       render_errors(@block, action: 'updated', now: true)
       editor_setup
-      @page_title = 'Edit Block: ' + @block.blocked_user.username
+      @page_title = t('.title', name: @block.blocked_user.username)
       render :edit and return
     end
 
-    flash[:success] = "Block updated."
+    flash[:success] = t('.success')
     redirect_to blocks_path
   end
 
@@ -54,9 +54,9 @@ class BlocksController < ApplicationController
     begin
       @block.destroy!
     rescue ActiveRecord::RecordNotDestroyed => e
-      render_errors(@block, action: 'delete', msg: 'User could not be unblocked', err: e)
+      render_errors(@block, action: 'delete', msg: t('.failure'), err: e)
     else
-      flash[:success] = "User unblocked."
+      flash[:success] = t('.success')
     end
     redirect_to blocks_path
   end
@@ -69,22 +69,22 @@ class BlocksController < ApplicationController
 
   def require_permission
     return if @block.editable_by?(current_user)
-    flash[:error] = "Block could not be found." # Return the same error message as if it didn't exist
+    flash[:error] = t('.errors.not_found') # Return the same error message as if it didn't exist
     redirect_to blocks_path
   end
 
   def find_model
     return if (@block = Block.find_by(id: params[:id]))
-    flash[:error] = "Block could not be found."
+    flash[:error] = t('.errors.not_found')
     redirect_to blocks_path
   end
 
   def editor_setup
     use_javascript('blocks')
     @options = {
-      "Nothing"    => :none,
-      "Just posts" => :posts,
-      "Everything" => :all,
+      t('.options.none')  => :none,
+      t('.options.posts') => :posts,
+      t('.options.all')   => :all,
     }
   end
 end
