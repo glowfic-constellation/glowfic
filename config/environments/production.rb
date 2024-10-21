d87c7c7529436e6f2e8cf596e669ada8fff53d19
Edward Jones <throne3d@gmail.com>
2024-10-21 16:11:38 -0300

    Fix cop offenses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_CACHE_URL'), expires_in: 30.days, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.cache_store = :redis_cache_store,
+                       { url: ENV.fetch('REDIS_CACHE_URL'), expires_in: 30.days, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :resque
