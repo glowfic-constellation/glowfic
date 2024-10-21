e64653303898513fd85d9a8e7782b7e9ee3f67f6
Edward Jones <throne3d@gmail.com>
2024-10-21 19:04:52 -0300

    Remove remaining deprecated options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -174,19 +174,6 @@ export default [
           markers: ["= require"],
         },
       ],
-
-      // deprecated options needing replacement (not style)
-      "no-return-await": 2,
-      "callback-return": 2,
-      "global-require": 2,
-      "handle-callback-err": 2,
-      "no-mixed-requires": 1,
-      "no-new-require": 1,
-      "no-path-concat": 2,
-      "no-process-env": 1,
-      "no-process-exit": 2,
-      "no-restricted-modules": 2,
-      "no-sync": 1,
     },
   },
 ];
