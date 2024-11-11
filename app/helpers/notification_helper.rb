ff05585122a6f64dfef2ff7f5b077cdc6d522403
teceler <19716939+teceler@users.noreply.github.com>
2024-11-11 08:50:37 -0600

    Fix case
    Co-authored-by: Edward Jones <throne3d@gmail.com>

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -7,11 +7,11 @@ module NotificationHelper
       'new_favorite_post'    => 'A once-favorited subject has a new post',
       'joined_favorite_post' => 'A once-favorited author has joined a post',
     },
-    'user'  => {
+    'User'  => {
       'new_favorite_post'    => 'An author you favorited has written a new post',
       'joined_favorite_post' => 'An author you favorited has joined a post',
     },
-    'board' => {
+    'Board' => {
       'new_favorite_post' => 'An continuity you favorited has a new post',
     },
   }
