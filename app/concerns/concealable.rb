635dd4c1487353f05701423bafc7d8a25112f635
Edward Jones <throne3d@gmail.com>
2024-11-24 12:10:48 -0300

    Fix enum syntax for concealable to be Rails 8 compatible
    Already linted in other contexts via Rails/EnumSyntax, but not caught in
    this concern.

--- app/concerns/concealable.rb
+++ app/concerns/concealable.rb
@@ -5,13 +5,13 @@ module Concealable
   included do
     # 3 used to be used as registered but was updated to full_accounts
     # with the release of read-only accounts to avoid a db migration.
-    enum privacy: {
+    enum :privacy, {
       public: 0,
       private: 1,
       access_list: 2,
       full_accounts: 3, # registered accounts that are not read-only
       registered: 4, # all registered accounts, both full and read-only
-    }, _prefix: true
+    }, prefix: true
 
     def visible_to?(user)
       # does not support access lists at this time
