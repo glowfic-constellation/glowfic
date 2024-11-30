f2dc5d38a7892e35fb7d744d8a877eb0b7c3e3b0
Red Magnos <redmagnos@gmail.com>
2024-11-30 19:22:32 -0300

    Fix pluck SQL

--- app/models/template.rb
+++ app/models/template.rb
@@ -9,8 +9,8 @@ class Template < ApplicationRecord
 
   scope :ordered, -> { order(name: :asc, created_at: :asc, id: :asc) }
 
-  CHAR_PLUCK = Arel.sql("id, concat_ws(' | ', name, nickname, screenname)")
-  NPC_PLUCK = Arel.sql("id, concat_ws(' | ', name, nickname)")
+  CHAR_PLUCK = Arel.sql("characters.id as id, concat_ws(' | ', characters.name, nickname, screenname)")
+  NPC_PLUCK = Arel.sql("characters.id as id, concat_ws(' | ', characters.name, nickname)")
 
   def plucked_characters
     characters.non_npcs.not_retired.pluck(CHAR_PLUCK)
