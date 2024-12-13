d349bca4e441c4fd95be34d912e801296ab0b9e4
Red Magnos <redmagnos@gmail.com>
2024-12-13 17:05:21 -0300

    Add handling for multiple checkbox groups

--- app/assets/javascripts/check_all.js
+++ app/assets/javascripts/check_all.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
-  // When the "Check All" checkbox is toggled
-  $('#check-all').on('change', function() {
-    $('.checkbox').prop('checked', this.checked);
-  });
+  $('.checkbox-set').each(function() {
+    // For multiple sets of checkboxes on the same page
+    const $set = $(this);
+    const $checkAll = $set.find('.check-all');
+    const $childCheckboxes = $set.find('.checkbox');
 
-  // When any child checkbox is toggled
-  $('.checkbox').on('change', function() {
-    if ($('.checkbox:checked').length !== $('.checkbox').length) {
-      $('#check-all').prop('checked', false);
-    } else {
-      $('#check-all').prop('checked', true);
-    }
+    // When the "Check All" checkbox is toggled
+    $checkAll.on('change', function() {
+      $childCheckboxes.prop('checked', this.checked);
+    });
+
+    // When any child checkbox is toggled
+    $childCheckboxes.on('change', function() {
+      const allChecked = $childCheckboxes.length === $childCheckboxes.filter(':checked').length;
+      $checkAll.prop('checked', allChecked);
+    });
   });
 });
