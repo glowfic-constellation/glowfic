9a81cdf878f9a434a8105a727da8afdf53cfc79f
Red Magnos <redmagnos@gmail.com>
2024-12-21 15:29:53 -0300

    Squish migrations

--- db/migrate/20241220001400_add_user_bookmarks.rb
+++ db/migrate/20241220001400_add_user_bookmarks.rb
@@ -1,6 +1,6 @@
 class AddUserBookmarks < ActiveRecord::Migration[7.2]
   def change
-    create_table :user_bookmarks do |t|
+    create_table :bookmarks do |t|
       t.integer :user_id, null: false
       t.integer :reply_id, null: false
       t.integer :post_id, null: false
@@ -8,11 +8,11 @@ class AddUserBookmarks < ActiveRecord::Migration[7.2]
       t.string :type, null: false, default: "reply_bookmark"
       t.timestamps null: true
     end
-    add_index :user_bookmarks, :user_id
-    add_index :user_bookmarks, :reply_id
-    add_index :user_bookmarks, :post_id
-    add_index :user_bookmarks, [:user_id, :reply_id, :type], unique: true
-    add_index :user_bookmarks, [:post_id, :user_id]
+    add_index :bookmarks, :user_id
+    add_index :bookmarks, :reply_id
+    add_index :bookmarks, :post_id
+    add_index :bookmarks, [:user_id, :reply_id, :type], unique: true
+    add_index :bookmarks, [:post_id, :user_id]
 
     add_column :users, :public_bookmarks, :boolean, default: false
   end
--- db/migrate/20241220001400_rename_user_bookmarks.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserBookmarks < ActiveRecord::Migration[7.2]
-  def change
-    rename_table :user_bookmarks, :bookmarks
-  end
-end
