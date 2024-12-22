61fdf97f9d054229f5c14680633dc16c421734dd
Edward Jones <throne3d@gmail.com>
2024-12-22 17:50:16 -0300

    fix: Enable frozen_string_literal via config.action_view
    Otherwise this is overwritten in Railties. Oops!

--- config/initializers/template_frozen_literal.rb
+++
@@ -1,3 +0,0 @@
-# reduce memory use of strings in ActionView Templates
-# https://github.com/rails/rails/blob/v7.2.2/actionview/lib/action_view/template.rb#L181
-ActionView::Template.frozen_string_literal = true
