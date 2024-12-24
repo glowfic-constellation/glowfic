5af5253bdba7221a06482771af4e051eb28f1cba
Edward Jones <throne3d@gmail.com>
2024-12-24 17:34:09 -0300

    Update db/schema.rb for Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_22_000400) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_22_000400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "audits", id: :serial, force: :cascade do |t|
     t.integer "auditable_id"
