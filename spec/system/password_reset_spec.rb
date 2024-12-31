43d2a64d66ffa98d6954de10ceecddd43cd6d84d
teceler <19716939+teceler@users.noreply.github.com>
2024-12-31 14:05:03 -0600

    Fix password reset test

--- spec/system/password_reset_spec.rb
+++ spec/system/password_reset_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe "Resetting password" do
     # request reset
     user = create(:user)
     visit new_password_reset_path
-    expect(page).to have_selector(".editor-title", exact_text: "Request Password Reset")
-    within(".form-table") do
+    expect(page).to have_selector('.editor-title', exact_text: "Request Password Reset")
+    within('.form-table') do
       fill_in "Username", with: user.username
       fill_in "Email address", with: user.email
       click_button "Reset Password"
@@ -14,7 +14,7 @@ RSpec.describe "Resetting password" do
     # complete reset request
     reset = PasswordReset.last
     visit password_reset_path(reset.auth_token)
-    expect(page).to have_selector("th.editor-title", exact_text: "Change Password")
+    expect(page).to have_selector('.editor-title', exact_text: "Change Password")
     fill_in "New Password", with: 'anewpass'
     fill_in "Confirm Password", with: 'anewpass'
     click_button "Save"
