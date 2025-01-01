c709d6aabdff64ffd735780f36a42dc31ce912bd
Red Magnos <redmagnos@gmail.com>
2025-01-01 16:06:25 -0300

    Remove user locking

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 require 'resque/server'
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users, skip: :unlock, controllers: {
     registrations: 'user/registrations',
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
