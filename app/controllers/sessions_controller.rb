de3cc2aa62e071d09d445f5e49ab63e06a161e0f
Red Magnos <redmagnos@gmail.com>
2025-01-01 16:27:03 -0300

    Restore accidentally removed function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,12 @@ class SessionsController < ApplicationController
     cookies.permanent[:accepted_tos] = cookie_hash(User::CURRENT_TOS_VERSION)
     redirect_to session[:previous_url] || root_url # allow_other_host: false
   end
+
+  private
+
+  def cookie_hash(value)
+    return { value: value, domain: 'glowfic-staging.herokuapp.com' } if request.host.include?('staging')
+    return { value: value, domain: '.glowfic.com', tld_length: 2 } if Rails.env.production?
+    { value: value }
+  end
 end
