df7b96f43d57dfa0340d53263176100408dffe8c
Edward Jones <throne3d@gmail.com>
2025-01-02 15:25:43 -0300

    Add other autogenerated Devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ require 'resque/server'
 
 Rails.application.routes.draw do
   devise_for :users, skip: :unlock, controllers: {
+    confirmations: 'user/confirmations',
+    # omniauth_callbacks: 'user/omniauth_callbacks',
+    passwords: 'user/passwords',
     registrations: 'user/registrations',
     sessions: 'user/sessions',
+    # unlocks: 'user/unlocks',
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   apipie
