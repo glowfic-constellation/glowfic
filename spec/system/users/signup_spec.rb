38792108135d05b6fb643e33a004967c47b88197
Red Magnos <redmagnos@gmail.com>
2025-01-03 15:42:39 -0300

    Add system spec for user signup without javascript

--- spec/system/users/signup_spec.rb
+++ spec/system/users/signup_spec.rb
@@ -166,4 +166,76 @@ RSpec.describe "Signing up" do
       expect(page).to have_link("Replies Owed")
     end
   end
+
+  scenario "performs validations without javascript" do
+    existing_user = create(:user)
+    new_user_name = "new_user_name"
+    new_user_password = "long password"
+    visit root_path
+    within("#header-buttons-links") { click_link "Sign up" }
+
+    # Fill in all required fields
+    within(".form-table") do
+      click_button "Sign Up"
+    end
+    expect(page).to have_selector('.flash.error', text: "You must accept the Terms and Conditions to use the Constellation.")
+    within(".form-table") do
+      check "Terms"
+      click_button "Sign Up"
+    end
+    expect(page).to have_selector('.flash.error', text: "Please check your math and try again.")
+    within(".form-table") do
+      fill_in "Captcha", with: "14"
+      click_button "Sign Up"
+    end
+    expect(page).to have_selector('.flash.error', text: "Username can't be blank")
+    expect(page).to have_selector('.flash.error', text: "Email can't be blank")
+    expect(page).to have_selector('.flash.error', text: "Password can't be blank")
+    within(".form-table") do
+      fill_in "Username", with: existing_user.username
+      fill_in "Email", with: existing_user.email
+      fill_in "Password", with: "short"
+      click_button "Sign Up"
+    end
+    expect(page).to have_selector('.flash.error', text: "Username has already been taken")
+    expect(page).to have_selector('.flash.error', text: "Email has already been taken")
+    expect(page).to have_selector('.flash.error', text: "Password confirmation doesn't match Password")
+    expect(page).to have_selector('.flash.error', text: "Password is too short (minimum is 6 characters)")
+    within(".form-table") do
+      fill_in "Username", with: new_user_name
+      fill_in "Email", with: "different" + existing_user.email
+      fill_in "Password", with: "short"
+      fill_in "Confirm", with: "short"
+      click_button "Sign Up"
+    end
+    expect(page).to have_selector('.flash.error', text: "Password is too short (minimum is 6 characters)")
+    within(".form-table") do
+      fill_in "Password", with: new_user_password
+      fill_in "Confirm", with: new_user_password
+      click_button "Sign Up"
+    end
+    expect(page).to have_selector('.flash.notice',
+      text: "User created. A confirmation link has been emailed to you; use this to activate your account.",)
+    expect(page).to have_current_path(root_path)
+
+    # Get confirmation email
+    expect(DeviseMailer).to have_queue_size_of(1)
+
+    # complete confirmation request
+    # NB. can't pull from the database as the token there is hashed - must pull the token direct from the mail
+    # https://github.com/heartcombo/devise/blob/v4.9.4/lib/devise/models/recoverable.rb#L134
+    token = ResqueSpec.queue_for(DeviseMailer).last[:args][1][1]
+    visit user_confirmation_path(confirmation_token: token)
+    expect(page).to have_current_path(new_user_session_path)
+    expect(page).to have_selector('.flash.notice', text: "Email address confirmed.")
+
+    within(".form-table") do
+      fill_in "Username", with: new_user_name
+      fill_in "Password", with: new_user_password
+      click_button "Sign In"
+    end
+    expect(page).to have_current_path(continuities_path)
+    expect(page).to have_selector('.flash.notice', text: "You are now logged in. Welcome back!")
+    within("#user-info") { expect(page).to have_text(new_user_name) }
+  end
 end
