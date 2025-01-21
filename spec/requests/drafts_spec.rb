589d5edfc053efd238716409bbf7424b976898d8
teceler <19716939+teceler@users.noreply.github.com>
2025-01-21 13:27:53 -0600

    Split out & fix draft testing

--- spec/requests/drafts_spec.rb
+++ spec/requests/drafts_spec.rb
@@ -1,59 +1,79 @@
 RSpec.describe 'Drafts' do
-  describe 'create'
+  let(:user) { create(:user) }
+  let(:rpost) { create(:post, user: user) }
 
-  describe "drafts" do
-    it "can be created and deleted" do
-      thread = create(:post)
+  before(:each) { login(user) }
 
-      login
-      get "/posts/#{thread.id}"
+  describe 'create' do
+    it 'works' do
+      get "/posts/#{rpost.id}"
       expect(response).to have_http_status(200)
 
-      # create draft
       expect {
-        post "/replies", params: { reply: { post_id: thread.id, content: "Test content" }, button_draft: 'Save Draft' }
+        post "/drafts", params: { reply: { post_id: rpost.id, content: "Test content" } }
       }.to change { ReplyDraft.count }.by(1).and not_change { Reply.count }
+
       aggregate_failures do
         expect(response).to have_http_status(302)
-        expect(response).to redirect_to(post_path(thread, page: 'unread', anchor: 'unread'))
+        expect(response).to redirect_to(post_path(rpost, page: 'unread', anchor: 'unread'))
         expect(flash[:error]).to be_nil
         expect(flash[:success]).to eq("Draft saved.")
       end
+
       follow_redirect!
+
       aggregate_failures do
         expect(response).to have_http_status(200)
         expect(response).to render_template(:show)
         expect(response.body).to include("Test content")
         expect(response.body).to include("Delete Draft")
       end
+    end
+  end
 
-      # delete draft
+  describe 'destroy' do
+    it 'requires a draft' do
+      get "/posts/#{rpost.id}"
+      expect(response).to have_http_status(200)
+      expect(response.body).not_to include("Delete Draft")
+
       expect {
-        post "/replies", params: { reply: { post_id: thread.id, content: "Test content" }, button_delete_draft: 'Delete Draft' }
-      }.to change { ReplyDraft.count }.by(-1).and not_change { Reply.count }
+        delete "/drafts/0", params: { reply: { post_id: rpost.id, content: "Test content" } }
+      }.to not_change { ReplyDraft.count }.and not_change { Reply.count }
+
       aggregate_failures do
         expect(response).to have_http_status(302)
-        expect(response).to redirect_to(post_path(thread, page: 'unread', anchor: 'unread'))
+        expect(response).to redirect_to(post_path(rpost, page: 'unread', anchor: 'unread'))
+        expect(flash[:error]).to eq({ message: "Draft could not be deleted", array: nil })
+        expect(flash[:success]).to be_nil
+      end
+    end
+
+    it 'works' do
+      draft = create(:reply_draft, user: user, post: rpost, content: 'Test content')
+
+      get "/posts/#{rpost.id}"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("Delete Draft")
+
+      expect {
+        delete "/drafts/#{draft.id}", params: { reply: { post_id: rpost.id, content: "Test content" } }
+      }.to change { ReplyDraft.count }.by(-1).and not_change { Reply.count }
+
+      aggregate_failures do
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(post_path(rpost, page: 'unread', anchor: 'unread'))
         expect(flash[:error]).to be_nil
         expect(flash[:success]).to eq("Draft deleted.")
       end
+
       follow_redirect!
+
       aggregate_failures do
         expect(response).to have_http_status(200)
         expect(response).to render_template(:show)
         expect(response.body).not_to include("Test content")
         expect(response.body).not_to include("Delete Draft")
-      end
-
-      # try forcing another delete
-      expect {
-        post "/replies", params: { reply: { post_id: thread.id, content: "Test content" }, button_delete_draft: 'Delete Draft' }
-      }.to not_change { ReplyDraft.count }.and not_change { Reply.count }
-      aggregate_failures do
-        expect(response).to have_http_status(302)
-        expect(response).to redirect_to(post_path(thread, page: 'unread', anchor: 'unread'))
-        expect(flash[:error]).to eq({ message: "Draft could not be deleted", array: nil })
-        expect(flash[:success]).to be_nil
       end
     end
   end
