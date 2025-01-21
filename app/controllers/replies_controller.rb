4bd4c218f47eb7b4fe1f1e4f30be0edf1cfc91f6
teceler <19716939+teceler@users.noreply.github.com>
2025-01-21 12:41:06 -0600

    Move make_draft to WritableController

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -408,28 +408,4 @@ class RepliesController < WritableController
       new_reply.update!(reply_params)
     end
   end
-
-  def make_draft(show_message=true)
-    if (draft = ReplyDraft.draft_for(params[:reply][:post_id], current_user.id))
-      draft.assign_attributes(permitted_params)
-    else
-      draft = ReplyDraft.new(permitted_params)
-      draft.user = current_user
-    end
-    process_npc(draft, permitted_character_params)
-    new_npc = !draft.character.nil? && !draft.character.persisted?
-
-    begin
-      draft.save!
-    rescue ActiveRecord::RecordInvalid => e
-      render_errors(draft, action: 'saved', class_name: 'Draft', err: e)
-    else
-      if show_message
-        msg = "Draft saved."
-        msg += " Your new NPC character has also been persisted!" if new_npc
-        flash[:success] = msg
-      end
-    end
-    draft
-  end
 end
