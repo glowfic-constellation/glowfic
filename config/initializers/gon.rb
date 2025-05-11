aa9c66001476154b7dab0da86eb2917bd8b084a0
Edward Jones <throne3d@gmail.com>
2025-05-11 17:23:23 -0300

    Monkey-patch gon, apipie-rails, yell for immutable string constants

---
+++ config/initializers/gon.rb
@@ -0,0 +1,22 @@
+class Gon
+  module Base
+    class << self
+      private
+
+      # replacing https://github.com/gazay/gon/blob/v6.4.0/lib/gon/base.rb#L45
+      # forcing mutable string
+      def formatted_data(_o)
+        script = +''
+        before, after = render_wrap(_o)
+        script << before
+
+        script << gon_variables(_o.global_root).
+                    map { |key, val| render_variable(_o, key, val) }.join
+        script << (render_watch(_o) || '')
+
+        script << after
+        script
+      end
+    end
+  end
+end
---
+++ config/initializers/yell.rb
@@ -0,0 +1,25 @@
+module Yell
+  class Formatter
+    private
+
+    # replacing https://github.com/rsmdt/yell/blob/master/lib/yell/formatter.rb#L203
+    # forcing mutable string
+    def to_sprintf( table )
+      buff, args, _pattern = +"", [], @pattern.dup
+
+      while true
+        match = PatternMatcher.match(_pattern)
+
+        buff << match[1] unless match[1].empty?
+        break if match[2].nil?
+
+        buff << match[2] + 's'
+        args << table[ match[3] ]
+
+        _pattern = match[4]
+      end
+
+      %Q{sprintf("#{buff.gsub(/"/, '\"')}", #{args.join(', ')})}
+    end
+  end
+end
