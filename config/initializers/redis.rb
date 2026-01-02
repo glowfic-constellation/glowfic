9f3ffb526f24e561dc63468acb84c215712e5c5f
Marri <xxstealthestarsxx@gmail.com>
2026-01-01 22:32:15 -0800

    Get out of dev

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 redis_connection = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }) if ENV['REDIS_URL']
-Rack::Attack.cache.store = redis_connection
 namespace = "glowfic:#{Rails.env}"
 $redis = Redis::Namespace.new(namespace, redis: redis_connection)
 Resque.redis = $redis
