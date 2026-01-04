38a86e35a50659fb50a6a93ab7fd95e4d3e7853f
Marri <xxstealthestarsxx@gmail.com>
2026-01-03 20:27:23 -0800

    Ban IPs (#2679)
    * Include throttle info if calling the API
    
    * more
    
    * Get out of dev
    
    * Exclude API from Allow2Ban
    
    * Grar
    
    * Grar more
    
    * We can try I guess

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -14,7 +14,9 @@ Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new(url: url, ssl_p
 
 # Throttle all requests by IP (60rpm)
 # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-Rack::Attack.throttle('req/ip', limit: ENV.fetch("RACK_ATTACK_IP_LIMIT", 25).to_i, period: 5.minutes, &:ip)
+Rack::Attack.throttle('req/ip', limit: ENV.fetch("RACK_ATTACK_IP_LIMIT", 25).to_i, period: 5.minutes) do |req|
+  req.ip unless req_logged_in?(req)
+end
 
 # Throttle POST requests to /login by IP address to prevent brute force login attacks
 # Key: "rack::attack:#{Time.now.to_i/:period}:logins/ip:#{req.ip}"
@@ -25,9 +27,9 @@ end
 # Return to user how many seconds to wait until they can start sending requests again
 Rack::Attack.throttled_response_retry_after_header = true
 
-# Includes conventional RateLimit-* headers for safe IPs:
+# Includes conventional RateLimit-* headers for safe IPs and the API:
 Rack::Attack.throttled_responder = lambda do |req|
-  return [429, {}, ["Throttled\n"]] unless safe_ips.include?(req.ip)
+  return [429, {}, ["Throttled\n"]] unless safe_ips.include?(req.ip) || req.path.starts_with?('/api')
 
   match_data = req.env['rack.attack.match_data']
   now = match_data[:epoch_time]
@@ -39,4 +41,23 @@ Rack::Attack.throttled_responder = lambda do |req|
   }
 
   [429, headers, ["Throttled\n"]]
+end
+
+def req_logged_in?(req)
+  req.session[:user_id].present?
+end
+
+# Lockout IP addresses that are hammering the app.
+Rack::Attack.blocklist('allow2ban bots') do |req|
+  next false if req_logged_in?(req)
+
+  # ban anyone at 5x the rate of our throttle limit per minute unless logged in or using API
+  Rack::Attack::Allow2Ban.filter("minute:#{req.ip}", maxretry: ENV.fetch("RACK_ATTACK_IP_LIMIT", 25).to_i, findtime: 1.minute, bantime: 1.hour) do
+    !req.path.starts_with?('/api')
+  end
+
+  # ban anyone at our throttle limit for the duration of an hour unless logged in or using API
+  Rack::Attack::Allow2Ban.filter("hour:#{req.ip}", maxretry: ENV.fetch("RACK_ATTACK_IP_LIMIT", 25).to_i * 60, findtime: 1.hour, bantime: 1.day) do
+    !req.path.starts_with?('/api')
+  end
 end
