ba9e9fc86e40ff66ba8b47d5f7d04046279c9484
Marri <xxstealthestarsxx@gmail.com>
2026-01-30 21:34:30 -0800

    Wrong method

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,5 +2,5 @@ Geocoder.configure(
   cache: Geocoder::CacheStore::Generic.new(Rails.cache, {}),
   cache_options: { expiration: 3.days },
   ip_lookup: :ipinfo_io_lite,
-  api_key: ENV.get("IPINFO_API_KEY", ""),
+  api_key: ENV.fetch("IPINFO_API_KEY", ""),
 )
