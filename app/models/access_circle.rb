5e65071341f9c53748bb7bd5b4753011b10da2fa
teceler <19716939+teceler@users.noreply.github.com>
2026-07-04 11:55:10 -0500

    More UserTag cleanup

--- app/models/access_circle.rb
+++ app/models/access_circle.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 class AccessCircle < Tag
-  has_many :user_tags, foreign_key: :tag_id, dependent: :destroy, inverse_of: :tag
-  has_many :users, through: :user_tags, inverse_of: :access_circles, dependent: :destroy
-
   validates :name, uniqueness: { scope: [:type, :user] }
 
   scope :visible, -> { where(owned: false) }
