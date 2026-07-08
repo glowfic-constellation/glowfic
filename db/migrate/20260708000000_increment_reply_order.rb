ac45b2d93e76ca74d7e47b78d47afe053965a0d4
Red Magnos <redmagnos@gmail.com>
2026-07-07 21:00:38 -0300

    Update migration

--- db/migrate/20260708000000_increment_reply_order.rb
+++ db/migrate/20260708000000_increment_reply_order.rb
@@ -1,4 +1,4 @@
-class IncrementReplyOrder < ActiveRecord::Migration[5.2]
+class IncrementReplyOrder < ActiveRecord::Migration[8.0]
   def change
     Reply.update_all('reply_order = reply_order + 1')
   end
