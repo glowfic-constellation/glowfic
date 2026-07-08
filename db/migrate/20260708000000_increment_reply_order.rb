60bbdf25dfff1383e916505485d5ada5e041848b
Red Magnos <redmagnos@gmail.com>
2026-07-07 21:03:26 -0300

    Set up and down and fix rubocop warning

--- db/migrate/20260708000000_increment_reply_order.rb
+++ db/migrate/20260708000000_increment_reply_order.rb
@@ -1,5 +1,9 @@
 class IncrementReplyOrder < ActiveRecord::Migration[8.0]
-  def change
-    Reply.update_all('reply_order = reply_order + 1')
+  def up
+    Reply.update_all('reply_order = reply_order + 1') # rubocop:disable Rails/SkipsModelValidations
+  end
+
+  def down
+    Reply.update_all('reply_order = reply_order - 1') # rubocop:disable Rails/SkipsModelValidations
   end
 end
