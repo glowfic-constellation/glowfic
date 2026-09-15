2ca8f9b7226d64ba2b89c46d60880dbd950581e4
teceler <19716939+teceler@users.noreply.github.com>
2026-09-15 18:31:12 -0500

    Refactor back to single character group per character

--- app/models/character.rb
+++ app/models/character.rb
@@ -20,8 +20,9 @@ class Character < ApplicationRecord
   has_many :character_tags, inverse_of: :character, dependent: :destroy
   has_many :settings, -> { ordered_by_char_tag }, through: :character_tags, source: :setting, dependent: :destroy
   has_many :gallery_groups, -> { ordered_by_char_tag }, through: :character_tags, source: :gallery_group, dependent: :destroy
-  has_one :character_group_tag, -> { where(section_order: 0) }, class_name: 'CharacterTag', dependent: :destroy, inverse_of: :character
-  has_one :character_group, through: :character_group_tag, source: :character_group, dependent: :destroy
+
+  has_one :character_group_tag, -> { only_character_groups }, class_name: 'CharacterTag', inverse_of: :character, dependent: :destroy
+  has_one :character_group, -> { where(type: 'CharacterGroup') }, through: :character_group_tag, dependent: :destroy
 
   validates :name,
     presence: true,
--- app/models/tag/template_tag.rb
+++ app/models/tag/template_tag.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 class Tag::TemplateTag < ApplicationRecord
-  # define this scope here or Orderable will redefine it
-  scope :ordered, -> { order(section_order: :asc) }
-  scope :ordered_manually, -> { ordered }
-  include Orderable
-
   self.table_name = 'template_tags'
 
   belongs_to :template, inverse_of: :template_tag, optional: false
@@ -12,8 +7,4 @@ class Tag::TemplateTag < ApplicationRecord
   belongs_to :character_group, foreign_key: :tag_id, inverse_of: :template_tags, optional: true # This is (currently) required but see above
 
   validates :template, uniqueness: true
-
-  def ordered_attributes
-    [:template_id]
-  end
 end
